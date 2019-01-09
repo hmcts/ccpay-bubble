785c6cbb28b8db5127a73bab7c81b4e8c166ed17
Your Name <you@example.com>
2019-01-09 14:37:27 +0000

    Added smoke test

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -42,6 +42,7 @@ module "ccpay-bubble" {
   asp_rg = "${local.rgName}"
 
   app_settings = {
+    E2E_FRONTEND_URL = "${TEST_URL}"
     // Logging vars
     REFORM_TEAM = "${var.product}"
     REFORM_SERVICE_NAME = "${var.microservice}"
