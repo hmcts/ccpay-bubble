ac7ac903d8144ecf5fe62122fb3e72b0ee0e4fcb
Your Name <you@example.com>
2019-01-16 14:34:08 +0000

    Added gulp and govuk styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './routes/app-routing.module';
 import { AppComponent } from './app.component';
+import { AddFeeDetailComponent } from './components/add-fee-detail/add-fee-detail.component';
+import { PhaseBannerComponent } from './shared/components/phase-banner/phase-banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddFeeDetailComponent,
+    PhaseBannerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/add-fee-detail/add-fee-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-fee-detail',
+  templateUrl: './add-fee-detail.component.html',
+  styleUrls: ['./add-fee-detail.component.scss']
+})
+export class AddFeeDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
