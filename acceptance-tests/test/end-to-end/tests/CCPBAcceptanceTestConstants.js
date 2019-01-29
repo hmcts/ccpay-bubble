41b95f3a7bc67923e4edc2ab5f9f5789b5db45bc
Your Name <you@example.com>
2019-01-29 11:48:31 +0000

    revamped pay bubble

---
+++ acceptance-tests/test/end-to-end/tests/CCPBAcceptanceTestConstants.js
@@ -0,0 +1,9 @@
+module.exports = {
+  windowsSizeX: 1600,
+  windowsSizeY: 1200,
+  twoSecondWaitTime: 4,
+  fiveSecondWaitTime: 10,
+  tenSecondWaitTime: 20,
+  thirtySecondWaitTime: 40,
+  retryCountForStep: 4
+};
