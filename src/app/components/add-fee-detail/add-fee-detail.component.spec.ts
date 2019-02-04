3c524a9ed02fd966c5d499a30e5660d3517050b2
Your Name <you@example.com>
2019-02-04 16:04:17 +0000

    Added missing test modules

--- src/app/components/add-fee-detail/add-fee-detail.component.spec.ts
+++ src/app/components/add-fee-detail/add-fee-detail.component.spec.ts
@@ -1,11 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
-import {RouterModule} from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AddFeeDetailComponent } from './add-fee-detail.component';
 import { AddFeeDetailServiceMock } from '../test-mocks/add-fee-detail.service.mock';
 import { AddFeeDetailService } from 'src/app/services/add-fee-detail/add-fee-detail.service';
+import { PaybubbleHttpClient } from 'src/app/services/httpclient/paybubble.http.client';
 
 describe('AddFeeDetailComponent', () => {
   let component: AddFeeDetailComponent;
@@ -15,9 +18,10 @@ describe('AddFeeDetailComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ AddFeeDetailComponent ],
       providers: [
+        PaybubbleHttpClient,
         { provide: AddFeeDetailService, useValue: AddFeeDetailServiceMock }
       ],
-      imports: [FormsModule, RouterModule, RouterTestingModule.withRoutes([])]
+      imports: [HttpModule, HttpClientModule, FormsModule, RouterModule, RouterTestingModule.withRoutes([])]
     })
     .compileComponents();
   }));
--- src/app/components/review-fee-detail/review-fee-detail.component.spec.ts
+++ src/app/components/review-fee-detail/review-fee-detail.component.spec.ts
@@ -2,6 +2,10 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ReviewFeeDetailComponent } from './review-fee-detail.component';
 import { Location, LocationStrategy } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AddFeeDetailServiceMock } from '../test-mocks/add-fee-detail.service.mock';
+import { AddFeeDetailService } from 'src/app/services/add-fee-detail/add-fee-detail.service';
 
 const LocationMock = {
   back() {
@@ -15,9 +19,11 @@ describe('ReviewFeeDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ RouterModule, RouterTestingModule.withRoutes([]) ],
       declarations: [ ReviewFeeDetailComponent ],
       providers: [
-        { provide: Location, useValue: LocationMock }
+        { provide: Location, useValue: LocationMock },
+        { provide: AddFeeDetailService, useValue: AddFeeDetailServiceMock }
       ]
     })
     .compileComponents();
