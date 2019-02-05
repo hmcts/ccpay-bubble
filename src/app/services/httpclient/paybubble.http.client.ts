45bd2b5f2cf648905c3b90468d91bcf3cf05e927
Your Name <you@example.com>
2019-02-05 18:08:40 +0000

    Added method to get service auth token

--- src/app/services/httpclient/paybubble.http.client.ts
+++ src/app/services/httpclient/paybubble.http.client.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Meta } from '@angular/platform-browser';
 import { Observable } from 'rxjs';
-import { httpFactory } from '@angular/http/src/http_module';
 
 @Injectable()
 export class PaybubbleHttpClient {
