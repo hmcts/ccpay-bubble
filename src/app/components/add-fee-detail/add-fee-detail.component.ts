a730ae0c590fdb05d845113dc1e24b922e1b6d71
James Nelson <Jamesnelson117@gmail.com>
2019-02-06 10:27:53 +0000

    Moved fee data into a stubbed object

--- src/app/components/add-fee-detail/add-fee-detail.component.ts
+++ src/app/components/add-fee-detail/add-fee-detail.component.ts
@@ -3,6 +3,7 @@ import { AddFeeDetailService } from 'src/app/services/add-fee-detail/add-fee-det
 import { FeeModel } from 'src/app/models/FeeModel';
 import { PaymentModel } from 'src/app/models/PaymentModel';
 import { RemissionModel } from 'src/app/models/RemissionModel';
+import { feeData } from '../../../stubs/feeData';
 
 @Component({
   selector: 'app-add-fee-detail',
@@ -22,400 +23,12 @@ export class AddFeeDetailComponent implements OnInit {
   feeModels: FeeModel[] = [];
   payModel: PaymentModel;
   remissionModel: RemissionModel;
-  feeData = `
-  [
-  {
-		"code": "FEE0002",
-		"fee_type": "fixed",
-		"channel_type": {
-			"name": "default"
-		},
-		"event_type": {
-			"name": "issue"
-		},
-		"jurisdiction1": {
-			"name": "family"
-		},
-		"jurisdiction2": {
-			"name": "family court"
-		},
-		"service_type": {
-			"name": "divorce"
-		},
-		"applicant_type": {
-			"name": "all"
-		},
-		"fee_versions": [
-			{
-				"description": "Filing an application for a divorce, nullity or civil partnership dissolution",
-				"status": "approved",
-				"version": 4,
-				"valid_from": "2016-03-21T00:00:00.000+0000",
-				"flat_amount": {
-					"amount": 550.00
-				},
-				"memo_line": "GOV - App for divorce/nullity of marriage or CP",
-				"statutory_instrument": "2016 No. 402 (L. 5)",
-				"si_ref_id": "1.2",
-				"natural_account_code": "4481102159",
-				"fee_order_name": "The Civil Proceedings, Family Proceedings and Upper Tribunal Fees (Amendment) Order 2016",
-				"direction": "enhanced"
-			}
-		],
-		"current_version": {
-			"description": "Filing an application for a divorce, nullity or civil partnership dissolution",
-			"status": "approved",
-			"version": 4,
-			"valid_from": "2016-03-21T00:00:00.000+0000",
-			"flat_amount": {
-				"amount": 550.00
-			},
-			"memo_line": "GOV - App for divorce/nullity of marriage or CP",
-			"statutory_instrument": "2016 No. 402 (L. 5)",
-			"si_ref_id": "1.2",
-			"natural_account_code": "4481102159",
-			"fee_order_name": "The Civil Proceedings, Family Proceedings and Upper Tribunal Fees (Amendment) Order 2016",
-			"direction": "enhanced"
-		},
-		"unspecified_claim_amount": false
-    },
-    {
-		"code": "FEE0387",
-		"fee_type": "fixed",
-		"channel_type": {
-			"name": "default"
-		},
-		"event_type": {
-			"name": "issue"
-		},
-		"jurisdiction1": {
-			"name": "family"
-		},
-		"jurisdiction2": {
-			"name": "family court"
-		},
-		"service_type": {
-			"name": "divorce"
-		},
-		"applicant_type": {
-			"name": "all"
-		},
-		"fee_versions": [
-			{
-				"description": "Originating proceedings where no other fee is specified",
-				"status": "approved",
-				"version": 4,
-				"valid_from": "2016-03-21T00:00:00.000+0000",
-				"flat_amount": {
-					"amount": 245.00
-				},
-				"memo_line": "GOV - App for divorce/nullity of marriage or CP",
-				"statutory_instrument": "2016 No. 402 (L. 5)",
-				"si_ref_id": "1.2",
-				"natural_account_code": "4481102159",
-				"fee_order_name": "The Civil Proceedings, Family Proceedings and Upper Tribunal Fees (Amendment) Order 2016",
-				"direction": "enhanced"
-			}
-		],
-		"current_version": {
-			"description": "Originating proceedings where no other fee is specified",
-			"status": "approved",
-			"version": 4,
-			"valid_from": "2016-03-21T00:00:00.000+0000",
-			"flat_amount": {
-				"amount": 245.00
-			},
-			"memo_line": "GOV - App for divorce/nullity of marriage or CP",
-			"statutory_instrument": "2016 No. 402 (L. 5)",
-			"si_ref_id": "1.2",
-			"natural_account_code": "4481102159",
-			"fee_order_name": "The Civil Proceedings, Family Proceedings and Upper Tribunal Fees (Amendment) Order 2016",
-			"direction": "enhanced"
-		},
-		"unspecified_claim_amount": false
-    },
-    {
-		"code": "FEE0232",
-		"fee_type": "fixed",
-		"channel_type": {
-			"name": "default"
-		},
-		"event_type": {
-			"name": "issue"
-		},
-		"jurisdiction1": {
-			"name": "family"
-		},
-		"jurisdiction2": {
-			"name": "family court"
-		},
-		"service_type": {
-			"name": "divorce"
-		},
-		"applicant_type": {
-			"name": "all"
-		},
-		"fee_versions": [
-			{
-				"description": "Amendment of application for matrimonial/civil partnership order",
-				"status": "approved",
-				"version": 4,
-				"valid_from": "2016-03-21T00:00:00.000+0000",
-				"flat_amount": {
-					"amount": 95.00
-				},
-				"memo_line": "GOV - App for divorce/nullity of marriage or CP",
-				"statutory_instrument": "2016 No. 402 (L. 5)",
-				"si_ref_id": "1.2",
-				"natural_account_code": "4481102159",
-				"fee_order_name": "The Civil Proceedings, Family Proceedings and Upper Tribunal Fees (Amendment) Order 2016",
-				"direction": "enhanced"
-			}
-		],
-		"current_version": {
-			"description": "Amendment of application for matrimonial/civil partnership order",
-			"status": "approved",
-			"version": 4,
-			"valid_from": "2016-03-21T00:00:00.000+0000",
-			"flat_amount": {
-				"amount": 95.00
-			},
-			"memo_line": "GOV - App for divorce/nullity of marriage or CP",
-			"statutory_instrument": "2016 No. 402 (L. 5)",
-			"si_ref_id": "1.2",
-			"natural_account_code": "4481102159",
-			"fee_order_name": "The Civil Proceedings, Family Proceedings and Upper Tribunal Fees (Amendment) Order 2016",
-			"direction": "enhanced"
-		},
-		"unspecified_claim_amount": false
-    },
-    {
-		"code": "FEE0270",
-		"fee_type": "fixed",
-		"channel_type": {
-			"name": "default"
-		},
-		"event_type": {
-			"name": "issue"
-		},
-		"jurisdiction1": {
-			"name": "family"
-		},
-		"jurisdiction2": {
-			"name": "family court"
-		},
-		"service_type": {
-			"name": "divorce"
-		},
-		"applicant_type": {
-			"name": "all"
-		},
-		"fee_versions": [
-			{
-				"description": "Application for decree nisi, conditional order, separation order (no fee if undefended)",
-				"status": "approved",
-				"version": 4,
-				"valid_from": "2016-03-21T00:00:00.000+0000",
-				"flat_amount": {
-					"amount": 50.00
-				},
-				"memo_line": "GOV - App for divorce/nullity of marriage or CP",
-				"statutory_instrument": "2016 No. 402 (L. 5)",
-				"si_ref_id": "1.2",
-				"natural_account_code": "4481102159",
-				"fee_order_name": "The Civil Proceedings, Family Proceedings and Upper Tribunal Fees (Amendment) Order 2016",
-				"direction": "enhanced"
-			}
-		],
-		"current_version": {
-			"description": "Application for decree nisi, conditional order, separation order (no fee if undefended)",
-			"status": "approved",
-			"version": 4,
-			"valid_from": "2016-03-21T00:00:00.000+0000",
-			"flat_amount": {
-				"amount": 50.00
-			},
-			"memo_line": "GOV - App for divorce/nullity of marriage or CP",
-			"statutory_instrument": "2016 No. 402 (L. 5)",
-			"si_ref_id": "1.2",
-			"natural_account_code": "4481102159",
-			"fee_order_name": "The Civil Proceedings, Family Proceedings and Upper Tribunal Fees (Amendment) Order 2016",
-			"direction": "enhanced"
-		},
-		"unspecified_claim_amount": false
-    },
-    {
-		"code": "FEE0391",
-		"fee_type": "fixed",
-		"channel_type": {
-			"name": "default"
-		},
-		"event_type": {
-			"name": "issue"
-		},
-		"jurisdiction1": {
-			"name": "family"
-		},
-		"jurisdiction2": {
-			"name": "family court"
-		},
-		"service_type": {
-			"name": "divorce"
-		},
-		"applicant_type": {
-			"name": "all"
-		},
-		"fee_versions": [
-			{
-				"description": "Request for service by a bailiff of document (see order for exceptions)",
-				"status": "approved",
-				"version": 4,
-				"valid_from": "2016-03-21T00:00:00.000+0000",
-				"flat_amount": {
-					"amount": 110.00
-				},
-				"memo_line": "GOV - App for divorce/nullity of marriage or CP",
-				"statutory_instrument": "2016 No. 402 (L. 5)",
-				"si_ref_id": "1.2",
-				"natural_account_code": "4481102159",
-				"fee_order_name": "The Civil Proceedings, Family Proceedings and Upper Tribunal Fees (Amendment) Order 2016",
-				"direction": "enhanced"
-			}
-		],
-		"current_version": {
-			"description": "Request for service by a bailiff of document (see order for exceptions)",
-			"status": "approved",
-			"version": 4,
-			"valid_from": "2016-03-21T00:00:00.000+0000",
-			"flat_amount": {
-				"amount": 110.00
-			},
-			"memo_line": "GOV - App for divorce/nullity of marriage or CP",
-			"statutory_instrument": "2016 No. 402 (L. 5)",
-			"si_ref_id": "1.2",
-			"natural_account_code": "4481102159",
-			"fee_order_name": "The Civil Proceedings, Family Proceedings and Upper Tribunal Fees (Amendment) Order 2016",
-			"direction": "enhanced"
-		},
-		"unspecified_claim_amount": false
-    },
-    {
-		"code": "FEE0229",
-		"fee_type": "fixed",
-		"channel_type": {
-			"name": "default"
-		},
-		"event_type": {
-			"name": "issue"
-		},
-		"jurisdiction1": {
-			"name": "family"
-		},
-		"jurisdiction2": {
-			"name": "family court"
-		},
-		"service_type": {
-			"name": "divorce"
-		},
-		"applicant_type": {
-			"name": "all"
-		},
-		"fee_versions": [
-			{
-				"description": "Application for a financial order",
-				"status": "approved",
-				"version": 4,
-				"valid_from": "2016-03-21T00:00:00.000+0000",
-				"flat_amount": {
-					"amount": 255.00
-				},
-				"memo_line": "GOV - App for divorce/nullity of marriage or CP",
-				"statutory_instrument": "2016 No. 402 (L. 5)",
-				"si_ref_id": "1.2",
-				"natural_account_code": "4481102159",
-				"fee_order_name": "The Civil Proceedings, Family Proceedings and Upper Tribunal Fees (Amendment) Order 2016",
-				"direction": "enhanced"
-			}
-		],
-		"current_version": {
-			"description": "Application for a financial order",
-			"status": "approved",
-			"version": 4,
-			"valid_from": "2016-03-21T00:00:00.000+0000",
-			"flat_amount": {
-				"amount": 255.00
-			},
-			"memo_line": "GOV - App for divorce/nullity of marriage or CP",
-			"statutory_instrument": "2016 No. 402 (L. 5)",
-			"si_ref_id": "1.2",
-			"natural_account_code": "4481102159",
-			"fee_order_name": "The Civil Proceedings, Family Proceedings and Upper Tribunal Fees (Amendment) Order 2016",
-			"direction": "enhanced"
-		},
-		"unspecified_claim_amount": false
-    },
-    {
-		"code": "FEE0228",
-		"fee_type": "fixed",
-		"channel_type": {
-			"name": "default"
-		},
-		"event_type": {
-			"name": "issue"
-		},
-		"jurisdiction1": {
-			"name": "family"
-		},
-		"jurisdiction2": {
-			"name": "family court"
-		},
-		"service_type": {
-			"name": "divorce"
-		},
-		"applicant_type": {
-			"name": "all"
-		},
-		"fee_versions": [
-			{
-				"description": "Application (without notice)",
-				"status": "approved",
-				"version": 4,
-				"valid_from": "2016-03-21T00:00:00.000+0000",
-				"flat_amount": {
-					"amount": 50.00
-				},
-				"memo_line": "GOV - App for divorce/nullity of marriage or CP",
-				"statutory_instrument": "2016 No. 402 (L. 5)",
-				"si_ref_id": "1.2",
-				"natural_account_code": "4481102159",
-				"fee_order_name": "The Civil Proceedings, Family Proceedings and Upper Tribunal Fees (Amendment) Order 2016",
-				"direction": "enhanced"
-			}
-		],
-		"current_version": {
-			"description": "Application (without notice)",
-			"status": "approved",
-			"version": 4,
-			"valid_from": "2016-03-21T00:00:00.000+0000",
-			"flat_amount": {
-				"amount": 50.00
-			},
-			"memo_line": "GOV - App for divorce/nullity of marriage or CP",
-			"statutory_instrument": "2016 No. 402 (L. 5)",
-			"si_ref_id": "1.2",
-			"natural_account_code": "4481102159",
-			"fee_order_name": "The Civil Proceedings, Family Proceedings and Upper Tribunal Fees (Amendment) Order 2016",
-			"direction": "enhanced"
-		},
-		"unspecified_claim_amount": false
-    }
-  ]`;
+  feeData = feeData;
 
   constructor(private addFeeDetailService: AddFeeDetailService) { }
 
   ngOnInit() {
-    this.feeModels = [];
-    this.buildFeeList();
+    this.buildFeeList(this.feeData);
   }
 
   toggleHwfFields() {
@@ -423,12 +36,13 @@ export class AddFeeDetailComponent implements OnInit {
   }
 
   saveAndContinue() {
-    const selectedFeeModels = this.feeModels.filter(
-      feeModel => feeModel.checked === true
-    );
-    FeeModel.models = selectedFeeModels;
-    this.populatePayAndRemissionModel(selectedFeeModels);
+    FeeModel.models = this.filterSelectedFees();
+    this.populatePayAndRemissionModel(FeeModel.models);
     this.addFeeDetailService.saveAndContinue();
+  }
+
+	filterSelectedFees(): FeeModel[] {
+    return this.feeModels.filter(feeModel => feeModel.checked === true);
   }
 
   selectPaymentInstruction(model: FeeModel) {
@@ -450,18 +64,14 @@ export class AddFeeDetailComponent implements OnInit {
     this.remissionModel.hwf_amount = (this.amount_to_pay) ? selectedFeeModels[0].calculated_amount - this.amount_to_pay : null;
     this.remissionModel.hwf_reference = this.hwf_code;
     RemissionModel.model = this.remissionModel;
-
-    console.log('PayModel: ' + JSON.stringify(PaymentModel.model));
-    console.log('RemissionModel: ' + JSON.stringify(RemissionModel.model));
-    console.log('FeeModel: ' + JSON.stringify(selectedFeeModels[0]));
   }
 
-  buildFeeList() {
-    const feesList = JSON.parse(this.feeData);
+  buildFeeList(feeData: string) {
+		const feesList = JSON.parse(feeData);
+
     feesList.forEach(data => {
       const keys = Object.keys(data);
-      let i = 0;
-      for (i = 0; i < keys.length; i++) {
+      for (let i = 0; i < keys.length; i++) {
         this.feeModel = new FeeModel();
         this.feeModel.checked = false;
         if (data.hasOwnProperty('code')) {
