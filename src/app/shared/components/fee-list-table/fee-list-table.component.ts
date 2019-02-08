3c5c38b689dc59f5ff5d7536a683d8ef9b5ce9f5
James Nelson <Jamesnelson117@gmail.com>
2019-02-08 17:18:41 +0000

    Fee list table seperated into a child component

---
+++ src/app/shared/components/fee-list-table/fee-list-table.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FeeModel } from 'src/app/models/FeeModel';
+
+@Component({
+  selector: 'app-fee-list-table',
+  templateUrl: './fee-list-table.component.html',
+  styleUrls: ['./fee-list-table.component.scss'],
+})
+export class FeeListTableComponent {
+  @Input() fees: FeeModel[];
+  @Output() feeChangedEventEmitter = new EventEmitter();
+
+  selectFee($event: FeeModel) {
+    this.feeChangedEventEmitter.emit($event);
+  }
+}
