22014b6504fcd512df0393715c2672ebe3787d4f
James Nelson <Jamesnelson117@gmail.com>
2019-02-08 17:21:45 +0000

    Refactored review fee detail component

--- src/app/components/review-fee-detail/review-fee-detail.component.ts
+++ src/app/components/review-fee-detail/review-fee-detail.component.ts
@@ -1,21 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { FeeModel } from 'src/app/models/FeeModel';
-import { PaymentModel } from 'src/app/models/PaymentModel';
-import { RemissionModel } from 'src/app/models/RemissionModel';
+import { Component } from '@angular/core';
 import { AddFeeDetailService } from 'src/app/services/add-fee-detail/add-fee-detail.service';
 import { IResponse } from 'src/app/interfaces/response';
 import { Router } from '@angular/router';
+import { FeeModel } from 'src/app/models/FeeModel';
 
 @Component({
   selector: 'app-review-fee-detail',
   templateUrl: './review-fee-detail.component.html',
   styleUrls: ['./review-fee-detail.component.scss']
 })
-export class ReviewFeeDetailComponent implements OnInit {
-
-  feeModels = FeeModel.models;
-  payModel = PaymentModel.model;
-  remissionModel = RemissionModel.model;
+export class ReviewFeeDetailComponent {
+  fee: FeeModel = this.addFeeDetailService.selectedFee;
   display_fee_amount: string;
   display_amount_to_pay: string;
   paymentReference = '';
@@ -26,18 +21,16 @@ export class ReviewFeeDetailComponent implements OnInit {
     private addFeeDetailService: AddFeeDetailService
   ) { }
 
-  ngOnInit() {
-    this.setDisplayAmounts();
+  get payModel() {
+    return this.addFeeDetailService.paymentModel;
   }
 
-  setDisplayAmounts() {
-    this.display_amount_to_pay = '£ ' + parseFloat(this.payModel.amount + '').toFixed(2);
-    this.display_fee_amount = '£ ' + parseFloat((this.feeModels) ? this.feeModels[0].calculated_amount + '' : '').toFixed(2);
+  get remissionModel() {
+    return this.addFeeDetailService.remissionModel;
   }
 
   sendPayDetailsToPayhub() {
-    this.addFeeDetailService.sendPayDetailsToPayhub(PaymentModel.cleanModel(this.payModel))
-    .subscribe((response: IResponse) => {
+    this.addFeeDetailService.sendPayDetailsToPayhub().subscribe((response: IResponse) => {
       if (!response.data && response.success) { return this.router.navigateByUrl('/api/addFeeDetail'); }
     });
   }
