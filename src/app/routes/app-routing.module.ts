2e5b9ebcab5e05e82a2357a1a882165e8a728681
James Nelson <Jamesnelson117@gmail.com>
2019-02-08 17:22:14 +0000

    Moved logic to property bag service

--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -2,22 +2,28 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddFeeDetailComponent } from '../components/add-fee-detail/add-fee-detail.component';
 import { ReviewFeeDetailComponent } from '../components/review-fee-detail/review-fee-detail.component';
+import { ReviewFeeDetailRouteGuard } from 'src/app/components/review-fee-detail/route-guards/review-fee-detail.service';
 
 const routes: Routes = [
-  { path: 'addFeeDetail',
+  {
+    path: 'addFeeDetail',
     component: AddFeeDetailComponent
   },
-  { path: '',
+  {
+    path: 'reviewFeeDetail',
+    component: ReviewFeeDetailComponent,
+    canActivate: [ReviewFeeDetailRouteGuard]
+  },
+  {
+    path: '',
     pathMatch: 'full',
     redirectTo: 'addFeeDetail'
   },
-  { path: 'reviewFeeDetail',
-    component: ReviewFeeDetailComponent
-  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [ReviewFeeDetailRouteGuard]
 })
 export class AppRoutingModule { }
