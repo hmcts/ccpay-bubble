a360bcd5a394788d9108ced363a10a80c81e2aed
James Nelson <Jamesnelson117@gmail.com>
2019-02-12 12:42:13 +0000

    Minor service refactoring

--- src/app/components/add-fee-detail/add-fee-detail.component.spec.ts
+++ src/app/components/add-fee-detail/add-fee-detail.component.spec.ts
@@ -45,33 +45,10 @@ describe('AddFeeDetailComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('Should GET and SET a service type', () => {
-    component.serviceType = 'test';
-    expect(component.serviceType).toBe('test');
-  });
-
   it('Should toggle help with fees', () => {
     component.helpWithFeesIsVisible = false;
     component.toggleHelpWithFees();
     expect(component.helpWithFeesIsVisible).toBeTruthy();
-  });
-
-  it('Should SET help with fees code', () => {
-    component.helpWithFeesCode = '123';
-    const service = fixture.debugElement.injector.get(AddFeeDetailService);
-    expect(service.helpWithFeesCode).toBe('123');
-  });
-
-  it('Should SET case reference', () => {
-    component.caseReference = '123';
-    const service = fixture.debugElement.injector.get(AddFeeDetailService);
-    expect(service.caseReference).toBe('123');
-  });
-
-  it('Should SET amount to pay', () => {
-    component.amountToPay = 10.00;
-    const service = fixture.debugElement.injector.get(AddFeeDetailService);
-    expect(service.amountToPay).toBe(10.00);
   });
 
   it('Should select a fee', () => {
