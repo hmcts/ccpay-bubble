f882014960a37fe5caf351795d039f84c16bb6d9
James Nelson <Jamesnelson117@gmail.com>
2019-02-12 12:42:13 +0000

    Minor service refactoring

--- src/app/components/add-fee-detail/add-fee-detail.component.ts
+++ src/app/components/add-fee-detail/add-fee-detail.component.ts
@@ -2,53 +2,39 @@ import { Component } from '@angular/core';
 import { FeeModel } from 'src/app/models/FeeModel';
 import { Router } from '@angular/router';
 import { AddFeeDetailService } from 'src/app/services/add-fee-detail/add-fee-detail.service';
-import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-add-fee-detail',
   templateUrl: './add-fee-detail.component.html',
   styleUrls: ['./add-fee-detail.component.scss']
 })
-export class AddFeeDetailComponent implements OnInit {
+export class AddFeeDetailComponent {
   helpWithFeesIsVisible = false;
   fees: FeeModel[] = this.addFeeDetailService.buildFeeList();
+  serviceType = 'DIVORCE';
+  helpWithFeesCode = '';
+  caseReference = '';
+  amountToPay = null;
 
   constructor(
     private router: Router,
     private addFeeDetailService: AddFeeDetailService
   ) { }
 
-  ngOnInit() {
-    this.addFeeDetailService.reset();
-  }
-
-  get serviceType() {
-    return this.addFeeDetailService.serviceType;
-  }
-
-  set serviceType(serviceType: string) {
-    this.addFeeDetailService.serviceType = serviceType;
-  }
-
-  set helpWithFeesCode(helpWithFeesCode: string) {
-    this.addFeeDetailService.helpWithFeesCode = helpWithFeesCode;
-  }
-
-  set caseReference(caseRef: string) {
-    this.addFeeDetailService.caseReference = caseRef;
-  }
-
-  set amountToPay(amountToPay: number) {
-    this.addFeeDetailService.amountToPay = amountToPay;
-  }
-
   toggleHelpWithFees() {
     this.helpWithFeesIsVisible = !this.helpWithFeesIsVisible;
   }
 
   saveAndContinue() {
-    this.addFeeDetailService.setNewPaymentModel();
-    this.addFeeDetailService.setNewRemissionModel();
+    const feeDetailProps = {
+      serviceType: this.serviceType,
+      helpWithFeesCode: this.helpWithFeesCode,
+      caseReference: this.caseReference,
+      amountToPay: this.amountToPay
+    };
+
+    this.addFeeDetailService.setNewPaymentModel(feeDetailProps);
+    this.addFeeDetailService.setNewRemissionModel(feeDetailProps);
     this.router.navigateByUrl('/reviewFeeDetail');
   }
 
