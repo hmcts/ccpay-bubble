fc00f2ae8d33b70ff7283408ffd7878f88e006fe
Your Name <you@example.com>
2019-02-12 17:14:19 +0000

    Added link reading code

--- express/mvc/controller/PayhubController.js
+++ express/mvc/controller/PayhubController.js
@@ -6,9 +6,16 @@ class PayhubController {
   }
 
   sendToPayhub(req, res, appInsights) {
-    return this.payhubService.sendToPayhub(req, appInsights)
+    return this.payhubService.sendToPayhub(req, res, appInsights)
       .then(result => {
-        res.json({ data: result, success: true });
+        if (result._links.next_url) {
+          this.nextUrl = result._links.next_url.href;
+          res.redirect(this.nextUrl);
+          res.json({ data: 'Redirected to payment page', success: true });
+        } else {
+          const invalidJson = `Invalid json received from Payment Hub: ${JSON.stringify(result)}`;
+          res.json({ err: `${invalidJson}`, success: false });
+        }
       })
       .catch(error => {
         res.json({ err: error, success: false });
