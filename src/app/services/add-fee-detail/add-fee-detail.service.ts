2b7461195065a88cce17a4cc31118cad54005395
James Nelson <Jamesnelson117@gmail.com>
2019-02-18 11:11:51 +0000

    PAY-1586 - Updated unit tests

--- src/app/services/add-fee-detail/add-fee-detail.service.ts
+++ src/app/services/add-fee-detail/add-fee-detail.service.ts
@@ -54,7 +54,8 @@ export class AddFeeDetailService {
     paymentModel.ccd_case_number = props.caseReference;
     paymentModel.fees = [this.selectedFee];
     paymentModel.service = props.serviceType;
-    paymentModel.amount = (props.amountToPay >= 0) ? props.amountToPay : this.selectedFee.calculated_amount;
+    paymentModel.amount = (props.amountToPay === 0 || props.amountToPay)
+    ? props.amountToPay : this.selectedFee.calculated_amount;
     this.paymentModel = paymentModel;
   }
 
@@ -62,7 +63,8 @@ export class AddFeeDetailService {
     const remissionModel = new RemissionModel();
     remissionModel.ccd_case_number = props.caseReference;
     remissionModel.fee = this.selectedFee;
-    remissionModel.hwf_amount = (props.amountToPay >= 0) ? this.selectedFee.calculated_amount - props.amountToPay : null;
+    remissionModel.hwf_amount = (props.amountToPay === 0 || props.amountToPay)
+     ? this.selectedFee.calculated_amount - props.amountToPay : null;
     remissionModel.hwf_reference = props.helpWithFeesCode;
     this.remissionModel = remissionModel;
   }
--- src/app/shared/validators/help-with-fees.validator.ts
+++ src/app/shared/validators/help-with-fees.validator.ts
@@ -3,14 +3,10 @@ import { AbstractControl } from '@angular/forms';
 export const helpWithFeesValidator = (control: AbstractControl): { [key: string]: boolean } | null => {
   const codeControl = control.get('code'),
     amountControl = control.get('amount');
-  console.log('hwf code', codeControl.value);
-  console.log('hwf amount', amountControl.value);
 
   if (!codeControl.value && amountControl.value !== null) {
-    console.log('hwfcodeInvalid');
     return { 'hwfCodeInvalid' : true };
   } else if (codeControl.value && amountControl.value == null) {
-    console.log('hwfAmountInvalid');
     return { 'hwfAmountInvalid' : true };
   } else if (amountControl.value < 0) {
     return { 'hwfAmountNegative' : true };
