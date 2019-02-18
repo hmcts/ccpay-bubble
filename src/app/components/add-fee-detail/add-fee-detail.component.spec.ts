71f156c94582ab6b8d155e160bc95bc175620ea6
James Nelson <Jamesnelson117@gmail.com>
2019-02-18 11:11:08 +0000

    PAY-1586 - Paymodel amount now convereted to a number type

--- src/app/components/add-fee-detail/add-fee-detail.component.spec.ts
+++ src/app/components/add-fee-detail/add-fee-detail.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
@@ -24,11 +24,13 @@ describe('AddFeeDetailComponent', () => {
       providers: [
         PaybubbleHttpClient,
         AddFeeDetailService,
+        FormBuilder,
         { provide: Router, useValue: routerMock }
       ],
       imports: [
         HttpClientModule,
         FormsModule,
+        ReactiveFormsModule,
         RouterModule,
         RouterTestingModule.withRoutes([])],
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
@@ -60,13 +62,103 @@ describe('AddFeeDetailComponent', () => {
     const fee: FeeModel = new FeeModel;
     component.selectFee(fee);
     fixture.detectChanges();
-    const service = fixture.debugElement.injector.get(AddFeeDetailService);
-    expect(service.selectedFee).toEqual(fee);
+    expect(component.selectedFee).toEqual(fee);
+  });
+
+  describe('Fee form validation', () => {
+    it('Validation errors should exist if a CCD case reference is not provided', () => {
+      component.feeDetailForm.controls['selectedFee'].setValue(true);
+      expect(component.feeDetailForm.invalid).toBe(true);
+    });
+
+    it('Validation errors should exist if a CCD case reference is too long', () => {
+      component.feeDetailForm.controls['caseReference'].setValue('1111-1111-1111-1111-1111');
+      component.feeDetailForm.controls['selectedFee'].setValue(true);
+      expect(component.feeDetailForm.invalid).toBe(true);
+    });
+
+    it('Validation errors should exist if a CCD case reference is too short', () => {
+      component.feeDetailForm.controls['caseReference'].setValue('1111-1111');
+      component.feeDetailForm.controls['selectedFee'].setValue(true);
+      expect(component.feeDetailForm.invalid).toBe(true);
+    });
+
+    it('Validation errors should exist if an HWF code is entered but not an HWF ammount', () => {
+      component.feeDetailForm.controls['caseReference'].setValue('1111-1111-1111-1111');
+      component.feeDetailForm.controls['selectedFee'].setValue(true);
+      component.feeDetailForm.get('helpWithFees.code').setValue('HWF-123');
+      component.setHelpWithFeesValidation();
+      expect(component.feeDetailForm.invalid).toBe(true);
+    });
+
+    it('Validation errors should exist if an HWF amount is entered but not an HWF code', () => {
+      component.feeDetailForm.controls['caseReference'].setValue('1111-1111-1111-1111');
+      component.feeDetailForm.controls['selectedFee'].setValue(true);
+      component.feeDetailForm.get('helpWithFees.amount').setValue(0);
+      component.setHelpWithFeesValidation();
+      expect(component.feeDetailForm.invalid).toBe(true);
+    });
+
+    it('Validation errors should exist if an HWF amount is negative', () => {
+      component.feeDetailForm.controls['caseReference'].setValue('1111-1111-1111-1111');
+      component.feeDetailForm.controls['selectedFee'].setValue(true);
+      component.feeDetailForm.get('helpWithFees.amount').setValue(-10);
+      component.setHelpWithFeesValidation();
+      expect(component.feeDetailForm.invalid).toBe(true);
+    });
+
+    it('Validation errors should exist if an HWF amount is greater than selected fee amount', () => {
+      const fee: FeeModel = new FeeModel;
+      fee.calculated_amount = 20;
+      component.selectedFee = fee;
+      fixture.detectChanges();
+      component.feeDetailForm.controls['caseReference'].setValue('1111-1111-1111-1111');
+      component.feeDetailForm.controls['selectedFee'].setValue(true);
+      component.setHelpWithFeesAmountValidation();
+      component.feeDetailForm.get('helpWithFees.code').setValue('HWF-123');
+      component.feeDetailForm.get('helpWithFees.amount').setValue(30);
+      component.saveAndContinue();
+      expect(component.feeDetailForm.invalid).toBe(true);
+    });
+
+    it('Validation errors should not show if no validation errors exist', () => {
+      component.feeDetailForm.controls['caseReference'].setValue('1111-1111-1111-1111');
+      component.feeDetailForm.controls['selectedFee'].setValue(true);
+      const fee: FeeModel = new FeeModel;
+      fee.calculated_amount = 20.00;
+      component.selectedFee = fee;
+      fixture.detectChanges();
+      component.saveAndContinue();
+      fixture.detectChanges();
+      expect(component.showErrors).toBe(false);
+    });
+
+    it('Validation errors should show if form builder validation errors exist', () => {
+      component.feeDetailForm.controls['caseReference'].setValue('1111-1111-1111');
+      component.feeDetailForm.controls['selectedFee'].setValue(false);
+      component.saveAndContinue();
+      fixture.detectChanges();
+      expect(component.showErrors).toBe(true);
+    });
+  });
+
+  it('Saving and continuing should commit a selected fee to the service', () => {
+    const fee: FeeModel = new FeeModel,
+      service = fixture.debugElement.injector.get(AddFeeDetailService);
+    fee.calculated_amount = 20.00;
+    component.selectedFee = fee;
+    component.feeDetailForm.controls['caseReference'].setValue('1111-1111-1111-1111');
+    component.feeDetailForm.controls['selectedFee'].setValue(true);
+    fixture.detectChanges();
+    component.saveAndContinue();
+    expect(service.selectedFee).toBe(fee);
   });
 
   it('Saving add fee detail should navigate to review fee detail page', () => {
     const fee: FeeModel = new FeeModel;
     fee.calculated_amount = 20.00;
+    component.feeDetailForm.controls['caseReference'].setValue('1111-1111-1111-1111');
+    component.feeDetailForm.controls['selectedFee'].setValue(true);
     component.selectFee(fee);
     fixture.detectChanges();
     component.saveAndContinue();
