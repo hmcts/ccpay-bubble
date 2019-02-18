2f29b62c9712db93728f0c2f973074614658f000
James Nelson <Jamesnelson117@gmail.com>
2019-02-18 13:30:38 +0000

    PAY-1540 - Added 500 server error component

---
+++ src/app/shared/components/service-failure/service-failure.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-service-failure',
+  templateUrl: './service-failure.component.html',
+  styleUrls: ['./service-failure.component.scss']
+})
+export class ServiceFailureComponent {}
