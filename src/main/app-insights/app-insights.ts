3367fc220517a285a813b3aebe393260d589e32a
Sateesh Mathangi <a-smathangi@hotels.com>
2019-04-04 16:46:13 +0100

    shared app insights

--- src/main/app-insights/app-insights.ts
+++ src/main/app-insights/app-insights.ts
@@ -3,6 +3,7 @@ import * as appInsights from 'applicationinsights';
 
 export class AppInsights {
   static enable () {
+    appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = config.get<string>('appInsights.roleName')
     appInsights.setup(config.get<string>('appInsights.instrumentationKey'))
       .setAutoDependencyCorrelation(true)
       .setAutoCollectConsole(true, true)
