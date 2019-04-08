70fab784f82f2de217d2f54f95bfee28254a2ab0
James Nelson <Jamesnelson117@gmail.com>
2019-04-08 15:34:49 +0100

    PAY-1698: Added view payment interfaces

---
+++ projects/view-payment/src/lib/interfaces/IFee.ts
@@ -0,0 +1,11 @@
+export interface IFee {
+  calculated_amount: number;
+  ccd_case_number: string;
+  code: string;
+  memo_line: string;
+  natural_account_code: string;
+  reference: string;
+  remission_reference: string;
+  version: string;
+  volume: number;
+}
