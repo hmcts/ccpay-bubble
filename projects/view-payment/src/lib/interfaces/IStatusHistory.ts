ecfefd9e0b5df3f9823ea02dc2b4b70dd74942f6
James Nelson <Jamesnelson117@gmail.com>
2019-04-08 15:34:49 +0100

    PAY-1698: Added view payment interfaces

---
+++ projects/view-payment/src/lib/interfaces/IStatusHistory.ts
@@ -0,0 +1,8 @@
+export interface IStatusHistory {
+  date_created: string;
+  date_updated: string;
+  error_code: string;
+  error_message: string;
+  external_status: string;
+  status: string;
+}
