72184a8eb28b3ca28944005ae6386c3a0900a803
James Nelson <Jamesnelson117@gmail.com>
2019-04-08 14:20:55 +0100

    PAY-1678: Created the view payment library component

---
+++ projects/view-payment/src/lib/view-payment.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ViewPaymentService } from './view-payment.service';
+
+describe('ViewPaymentService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: ViewPaymentService = TestBed.get(ViewPaymentService);
+    expect(service).toBeTruthy();
+  });
+});
