c9a326f3d782875d1aca25cc99bc1350077e75f5
James Nelson <Jamesnelson117@gmail.com>
2019-04-08 13:53:05 +0100

    PAY-1663: Cleaned up logging data in API

--- express/mvc/controller/PayhubController.js
+++ express/mvc/controller/PayhubController.js
@@ -14,7 +14,7 @@ class PayhubController {
           request({ uri: result._links.next_url.href },
             (error, response, body) => {
               if (error) {
-                return res.status(500).json({ err: `${error}`, success: false, requestedUrl: result._links.next_url });
+                return res.status(500).json({ err: `${error}`, success: false });
               }
               return res.status(200).send(body);
             });
