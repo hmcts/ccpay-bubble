658a757d34ed53ee19e96d107cafa33e82e0231d
James Nelson <Jamesnelson117@gmail.com>
2019-04-10 14:58:55 +0100

    PAY-1698: Added single payment view web component

--- projects/view-payment/src/lib/view-payment.component.ts
+++ projects/view-payment/src/lib/view-payment.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { ViewPaymentService } from './view-payment.service';
+import { IPayment } from './interfaces';
 
 @Component({
-  selector: 'lib-view-payment',
-  template: `
-    <p>
-      view-payment works!
-    </p>
-  `,
-  styles: []
+  selector: 'ccpay-view-payment',
+  templateUrl: './view-payment.component.html'
 })
 export class ViewPaymentComponent implements OnInit {
+  @Input() paymentReference: string;
+  payment?: IPayment;
+  errorMessage?: string;
 
-  constructor() { }
+  constructor(
+    private viewPaymentService: ViewPaymentService
+  ) { }
 
   ngOnInit() {
+    this.viewPaymentService.getPaymentDetail(this.paymentReference).subscribe(
+      (payment: IPayment) => this.payment = payment,
+      (err: string) => this.errorMessage = err
+    );
   }
-
 }
--- projects/view-payment/src/lib/view-payment.service.spec.ts
+++ projects/view-payment/src/lib/view-payment.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ViewPaymentService } from './view-payment.service';
 
 describe('ViewPaymentService', () => {
