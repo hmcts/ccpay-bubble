21827445a4935e8e32b7035ff610b4f8a8193223
James Nelson <Jamesnelson117@gmail.com>
2019-04-11 10:32:00 +0100

    PAY-1698: IPayment properties are now optional

--- projects/view-payment/src/lib/interfaces/IPayment.ts
+++ projects/view-payment/src/lib/interfaces/IPayment.ts
@@ -1,23 +1,23 @@
 import { IFee, IStatusHistory } from './';
 
 export interface IPayment {
-  amount: number;
-  date_created: string;
-  date_updated: string;
-  currency: string;
-  ccd_case_number: string;
-  case_reference: string;
-  payment_reference: string;
-  channel: string;
-  method: string;
-  site_id: string;
-  status: string;
-  service_name: string;
-  customer_reference: string;
-  account_number: string;
-  organisation_name: string;
-  payment_group_reference: string;
-  description: string;
-  fees: IFee[];
-  status_histories: IStatusHistory[];
+  amount?: number;
+  date_created?: string;
+  date_updated?: string;
+  currency?: string;
+  ccd_case_number?: string;
+  case_reference?: string;
+  payment_reference?: string;
+  channel?: string;
+  method?: string;
+  site_id?: string;
+  status?: string;
+  service_name?: string;
+  customer_reference?: string;
+  account_number?: string;
+  organisation_name?: string;
+  payment_group_reference?: string;
+  description?: string;
+  fees?: IFee[];
+  status_histories?: IStatusHistory[];
 }
