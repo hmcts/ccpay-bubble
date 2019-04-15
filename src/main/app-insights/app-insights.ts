5950ed730f38ef76be21fd8d9449bc2c633bb75d
Sateesh Mathangi <a-smathangi@hotels.com>
2019-04-15 10:54:25 +0100

    reverted helm preview and minor cleanup

--- src/main/app-insights/app-insights.ts
+++ src/main/app-insights/app-insights.ts
@@ -3,10 +3,10 @@ import * as appInsights from 'applicationinsights';
 
 export class AppInsights {
   static enable () {
-    appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = config.get<string>('appInsights.roleName');
     appInsights.setup(config.get<string>('appInsights.instrumentationKey'))
       .setAutoDependencyCorrelation(true)
-      .setAutoCollectConsole(true, true)
-      .start();
+      .setAutoCollectConsole(true, true);
+    appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = config.get<string>('appInsights.roleName');
+    appInsights.start();
   }
 }
