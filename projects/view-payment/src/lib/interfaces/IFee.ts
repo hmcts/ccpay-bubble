6e52e260d88dc095f8a430853099cce616370bb3
James Nelson <Jamesnelson117@gmail.com>
2019-04-17 10:48:55 +0100

    PAY-1698: Minor interface updates to support optional params

--- projects/view-payment/src/lib/interfaces/IFee.ts
+++ projects/view-payment/src/lib/interfaces/IFee.ts
@@ -1,8 +1,8 @@
 export interface IFee {
   code: string;
   version: string;
-  volume_amount: number;
-  calculated_amount: number;
-  memo_line: string;
-  natural_account_code: string;
+  volume_amount?: number;
+  calculated_amount?: number;
+  memo_line?: string;
+  natural_account_code?: string;
 }
