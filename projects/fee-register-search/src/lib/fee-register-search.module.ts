737379bcfea61fa38f9a81817c55be19b20dd325
James Nelson <Jamesnelson117@gmail.com>
2019-04-17 10:28:38 +0100

    Updates to support fee searching

--- projects/fee-register-search/src/lib/fee-register-search.module.ts
+++ projects/fee-register-search/src/lib/fee-register-search.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FeeRegisterSearchComponent } from './fee-register-search.component';
 import { FeeRegisterSearchService } from './fee-register-search.service';
 import { FeeListComponent } from './components/fee-list/fee-list.component';
 import { FeeSearchComponent } from './components/fee-search/fee-search.component';
+import { FilterFeesPipe } from './pipes/filter-fees.pipe';
 
 @NgModule({
   declarations: [
     FeeRegisterSearchComponent,
     FeeListComponent,
-    FeeSearchComponent
+    FeeSearchComponent,
+    FilterFeesPipe
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [FeeRegisterSearchService],
   exports: [FeeRegisterSearchComponent]
