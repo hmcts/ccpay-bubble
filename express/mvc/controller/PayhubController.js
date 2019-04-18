1012f14d4312309ca712e1e8de35dc9796b20eaf
James Nelson <Jamesnelson117@gmail.com>
2019-04-18 13:38:11 +0100

    PAY-1665: Reverted request header changes

--- express/mvc/controller/PayhubController.js
+++ express/mvc/controller/PayhubController.js
@@ -6,26 +6,18 @@ class PayhubController {
     this.payhubService = payhubService;
   }
 
-  async sendToPayhub(req, res, appInsights) {
-    const serviceAuthToken = await this.payhubService.createAuthToken();
+  sendToPayhub(req, res, appInsights) {
     return this.payhubService.sendToPayhub(req, res, appInsights)
       // eslint-disable-next-line
       .then(result => {
         if (result._links.next_url) {
-          request({
-            method: 'GET',
-            uri: result._links.next_url.href,
-            headers: {
-              Authorization: `Bearer ${req.authToken}`,
-              ServiceAuthorization: `Bearer ${serviceAuthToken}`
-            }
-          },
-          (error, response, body) => {
-            if (error) {
-              return res.status(500).json({ err: `${error}`, success: false });
-            }
-            return res.status(200).send(body);
-          });
+          request({ uri: result._links.next_url.href },
+            (error, response, body) => {
+              if (error) {
+                return res.status(500).json({ err: `${error}`, success: false });
+              }
+              return res.status(200).send(body);
+            });
         } else {
           const error = `Invalid json received from Payment Hub: ${JSON.stringify(result)}`;
           return res.status(500).json({ err: `${error}`, success: false });
