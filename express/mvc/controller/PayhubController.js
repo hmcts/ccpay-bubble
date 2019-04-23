a1a25090eeb76af29c6bfc22652b4d2a13b35ec0
Sateesh Mathangi <Sateesh.Mathangi@kcom.com>
2019-04-23 13:58:43 +0100

    fixed log statement

--- express/mvc/controller/PayhubController.js
+++ express/mvc/controller/PayhubController.js
@@ -21,8 +21,9 @@ class PayhubController {
             if (error) {
               return res.status(500).json({ err: `${error}`, success: false });
             }
-            console.log('pci-pal-response status:' + response.code);
+            console.log('pci-pal-response status:' + response.statusCode);
             console.log('pci-pal-response body:' + body);
+            console.log('pci-pal-response:' + response);
             return res.status(200).send(body);
           });
         } else {
