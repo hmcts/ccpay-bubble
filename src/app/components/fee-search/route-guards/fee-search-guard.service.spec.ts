0121e7db141f84ab79c47df34e3153726e723240
Elviani Huseyin <elviani_s@hotmail.com>
2019-05-07 11:56:12 +0100

    PAY-1734 - adding link to fee search for only .internal url and only enable the page for .internal url

---
+++ src/app/components/fee-search/route-guards/fee-search-guard.service.spec.ts
@@ -0,0 +1,25 @@
+import { instance, mock } from 'ts-mockito/lib/ts-mockito';
+import { ConfirmationGuard } from 'src/app/components/confirmation/route-guards/confirmation-guard.service';
+import { FeeSearchGuard } from './fee-search-guard.service';
+import { WindowUtil } from 'src/app/services/window-util/window-util';
+
+describe('Fee Search route guard', () => {
+  let feeSearchGuard: FeeSearchGuard;
+  let windowUtil: WindowUtil;
+
+  beforeEach(() => {
+    windowUtil = new WindowUtil();
+    feeSearchGuard = new FeeSearchGuard(windowUtil);
+  });
+
+  it('should activate fee search page with window href .internal', () => {
+    windowUtil.setWindowHref('www.testwith.internal');
+    expect(feeSearchGuard.canActivate()).toBeTruthy();
+  });
+
+  it('should not activate fee search page with window href without .internal', () => {
+    windowUtil.setWindowHref('www.testwith.com');
+    expect(feeSearchGuard.canActivate()).toBeFalsy();
+  });
+
+});
