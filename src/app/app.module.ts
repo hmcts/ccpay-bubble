9febd433b0b18a929d4fa2cc09410d60cef03840
Elviani Huseyin <elviani_s@hotmail.com>
2019-05-07 11:56:12 +0100

    PAY-1734 - adding link to fee search for only .internal url and only enable the page for .internal url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { HeaderComponent } from './shared/components/header/header.component';
 import { SanitizeHtmlPipe } from 'src/app/shared/pipes/sanitize-html.pipe';
 import { ViewPaymentModule } from 'view-payment';
 import { FeeRegisterSearchModule } from 'fee-register-search';
+import { WindowUtil } from './services/window-util/window-util';
 
 @NgModule({
   declarations: [
@@ -49,7 +50,8 @@ import { FeeRegisterSearchModule } from 'fee-register-search';
   ],
   providers: [
     PaybubbleHttpClient,
-    AddFeeDetailService
+    AddFeeDetailService,
+    WindowUtil
   ],
   bootstrap: [AppComponent]
 })
