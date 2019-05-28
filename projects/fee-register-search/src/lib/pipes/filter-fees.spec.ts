cafc26568ca136d06df5667164f14a75ecc383be
Elviani Huseyin <elviani_s@hotmail.com>
2019-05-28 16:24:30 +0100

    Adding filter by number, when it is number, sort by amount and description

--- projects/fee-register-search/src/lib/pipes/filter-fees.spec.ts
+++ projects/fee-register-search/src/lib/pipes/filter-fees.spec.ts
@@ -24,6 +24,12 @@ describe('Filter fees pipe', () => {
     expect(results[0]).toEqual(mockFees[0]);
   });
 
+  it('Should filter an array of fees on amount and description when it is a number also sort by amount first', () => {
+    const results = filterFeesPipe.filterByNumber(mockFees, '500');
+    expect(results.length).toBe(2);
+    expect(results[0]).toEqual(mockFees[1]);
+  });
+
   it('Should filter an array of fees on jurisdiction tribunal to be empty', () => {
     const results = filterFeesPipe.filterByJurisdictions(mockFees, ['tribunal']);
     expect(results.length).toBe(0);
