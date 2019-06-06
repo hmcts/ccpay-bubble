111f5e3bdcb60d13acf15f3d1f0271eaed2f31f9
Elviani Huseyin <elviani_s@hotmail.com>
2019-06-06 16:34:39 +0100

    adding test

--- projects/fee-register-search/src/lib/components/fee-filter/fee-filter.component.spec.ts
+++ projects/fee-register-search/src/lib/components/fee-filter/fee-filter.component.spec.ts
@@ -2,9 +2,11 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TestBed, ComponentFixture} from '@angular/core/testing';
 import { FeeFilterComponent } from './fee-filter.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, CheckboxRequiredValidator } from '@angular/forms';
 import { FeeRegisterSearchService } from '../../fee-register-search.service';
 import { HttpClientModule } from '@angular/common/http';
+import { Jurisdictions } from '../../models/Jurisdictions';
+import { MockFeeRegisterSearchService } from '../../mock-fee-register-search.service';
 
 describe('Fee Filter component', () => {
   let component: FeeFilterComponent,
@@ -22,7 +24,9 @@ describe('Fee Filter component', () => {
         HttpClientModule
       ],
       schemas: [NO_ERRORS_SCHEMA],
-      providers: [FeeRegisterSearchService]
+      providers: [
+        { provide: FeeRegisterSearchService, useClass: MockFeeRegisterSearchService }
+      ]
     });
 
     fixture = TestBed.createComponent(FeeFilterComponent);
@@ -33,13 +37,24 @@ describe('Fee Filter component', () => {
     expect(component).toBeTruthy();
   });
 
-  // it('Should emit correct jurisdiction array', () => {
-  //   component.ngOnInit();
-  //   component.filterForm.get('civil').setValue(true);
-  //   component.applyFilter();
-  //   fixture.detectChanges();
-  //   component.jurisdictionsFilterEvent.subscribe(jurisdiction => {
-  //     expect(jurisdiction).toEqual(['civil']);
-  //   });
-  // });
+  it('Should emit correct jurisdiction array', () => {
+    component.ngOnInit();
+    component.filterForm.get('jurisdiction1').setValue('civil');
+    component.filterForm.get('jurisdiction2').setValue('family court');
+    component.applyFilter();
+    fixture.detectChanges();
+    const emitJurisdiction = new Jurisdictions();
+    emitJurisdiction.jurisdiction1 = 'civil';
+    emitJurisdiction.jurisdiction2 = 'family court';
+    component.jurisdictionsFilterEvent.subscribe(jurisdiction => {
+      expect(jurisdiction).toEqual(emitJurisdiction);
+    });
+  });
+
+  it('Should toggle correct jurisdiction', () => {
+    component.ngOnInit();
+    component.jurisdictionData.jurisdiction1.show = true;
+    component.toggleJurisdiction(component.jurisdictionData.jurisdiction1);
+    expect(component.jurisdictionData.jurisdiction1.show).toBeFalsy();
+  });
 });
