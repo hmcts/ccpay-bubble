4e2d00d75ab831e0187e50e26c843ee9f3a8225c
tarun-palisetty <tarun.mca@gmail.com>
2019-06-11 18:31:34 +0100

    Payment webcomponent integration test

--- src/app/components/payment-history/payment-history.component.spec.ts
+++ src/app/components/payment-history/payment-history.component.spec.ts
@@ -1,14 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PaymentHistoryComponent } from './payment-history.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import {Subject} from "rxjs";
 
 describe('PaymentHistoryComponent', () => {
   let component: PaymentHistoryComponent;
   let fixture: ComponentFixture<PaymentHistoryComponent>;
+  let activatedRoute: ActivatedRoute;
+  let params: Subject<Params>;
 
   beforeEach(async(() => {
+    params = new Subject<Params>();
     TestBed.configureTestingModule({
-      declarations: [ PaymentHistoryComponent ]
+      declarations: [ PaymentHistoryComponent ],
+      providers: [
+        { provide: ActivatedRoute, useValue: { params: params } }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
@@ -16,10 +26,15 @@ describe('PaymentHistoryComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PaymentHistoryComponent);
     component = fixture.componentInstance;
+    activatedRoute = TestBed.get(ActivatedRoute);
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    component.ccdCaseNumber = '1111-2222-3333-4444';
+    component.apiRoot = 'api/payment-history';
+    fixture.detectChanges();
+    expect(component.apiRoot).toBe('api/payment-history');
+    expect(component.ccdCaseNumber).toBe('1111-2222-3333-4444');
   });
 });
