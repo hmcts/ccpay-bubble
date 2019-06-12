20d3c599916c2c7c4fb78bcd7d7ea6c912f163ad
Elviani Huseyin <elviani_s@hotmail.com>
2019-06-12 13:53:48 +0100

    fixing the linting errors

--- express/services/FeeService.js
+++ express/services/FeeService.js
@@ -6,19 +6,16 @@ const feeJurisdictionUrl = config.get('fee.feeJurisdictionUrl');
 
 class FeeService {
   getFees() {
-    return request.get({ uri: feeRegistrationUrl,
-      headers: {
-        'Cache-Control':'public, max-age=31557600'
-      }
+    return request.get({
+      uri: feeRegistrationUrl,
+      headers: { 'Cache-Control': 'public, max-age=31557600' }
     });
   }
 
   getJurisdictions(req) {
     return request.get({
       uri: `${feeJurisdictionUrl}${req.params.id}`,
-      headers: {
-       'Cache-Control':'public, max-age=31557600'
-      }
+      headers: { 'Cache-Control': 'public, max-age=31557600' }
     });
   }
 }
