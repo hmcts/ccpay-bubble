68280887e3752e0d4bd188a6129524fc4742f08d
mswirski <matiswir@gmail.com>
2019-07-18 14:50:19 +0100

    routing to fee-summary page, added tests

--- projects/fee-register-search/src/lib/mocks/mock-payment-group-service.ts
+++ projects/fee-register-search/src/lib/mocks/mock-payment-group-service.ts
@@ -2,12 +2,11 @@ import {defer} from 'rxjs';
 import {FeeModel} from '../../../../../src/app/models/FeeModel';
 import {mockPaymentGroup} from './mock-payment-group';
 
-export function fakeAsyncResponse<T>(data: T) {
-  return defer(() => Promise.resolve(data));
-}
 
 export class MockPaymentGroupService {
   postPaymentGroup(fee: FeeModel) {
-    return fakeAsyncResponse(mockPaymentGroup).toPromise();
+    return {
+      then: (fun) => fun({payment_group_reference: '2019-12341234'})
+    };
   }
 }
