2d38133c0a3669faf5a084e9c49085bd3d6854f7
mswirski <matiswir@gmail.com>
2019-07-18 16:44:07 +0100

    WIP

--- src/app/components/fee-search/fee-search.component.spec.ts
+++ src/app/components/fee-search/fee-search.component.spec.ts
@@ -2,47 +2,70 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {FeeSearchComponent} from './fee-search.component';
 import {NO_ERRORS_SCHEMA} from '@angular/core';
 import {PaymentGroupService} from '../../services/payment-group/payment-group.service';
-import {MockPaymentGroupService} from '../../../../projects/fee-register-search/src/lib/mocks/mock-payment-group-service';
 import {ActivatedRoute, Router} from '@angular/router';
-import {MockActivatedRouteService} from '../../../../projects/fee-register-search/src/lib/mocks/mock-activated-route-service';
-import {MockRouterService} from '../../../../projects/fee-register-search/src/lib/mocks/mock-router-service';
+import {PaybubbleHttpClient} from '../../services/httpclient/paybubble.http.client';
+import {instance, mock} from 'ts-mockito';
+import {HttpClient} from '@angular/common/http';
+import {Meta} from '@angular/platform-browser';
 
-describe('Fee search component', () => {
+fdescribe('Fee search component', () => {
   let component: FeeSearchComponent,
     fixture: ComponentFixture<FeeSearchComponent>,
+    paymentGroupService: PaymentGroupService,
+    routerService: any,
+    activatedRoute: any,
     router: Router;
 
   beforeEach(() => {
+    activatedRoute = {
+      params: {
+        subscribe: (fun) => fun()
+      },
+      snapshot: {
+        queryParams: {
+          'ccdCaseNumber': '1234-1234-1234-1234'
+        }
+      }
+    };
+    routerService = {
+      navigateByUrl: () => true
+    };
     TestBed.configureTestingModule({
       declarations: [FeeSearchComponent],
       providers: [
-        {provide: ActivatedRoute, useClass: MockActivatedRouteService},
-        {provide: Router, useClass: MockRouterService},
-        {provide: PaymentGroupService, useClass: MockPaymentGroupService}
+        {provide: ActivatedRoute, useValue: activatedRoute},
+        {provide: Router, useValue: routerService},
+        {
+          provide: PaymentGroupService,
+          useValue: new PaymentGroupService(new PaybubbleHttpClient(instance(mock(HttpClient)), instance(mock(Meta))))
+        }
       ],
       schemas: [NO_ERRORS_SCHEMA]
     });
 
     fixture = TestBed.createComponent(FeeSearchComponent);
+    paymentGroupService = fixture.debugElement.injector.get(PaymentGroupService);
+    spyOn(paymentGroupService, 'postPaymentGroup').and.returnValue({then: (fun) => fun({payment_group_reference: '2019-12341234'})});
     router = TestBed.get(Router);
     component = fixture.componentInstance;
+    component.ngOnInit();
   });
 
   it('Should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('Should assign selected fee', () => {
+  it('Should pass selected fee into POST call for backend', () => {
     component.selectFee('test');
     fixture.detectChanges();
-    expect(component.selectedFee).toBe('test');
+    expect(paymentGroupService.postPaymentGroup).toHaveBeenCalledWith('test');
   });
 
   it('Should set ccd number from URL', async(async () => {
     expect(component.ccdNo).toBe('1234-1234-1234-1234');
   }));
 
-  it('Should set ccd number from URL', async(async () => {
+  it('Should navigate to fee-summary page using correct CCD case number and payment group reference', async(async () => {
     spyOn(router, 'navigateByUrl');
     component.selectFee({});
     await fixture.whenStable();
