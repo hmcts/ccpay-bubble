3ec723e88a18fd77c5abf3ce70c3a19885484680
tarun-palisetty <tarun.mca@gmail.com>
2019-07-20 02:46:16 +0100

    Fixed test

--- src/app/components/payment-history/payment-history.component.spec.ts
+++ src/app/components/payment-history/payment-history.component.spec.ts
@@ -8,23 +8,19 @@ describe('Payment History component', () => {
   let component: PaymentHistoryComponent,
   activatedRoute: ActivatedRoute,
   params: Subject<Params>,
+  queryParams: Subject<Params>,
   fixture: ComponentFixture<PaymentHistoryComponent>;
 
   beforeEach(() => {
     params = new Subject<Params>();
+    queryParams = new Subject<Params>();
     TestBed.configureTestingModule({
       declarations: [PaymentHistoryComponent],
       providers: [
         {
           provide: ActivatedRoute, useValue: {
-            params: of({ccdCaseNumber: '1111-2222-3333-4444'}),
-            snapshot: {
-              parent: {
-                params: {
-                  view: 'case-transactions'
-                }
-              }
-            }
+            params: params,
+            snapshot: { queryParams: queryParams }
           }
         }],
       schemas: [NO_ERRORS_SCHEMA]
@@ -36,17 +32,21 @@ describe('Payment History component', () => {
     component = fixture.componentInstance;
   });
 
-  it('Should create', () => {
-    params.next({ ccdCaseNumber: '1111-2222-3333-4444' });
+  it('should create', function () {
+    expect(component).toBeTruthy();
+  });
+
+  it('Should get...', () => {
+    params.next({ 'ccdCaseNumber': '1111-2222-3333-4444' });
+    queryParams.next( { 'view': 'case-transactions' });
 
     activatedRoute.params.subscribe((p) => {
       component.ccdCaseNumber = p['ccdCaseNumber'];
-      component.view = p['view'];
+      component.view = activatedRoute.snapshot.queryParams['view'];
       component.apiRoot = 'api/payment-history';
       fixture.detectChanges();
       expect(component.apiRoot).toBe('api/payment-history');
       expect(component.ccdCaseNumber).toBe('1111-2222-3333-4444');
     });
-    expect(component).toBeTruthy();
   });
 });
