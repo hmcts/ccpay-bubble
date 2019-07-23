78570dd922948e4e776fb18e67a6ff88074d91d3
mswirski <matiswir@gmail.com>
2019-07-23 14:21:37 +0100

    test fix

--- src/app/services/payment-group/payment-group.service.ts
+++ src/app/services/payment-group/payment-group.service.ts
@@ -1,18 +1,18 @@
 import {Injectable} from '@angular/core';
 import {IPaymentGroup} from '@hmcts/ccpay-web-component/lib/interfaces/IPaymentGroup';
-import {HttpClient} from '@angular/common/http';
+import {PaybubbleHttpClient} from '../httpclient/paybubble.http.client';
 
 @Injectable()
 export class PaymentGroupService {
 
   constructor(
-    private http: HttpClient
+    private http: PaybubbleHttpClient
   ) {
   }
 
   postPaymentGroup(paymentGroup: IPaymentGroup): Promise<IPaymentGroup> {
     return this.http.post('api/payment-groups', paymentGroup).toPromise().then(paymentGroupJson => {
-      return <IPaymentGroup> paymentGroupJson;
+      return <IPaymentGroup>paymentGroupJson;
     });
   }
 }
