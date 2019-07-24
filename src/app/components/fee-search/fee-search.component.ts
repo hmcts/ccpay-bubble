958a3ee0701f78e40f77159381916289c68ccfd3
kumarmadurai <kumarmcamadurai@gmail.com>
2019-07-25 00:10:28 +0100

    Final fixes for PAY-1879

--- src/app/components/fee-search/fee-search.component.ts
+++ src/app/components/fee-search/fee-search.component.ts
@@ -41,7 +41,6 @@ export class FeeSearchComponent implements OnInit {
       }]
     };
     this.paymentGroupService.postPaymentGroup(paymentGroup).then(paymentGroupReceived => {
-      const response: any = paymentGroupReceived;
       this
         .router
         .navigateByUrl(`/payment-history/${this.ccdNo}`
