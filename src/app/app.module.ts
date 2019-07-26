d852b92a73fa55b09caf5dd38f5ec4c7e8a8641b
kumarmadurai <kumarmcamadurai@gmail.com>
2019-07-26 13:32:35 +0100

    Merge confilct issue fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,7 +26,6 @@ import {PaymentHistoryComponent} from './components/payment-history/payment-hist
 import {PaymentLibModule} from '@hmcts/ccpay-web-component';
 import {WindowUtil} from './services/window-util/window-util';
 import {CcdSearchComponent} from './components/ccd-search/ccd-search.component';
-import {FeesSummaryComponent} from './components/fees-summary/fees-summary.component';
 import {AuthDevInterceptor} from './shared/interceptors/auth.dev.interceptor';
 import {environment} from '../environments/environment';
 
@@ -52,8 +51,7 @@ const nonProductionProviders = [{
     NavigationComponent,
     PaymentHistoryComponent,
     FeeSearchComponent,
-    CcdSearchComponent,
-    FeesSummaryComponent
+    CcdSearchComponent
   ],
   imports: [
     BrowserModule,
