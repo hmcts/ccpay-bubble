0b18de8d8025dd9934b66a6c1f90c36bdbb292fe
Elviani Huseyin <elviani_s@hotmail.com>
2019-08-01 11:19:55 +0100

    adding some test

--- src/app/components/fee-search/fee-search.component.spec.ts
+++ src/app/components/fee-search/fee-search.component.spec.ts
@@ -4,10 +4,9 @@ import {NO_ERRORS_SCHEMA} from '@angular/core';
 import {PaymentGroupService} from '../../services/payment-group/payment-group.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {PaybubbleHttpClient} from '../../services/httpclient/paybubble.http.client';
-import {instance, mock} from 'ts-mockito';
+import {instance, mock, anyFunction} from 'ts-mockito';
 import {HttpClient} from '@angular/common/http';
 import {Meta} from '@angular/platform-browser';
-import {IFee} from '@hmcts/ccpay-web-component/lib/interfaces/IFee';
 
 describe('Fee search component', () => {
   let component: FeeSearchComponent,
@@ -16,7 +15,8 @@ describe('Fee search component', () => {
     routerService: any,
     activatedRoute: any,
     router: Router,
-    testFee: any;
+    testFee: any,
+    testVolumeFee: any;
 
   beforeEach(() => {
     testFee = {
@@ -28,6 +28,24 @@ describe('Fee search component', () => {
         natural_account_code: '1234-1234-1234-1234',
         flat_amount: {
           amount: 1234
+        },
+        description: 'test-description'
+      },
+      ccdCaseNumber: '1111-2222-3333-4444',
+      jurisdiction1: {name: 'test-jurisdiction1'},
+      jurisdiction2: {name: 'test-jurisdiction2'},
+    };
+
+    testVolumeFee = {
+      code: 'test-volume-fee',
+      fee_type: 'fixed',
+      'current_version': {
+        version: 1,
+        calculatedAmount: 1234,
+        memo_line: 'test-memoline',
+        natural_account_code: '1234-1234-1234-1234',
+        volume_amount: {
+          amount: 5
         },
         description: 'test-description'
       },
@@ -88,6 +106,34 @@ describe('Fee search component', () => {
         jurisdiction1: testFee.jurisdiction1.name,
         jurisdiction2: testFee.jurisdiction2.name,
         description: testFee.current_version.description
+      }]
+    });
+  });
+
+  it('Should go to volume page for volume fee ', () => {
+    component.selectFee(testVolumeFee);
+    fixture.detectChanges();
+    expect(component.preselectedFee).toBe(testVolumeFee);
+    expect(component.showFeeDetails).toBeTruthy();
+  });
+
+  it('Should create a correct params with volume for volume fee ', () => {
+    component.selectFee(testVolumeFee);
+    component.selectPreselectedFeeWithVolume(10);
+    fixture.detectChanges();
+    expect(paymentGroupService.postPaymentGroup).toHaveBeenCalledWith({
+      fees: [{
+        code: testVolumeFee.code,
+        version: testVolumeFee['current_version'].version.toString(),
+        'calculated_amount': (testVolumeFee['current_version'].volume_amount.amount * 10).toString(),
+        'memo_line': testVolumeFee['current_version'].memo_line,
+        'natural_account_code': testVolumeFee['current_version'].natural_account_code,
+        'ccd_case_number': component.ccdNo,
+        jurisdiction1: testVolumeFee.jurisdiction1.name,
+        jurisdiction2: testVolumeFee.jurisdiction2.name,
+        description: testVolumeFee.current_version.description,
+        volume: 10,
+        volume_amount: testVolumeFee.current_version.volume_amount.amount
       }]
     });
   });
