fb810229eb6257baa69ca85d1d49198747e18fde
kumarmadurai <kumarmcamadurai@gmail.com>
2019-08-21 16:01:29 +0100

    PAY-1987 api integration changes

--- express/services/PayhubService.js
+++ express/services/PayhubService.js
@@ -103,6 +103,19 @@ class PayhubService {
     }));
   }
 
+  postAllocatePayment(req) {
+    return this.createAuthToken().then(token => request.post({
+      uri: `${payhubUrl}/payment-groups/${req.params.paymentGroup}/bulk-scan-payments`,
+      body: req.body,
+      headers: {
+        Authorization: `Bearer ${req.authToken}`,
+        ServiceAuthorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      },
+      json: true
+    }));
+  }
+
   getPayment(req) {
     return this.createAuthToken().then(token => request.get({
       uri: `${payhubUrl}/payments/${req.params.id}`,
