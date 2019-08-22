18e64cc521fcb3b078c37a99eeb7eefe4958c382
kumarmadurai <kumarmcamadurai@gmail.com>
2019-08-22 16:51:57 +0100

    Express intgeration with bulk scan servies

---
+++ express/services/BulkScanService.js
@@ -0,0 +1,36 @@
+const config = require('config');
+const otp = require('otp');
+const request = require('request-promise-native');
+
+const bulkScanUrl = config.get('bulkscan.url');
+const ccpayBubbleReturnUrl = config.get('ccpaybubble.url');
+const s2sUrl = config.get('s2s.url');
+const ccpayBubbleSecret = config.get('s2s.key');
+const microService = config.get('ccpaybubble.microservice');
+
+class BulkScanService {
+  getPaymentDetailsForDcn(req) {
+    return this.createAuthToken().then(token => request.get({
+      uri: `${bulkScanUrl}/cases?document_control_number=${req.params.id}`,
+      headers: {
+        Authorization: `Bearer ${req.authToken}`,
+        ServiceAuthorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      },
+      json: true
+    }));
+  }
+  getPaymentDetailsForCcd(req) {
+    return this.createAuthToken().then(token => request.get({
+      uri: `${bulkScanUrl}/cases/${req.params.id}`,
+      headers: {
+        Authorization: `Bearer ${req.authToken}`,
+        ServiceAuthorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      },
+      json: true
+    }));
+  }
+}
+
+module.exports = BulkScanService;
