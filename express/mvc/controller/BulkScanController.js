b5f9380d3ded547b97cea54ccc3a09cb3f1b39f8
Santu412 <santu412@gmail.com>
2019-09-02 12:28:01 +0100

    PAY-2026 ccd or dcn search api integration changes

--- express/mvc/controller/BulkScanController.js
+++ express/mvc/controller/BulkScanController.js
@@ -10,8 +10,8 @@ class BulkScanController {
   getPaymentDetailsForDcn(req, res) {
     return this.bulkScanService.getPaymentDetailsForDcn(req)
       .then(result => {
-        res.status(200).json({ data: result, success: true });
-      })
+       res.status(200).json({ data: result, success: true });
+         })
       .catch(error => {
         res.status(500).json({ err: error, success: false });
       });
