a9ad606f598752b33e7ceec04fbb0fc397c1e75c
Santu412 <santu412@gmail.com>
2019-09-02 12:28:01 +0100

    PAY-2026 ccd or dcn search api integration changes

--- src/app/components/payment-history/payment-history.component.ts
+++ src/app/components/payment-history/payment-history.component.ts
@@ -9,22 +9,26 @@ import { PlatformLocation } from '@angular/common';
 })
 export class PaymentHistoryComponent implements OnInit {
   apiRoot: string;
+  bulkscanapiRoot: string;
   view: string;
   takePayment: boolean;
   ccdCaseNumber: string;
   paymentGroupRef: string;
   dcnNumber: string;
+  selectedOption: string;
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    this.activatedRoute.params.subscribe((params) => {
+      this.activatedRoute.params.subscribe((params) => {
       this.apiRoot = 'api/payment-history';
+      this.bulkscanapiRoot = 'api/bulk-scan';
       this.ccdCaseNumber = params['ccdCaseNumber'];
       this.view = this.activatedRoute.snapshot.queryParams['view'];
-      this.dcnNumber = this.activatedRoute.snapshot.queryParams['dcn'];
       this.takePayment = this.activatedRoute.snapshot.queryParams['takePayment'];
       this.paymentGroupRef = this.activatedRoute.snapshot.queryParams['paymentGroupRef'];
+      this.dcnNumber = this.activatedRoute.snapshot.queryParams['dcn'];
+      this.selectedOption = this.activatedRoute.snapshot.queryParams['selectedOption'];
     });
   }
 }
