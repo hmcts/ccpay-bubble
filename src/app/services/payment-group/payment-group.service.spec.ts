86a3403e11e20a02475283ee7b7b220542f46e5e
kumarmadurai <kumarmcamadurai@gmail.com>
2019-09-12 21:49:27 +0100

    fee summary page partial payment changes

--- src/app/services/payment-group/payment-group.service.spec.ts
+++ src/app/services/payment-group/payment-group.service.spec.ts
@@ -60,4 +60,38 @@ describe('Payment group service', () => {
         expect(response.payment_group_reference).toBe(paymentGroup.payment_group_reference);
       });
   });
+
+    it('Should call get bulk scanning Payment details', () => {
+    const paymentGroup = <any>{
+        ccd_reference: '1111222233334444',
+        exception_record_reference: '1111222233334444',
+        payments: [
+          {
+            amount: 100,
+            bgc_reference: 'BGC1203',
+            case_reference: '1111222233334444',
+            currency: 'GBP',
+            date_banked: '2019-DEC-02',
+            date_created: '2019-DEC-19',
+            date_updated: '2019-DEC-30',
+            dcn_case: '11112222333344440',
+            dcn_reference: '11112222333344440',
+            first_cheque_dcn_in_batch: 'string',
+            outbound_batch_number: 'string',
+            payer_name: 'tester',
+            payment_method: 'CHEQUE',
+            po_box: 'string'
+          }
+        ],
+        responsible_service_id: 'AA07'
+    };
+    spyOn(http, 'get').and.callFake((param1: string) => of(paymentGroup));
+    paymentGroupService.getBSPaymentsByDCN('1234')
+      .then((response) => {
+        expect(response.ccd_reference).toBe(paymentGroup.ccd_reference);
+        expect(response.exception_record_reference).toBe(paymentGroup.exception_record_reference);
+      }).catch(() => {
+
+      });
+  });
 });
