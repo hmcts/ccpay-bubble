555b4b92842b6aed481ba8604b798845e039a700
Santosh Siva Kumar <490436@cognizant.com>
2019-09-18 15:23:03 +0100

    reports

--- src/app/components/payment-history/payment-history.component.ts
+++ src/app/components/payment-history/payment-history.component.ts
@@ -20,7 +20,6 @@ export class PaymentHistoryComponent implements OnInit {
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    debugger;
       this.activatedRoute.params.subscribe((params) => {
       this.apiRoot = 'api/payment-history';
       this.bulkscanapiRoot = 'api/bulk-scan';
