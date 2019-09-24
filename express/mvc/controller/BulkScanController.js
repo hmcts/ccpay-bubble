a056c833a43dfa7176a49cc46552c8222ab1c61c
Santosh Siva Kumar <490436@cognizant.com>
2019-09-24 16:47:41 +0100

    reports

--- express/mvc/controller/BulkScanController.js
+++ express/mvc/controller/BulkScanController.js
@@ -33,6 +33,15 @@ class BulkScanController {
         res.status(500).json({ err: error, success: false });
       });
   }
+  getSelectedReport(req, res) {
+    return this.bulkScanService.getSelectedReport(req)
+      .then(result => {
+        res.status(200).json({ data: result, success: true });
+      })
+      .catch(error => {
+        res.status(500).json({ err: error, success: false });
+      });
+  }
 }
 
 module.exports = BulkScanController;
