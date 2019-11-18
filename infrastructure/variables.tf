3dc84c86bf53fa27d5dbc8e5005d359ff20359e9
“Kumar <Kumarmcamadura@gmail.com>
2019-11-18 11:02:51 +0000

    fixing conflict fixes

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -64,7 +64,7 @@ variable "authentication_web_url" {
 }
 
 variable "ccd_api_url" {
-  default = "https://gateway-ccd.aat.platform.hmcts.net/aggregated"
+  default = "https://ccd-data-store-api-prod.service.core-compute-prod.internal"
 }
 
 variable "vault_section" {
