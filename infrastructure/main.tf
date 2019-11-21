f7d89f389daa036a2bfc9fc6a98f0c30d89cee28
amitchidrewar1301 <amit.chidrewar@hmcts.net>
2019-11-21 17:31:08 +0000

    PAY-2419:AKS Migration Payment App

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -59,7 +59,7 @@ module "ccpay-bubble" {
     IDAM_CLIENT_SECRET = "${data.azurerm_key_vault_secret.paybubble_idam_client_secret.value}"
     CCPAY_BUBBLE_URL = "https://ccpay-bubble-frontend-${var.env}.service.core-compute-${var.env}.internal/"
     CCPAY_BUBBLE_MICROSERVICE = "ccpay_bubble"
-    PAYHUB_API_URL = "http://payment-api-${var.env}.service.core-compute-${var.env}.internal/"
+    PAYHUB_API_URL = "https://payment-api-${var.env}.service.core-compute-${var.env}.internal/"
     FEE_REGISTRATION_URL = "http://fees-register-api-${local.local_env}.service.${local.local_ase}.internal/fees-register/fees"
     FEE_JURISDICTION_URL = "http://fees-register-api-${local.local_env}.service.${local.local_ase}.internal/jurisdictions"
 
