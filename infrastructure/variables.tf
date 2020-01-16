2a41b2a79ad2b97c8d38388f17b74e7c64a19982
“Kumar <Kumarmcamadura@gmail.com>
2020-01-16 11:56:30 +0000

    UAT issue fixes

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -64,7 +64,7 @@ variable "authentication_web_url" {
 }
 
 variable "ccd_api_url" {
-  default = "http://ccd-data-store-api-prod.service.core-compute-prod.internal"
+  default = "https://gateway-ccd.aat.platform.hmcts.net/aggregated"
 }
 
 variable "vault_section" {
