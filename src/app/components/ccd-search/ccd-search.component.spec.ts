bbf48f45ddbd568311baffb602b43d480ab3a368
Santosh Siva Kumar <490436@AMBGB000021.local>
2020-02-17 10:26:30 +0000

    PAY-2769

--- src/app/components/ccd-search/ccd-search.component.spec.ts
+++ src/app/components/ccd-search/ccd-search.component.spec.ts
@@ -209,7 +209,7 @@ describe('CCD search component with takePayment is equal to true', () => {
     await fixture.whenStable();
     expect(component.selectedValue).toBe('DCN');
     expect(component.dcnNumber).toBe('111122223333444401234');
-    expect(component.ccdCaseNumber).toBe('1111222233234444');
+    expect(component.ccdCaseNumber).toBe('');
   });
   it('Should get go to correct navigation', async () => {
     mockResponse['data'].ccd_reference = null;
@@ -228,7 +228,7 @@ describe('CCD search component with takePayment is equal to true', () => {
     expect(component.dcnNumber).toBe('111122223333444401234');
     expect(component.ccdCaseNumber).toBe('1111222233234444');
     // tslint:disable-next-line:max-line-length
-    expect(routerMock.navigateByUrl).toHaveBeenCalledWith('/payment-history/1111222233234444?selectedOption=DCN&dcn=111122223333444401234&view=case-transactions&takePayment=true&isBulkScanning=Enable');
+    expect(routerMock.navigateByUrl).toHaveBeenCalledWith('/payment-history/1111222233234444?selectedOption=DCN&exceptionRecord=1111222233234444&dcn=111122223333444401234&view=case-transactions&takePayment=true&isBulkScanning=Enable');
   });
 });
 
