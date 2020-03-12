d46b604590d78e9bef61e97d41f0f3a832916445
“Kumar <Kumarmcamadura@gmail.com>
2020-03-12 21:47:12 +0000

    OpenId changes

--- express/infrastructure/security.js
+++ express/infrastructure/security.js
@@ -101,7 +101,7 @@ function getTokenFromCode(self, req) {
 }
 
 function getUserDetails(self, securityCookie) {
-  return request.get(`${self.opts.apiUrl}/details`)
+  return request.get(`${self.opts.apiUrl}/o/userinfo`)
     .set('Accept', 'application/json')
     .set('Authorization', `Bearer ${securityCookie}`);
 }
