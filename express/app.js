f09516ec7edd0b8afd7c82cf5658f62684aafb0c
“Kumar <Kumarmcamadura@gmail.com>
2020-04-01 13:14:41 +0100

    Express setTimeout functionality

--- express/app.js
+++ express/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const controllers = require('./mvc/controller');
 const config = require('config');
 const HttpStatus = require('http-status-codes');
+
 const timeOutSecond = 10;
 
 module.exports = appInsights => express.Router()
