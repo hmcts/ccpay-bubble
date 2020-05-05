1362efe6700c8371f201f03635c5eaddb044092b
“Kumar <Kumarmcamadura@gmail.com>
2020-05-05 14:34:02 +0100

    Demo changes revert

--- express/infrastructure/security-factory.js
+++ express/infrastructure/security-factory.js
@@ -8,7 +8,6 @@ const security = appInsights =>
     loginUrl: config.idam.login_url,
     registrationUrl: config.idam.registration_url,
     apiUrl: config.idam.api_url,
-    webUrl: config.idam.web_url,
     ccpayBubbleUrl: config.ccpaybubble.url,
     redirectUri: '/oauth2/callback',
     appInsights
