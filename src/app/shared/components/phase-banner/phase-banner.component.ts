0a520533c7fe852be067fb356c145f4e8d32d32c
Santosh Siva Kumar <490436@ambgb000021.home>
2020-06-18 16:02:58 +0100

    PAY-3340

--- src/app/shared/components/phase-banner/phase-banner.component.ts
+++ src/app/shared/components/phase-banner/phase-banner.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, ViewEncapsulation, ViewChild } from '@angular/core';
-import { BulkScanService } from 'express/services/'
+import { BulkScanService } from 'express/services/';
 
 @Component({
   selector: 'app-phase-banner',
@@ -11,7 +11,7 @@ export class PhaseBannerComponent {
   @Input() type = 'beta';
 
   myFunction () {
-    let myInput = (document.getElementById('iFrameDrivenImageValue') as HTMLInputElement).value;
-    window.open('https://www.smartsurvey.co.uk/s/PayBubble/?pageurl='+myInput, '_blank');
+    const myInput = (document.getElementById('iFrameDrivenImageValue') as HTMLInputElement).value;
+    window.open('https://www.smartsurvey.co.uk/s/PayBubble/?pageurl=' + myInput, '_blank');
   }
 }
