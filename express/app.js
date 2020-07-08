bc734ab532dd8ead0867fcaf04acf963c19ce9b1
“Kumar <Kumarmcamadura@gmail.com>
2020-07-08 16:29:47 +0100

    Latest apportionment changes

--- express/app.js
+++ express/app.js
@@ -85,6 +85,10 @@ module.exports = appInsights => express.Router()
     controllers.payhubController.bulkScanToggleFeature(req, res);
   })
 
+  .get('/payment-history/fee-pay-apportion/:id', (req, res) => {
+    controllers.payhubController.getApportionPaymentGroup(req, res);
+  })
+
   .delete('/payment-history/fees/:id', (req, res) => {
     controllers.payhubController.deleteFeesFromPaymentGroup(req, res, appInsights);
   })
