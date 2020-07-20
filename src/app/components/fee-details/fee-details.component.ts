cb8be2aa9e5fc82d1f0cb06d2fd175d46e6433d7
amitchidrewar1301 <amit.chidrewar@hmcts.net>
2020-07-20 13:53:53 +0100

    PAY-3377 - Paybubble display changes for fees-details

--- src/app/components/fee-details/fee-details.component.ts
+++ src/app/components/fee-details/fee-details.component.ts
@@ -1,6 +1,7 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {IFee} from '../../../../projects/fee-register-search/src/lib/interfaces';
-import {FormBuilder, FormGroup, FormControl} from '@angular/forms';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { IFee } from '../../../../projects/fee-register-search/src/lib/interfaces';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-fee-details',
@@ -9,19 +10,40 @@ import {FormBuilder, FormGroup, FormControl} from '@angular/forms';
 })
 export class FeeDetailsComponent implements OnInit {
   @Input() fee = {
-                        code: 'test-code',
-                        fee_type: 'banded',
-                        'current_version': {
-                          version: 1,
-                          calculatedAmount: 1234,
-                          memo_line: 'test-memoline',
-                          natural_account_code: '1234-1234-1234-1234',
-                          flat_amount: {
-                            amount: 1234
-                          },
-                          description: 'test-description'
-                        }
-                  };
+    code: 'test-code',
+    fee_type: 'banded',
+    fee_versions: [
+      {
+        description: "Recovery order (section 50)",
+        status: "approved",
+        author: "126172",
+        approvedBy: "126175",
+        version: 1,
+        valid_from: "2014-04-21T00:00:00.000+0000",
+        valid_to: "2014-04-21T00:00:00.000+0000",
+        flat_amount: {
+          "amount": 215
+        },
+        memo_line: "RECEIPT OF FEES - Family misc private",
+        statutory_instrument: "2014 No 877 ",
+        si_ref_id: "2.1q",
+        natural_account_code: "4481102174",
+        fee_order_name: "Family Proceedings",
+        direction: "cost recovery"
+      }
+    ],
+    'current_version': {
+      version: 1,
+      calculatedAmount: 1234,
+      memo_line: 'test-memoline',
+      natural_account_code: '1234-1234-1234-1234',
+      flat_amount: {
+        amount: 1234
+      },
+      description: 'test-description'
+    }
+
+  };
   @Output() submitFeeVolumeEvent: EventEmitter<IFee> = new EventEmitter();
   @Output() restartSearchEvent: EventEmitter<IFee> = new EventEmitter();
 
@@ -37,10 +59,10 @@ export class FeeDetailsComponent implements OnInit {
   }
 
   ngOnInit() {
-     this.feeDetailFormGroup = this.formBuilder.group({
-          feeOrVolumeControl : new FormControl({value: 1, disabled: false}),
-          feeAmountFormControl : new FormControl({value: '0', disabled: false})
-     });
+    this.feeDetailFormGroup = this.formBuilder.group({
+      feeOrVolumeControl: new FormControl({ value: 1, disabled: false }),
+      feeAmountFormControl: new FormControl({ value: '0', disabled: false })
+    });
   }
 
   goBack() {
@@ -52,5 +74,18 @@ export class FeeDetailsComponent implements OnInit {
       this.fee.current_version.flat_amount.amount = this.feeDetailFormGroup.get('feeAmountFormControl').value;
     }
     this.submitFeeVolumeEvent.emit(this.feeDetailFormGroup.get('feeOrVolumeControl').value);
+  }
+
+  get validOldFeesVersions() {
+    const feesLimitDate = new Date();
+    feesLimitDate.setMonth(feesLimitDate.getMonth() - 3);
+
+    if (this.fee.fee_versions.length > 1) {
+      return this.fee.fee_versions.filter(feesVersion => <any>new Date(feesVersion.valid_to) > feesLimitDate && feesVersion.status === 'approved')
+        .sort((a, b) => {
+          return <any>new Date(b.valid_to) - <any>new Date(a.valid_to);
+        })
+    }
+    return [];
   }
 }
