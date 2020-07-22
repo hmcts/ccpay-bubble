26b8db0b790538f3c9028fb369600f62fa717379
amitchidrewar1301 <amit.chidrewar@hmcts.net>
2020-07-22 23:14:21 +0100

    PAY-3377- Test case fixes and logic updates

--- src/app/components/fee-details/fee-details.component.ts
+++ src/app/components/fee-details/fee-details.component.ts
@@ -11,28 +11,29 @@ import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./fee-details.component.scss']
 })
 export class FeeDetailsComponent implements OnInit {
-  selectedFeeVersion: IVersion
+  selectedFeeVersion: IVersion;
+  validOldVersionArray: IVersion[];
   @Input() fee = {
     code: 'test-code',
     fee_type: 'banded',
     fee_versions: [
       {
-        description: "Recovery order (section 50)",
-        status: "approved",
-        author: "126172",
-        approvedBy: "126175",
+        description: 'Recovery order (section 50)',
+        status: 'approved',
+        author: '126172',
+        approvedBy: '126175',
         version: 1,
-        valid_from: "2014-04-21T00:00:00.000+0000",
-        valid_to: "2014-04-21T00:00:00.000+0000",
+        valid_from: '2014-04-21T00:00:00.000+0000',
+        valid_to: '2014-04-21T00:00:00.000+0000',
         flat_amount: {
-          "amount": 215
+          'amount': 215
         },
-        memo_line: "RECEIPT OF FEES - Family misc private",
-        statutory_instrument: "2014 No 877 ",
-        si_ref_id: "2.1q",
-        natural_account_code: "4481102174",
-        fee_order_name: "Family Proceedings",
-        direction: "cost recovery"
+        memo_line: 'RECEIPT OF FEES - Family misc private',
+        statutory_instrument: '2014 No 877 ',
+        si_ref_id: '2.1q',
+        natural_account_code: '4481102174',
+        fee_order_name: 'Family Proceedings',
+        direction: 'cost recovery'
       }
     ],
     'current_version': {
@@ -66,6 +67,8 @@ export class FeeDetailsComponent implements OnInit {
       feeOrVolumeControl: new FormControl({ value: 1, disabled: false }),
       feeAmountFormControl: new FormControl({ value: '0', disabled: false })
     });
+
+    this.validOldFeesVersions();
   }
 
   goBack() {
@@ -75,36 +78,82 @@ export class FeeDetailsComponent implements OnInit {
   submitVolume() {
     if (this.fee.current_version.flat_amount !== undefined && this.fee.fee_type === 'banded') {
       this.fee.current_version.flat_amount.amount = this.feeDetailFormGroup.get('feeAmountFormControl').value;
+
+      if (this.selectedFeeVersion != null) {
+           this.selectedFeeVersion.flat_amount.amount = this.feeDetailFormGroup.get('feeAmountFormControl').value;
+      }
     }
 
-    this.submitFeeVolumeEvent.emit({ volumeAmount: this.feeDetailFormGroup.get('feeAmountFormControl').value, selectedVersionEmit: this.selectedFeeVersion });
+    this.submitFeeVolumeEvent.emit({
+      volumeAmount: this.feeDetailFormGroup.get('feeOrVolumeControl').value, selectedVersionEmit: this.selectedFeeVersion
+    });
   }
 
   getSelectedFeesVersion(currentSelectedFeeVersion: IVersion) {
     this.selectedFeeVersion = currentSelectedFeeVersion;
   }
 
-  get validOldFeesVersions() {
+  validOldFeesVersions() {
+    const validOldFeeVersionArray = new Array();
+
+    if (this.fee.fee_versions.length > 1) {
+      /* sort based on valid from */
+      this.fee.fee_versions = this.fee.fee_versions.
+        filter(feesVersion => feesVersion.status === 'approved')
+        .sort((a, b) => {
+          return <any>new Date(b.valid_from) - <any>new Date(a.valid_from);
+        });
+
+
+
+      console.log('List of all old fee versions', this.fee.fee_versions);
+
+      this.fee.fee_versions.forEach(function (value, i) {
+        if (i !== 0) {
+          // if amount is diffrent then only consider it for push need to confirm that as well
+          // if (this.getAmountFromFeeVersion(value) == this.getAmountFromFeeVersion(this.fee.fee_versions[i - 1])) {
+          //   console.log('fee version deleted due to same amount with previous vesion ', value)
+          //   return
+          // }
+          /* set valid to date if not present for fee version from previous version*/
+          if (value.valid_to == null) {
+            const new_valid_to = new Date(validOldFeeVersionArray[i - 1].valid_from);
+            new_valid_to.setDate(new_valid_to.getDate() - 1);
+            value.valid_to = new_valid_to.toDateString();
+          }
+        }
+        validOldFeeVersionArray.push(value);
+      });
+    }
+    console.log('Valid old fee version list', validOldFeeVersionArray);
+
+    if (validOldFeeVersionArray.length > 1) {
+      return this.validOldVersionArray = validOldFeeVersionArray.filter(feesVersion => this.getValidFeeVersionsBasedOnDate(feesVersion));
+    } else {
+      return this.validOldVersionArray = [];
+    }
+  }
+
+  getValidFeeVersionsBasedOnDate(feeVersion: IVersion) {
     const feesLimitDate = new Date();
     /* Check valid fees till 6 months  */
     feesLimitDate.setMonth(feesLimitDate.getMonth() - 3);
 
-    if (this.fee.fee_versions.length > 1) {
-      return this.fee.fee_versions.filter(feesVersion => <any>new Date(feesVersion.valid_to) > feesLimitDate && feesVersion.status === 'approved')
-        .sort((a, b) => {
-          return <any>new Date(b.valid_to) - <any>new Date(a.valid_to);
-        })
+    if (feeVersion.valid_from != null && <any>new Date(feeVersion.valid_from) > feesLimitDate) {
+      return true;
+    } else if (feeVersion.valid_to != null && <any>new Date(feeVersion.valid_to) > feesLimitDate) {
+      return true;
     }
-    return [];
+    return false;
   }
 
   getAmountFromFeeVersion(feeVersion: IVersion) {
     if (feeVersion['volume_amount'] != null) {
-      return feeVersion['volume_amount'].amount
+      return feeVersion['volume_amount'].amount;
     } else if (feeVersion['flat_amount'] != null) {
-      return feeVersion['flat_amount'].amount
+      return feeVersion['flat_amount'].amount;
     } else if (feeVersion['percentage_amount'] != null) {
-      return feeVersion['percentage_amount'].percentage
+      return feeVersion['percentage_amount'].percentage;
     }
   }
 }
