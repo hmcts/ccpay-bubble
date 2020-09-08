86e777920eb15a28793e29e591744d0622b8a281
“Kumar <Kumarmcamadura@gmail.com>
2020-09-08 19:36:27 +0100

    Merge all paybubble enhancement changes

--- server.js
+++ server.js
@@ -78,11 +78,11 @@ module.exports = (security, appInsights) => {
   // enable the dist folder to be accessed statically
   app.use(express.static('dist/ccpay-bubble'));
 
-  app.use('/pcipalTest', () => dom.firstChild.outerHTML);
+  app.use('/pcipalTest', (req, res) => res.status(HttpStatus.OK).send(dom.firstChild.outerHTML));
 
   app.use('/logout', security.logout());
   app.use('/oauth2/callback', security.OAuth2CallbackEndpoint());
-  app.use('/health/liveness', (req, res) => res.status(HttpStatus.OK).h({ status: 'UP' }));
+  app.use('/health/liveness', (req, res) => res.status(HttpStatus.OK).json({ status: 'UP' }));
   app.use('/health', healthcheck);
 
   // allow access origin
