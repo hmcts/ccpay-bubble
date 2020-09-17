aadffa4babaf5f576012f8d22af0ca51e40525d3
“Kumar <Kumarmcamadura@gmail.com>
2020-09-17 17:09:40 +0100

    DNS redirect  changes

--- server.js
+++ server.js
@@ -79,7 +79,10 @@ module.exports = (security, appInsights) => {
   // enable the dist folder to be accessed statically
   app.use(express.static('dist/ccpay-bubble'));
 
-  app.use('/pcipalThirdCall', (req, res) => res.status(HttpStatus.OK).send(security.pcipalForm(req, res)));
+  app.use('/pcipalThirdCall', (req, res) => {
+    res.status(HttpStatus.OK).send(security.pcipalForm(req, res));
+    res.clearCookie(constants.PCIPAL_SECURITY_INFO);
+  });
 
   app.use('/logout', security.logout());
   app.use('/oauth2/callback', security.OAuth2CallbackEndpoint());
