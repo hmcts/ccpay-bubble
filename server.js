01d132c69beee7929b1bbdd5615abe9ff557e7a3
“Kumar <Kumarmcamadura@gmail.com>
2020-10-09 11:19:12 +0100

    Strategic fix changes

--- server.js
+++ server.js
@@ -72,6 +72,7 @@ module.exports = (security, appInsights) => {
 
   // enable the dist folder to be accessed statically
   app.use(express.static('dist/ccpay-bubble'));
+
   app.use('/logout', security.logout());
   app.use('/oauth2/callback', security.OAuth2CallbackEndpoint());
   app.use('/health/liveness', (req, res) => res.status(HttpStatus.OK).json({ status: 'UP' }));
