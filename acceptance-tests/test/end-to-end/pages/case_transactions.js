ce46e3a771af782f5c0b591127868c23e53dc78f
Satish Ramachandrappa <satish.ramachandrappa@hmcts.net>
2021-01-21 10:03:11 +0000

    Remission functionality added and code refactored

--- acceptance-tests/test/end-to-end/pages/case_transactions.js
+++ acceptance-tests/test/end-to-end/pages/case_transactions.js
@@ -1,40 +1,29 @@
 'use strict';
 const CCPBConstants = require('../tests/CCPBAcceptanceTestConstants');
-// in this file you can append custom step methods to 'I' object
-// const faker = require('faker');
-const faker = require('faker');
-
 const {I} = inject();
 
-const RANDOM_NUMBER = 9999999999999999;
-
-const CCDNumber = faker.random.number(RANDOM_NUMBER);
-
-module.exports =  {
-    locators :{
-      caseTitle : {xpath : '//*[@class = "heading-medium"]'},
-      unallocatedPaymentsCount :{xpath : '//table[@class="govuk-table"]/tbody//td[4]'},
-      unallocatedPaymentSelectOption : {xpath : '//ccpay-app-unprocessed-payments//tbody/tr[1]//input'},
+module.exports = {
+  locators: {
+    case_title: {xpath: '//*[@class = "heading-medium"]'},
+    unallocated_payments_count: {xpath: '//table[@class="govuk-table"]/tbody//td[4]'},
+    unallocated_payment_select_option: {xpath: '//ccpay-app-unprocessed-payments//tbody/tr[1]//input'},
 
 
-
-    },
+  },
   // done
-  check_bulk_case(case_number, case_title)
-  {
+  checkBulkCase(case_number, case_title) {
     I.wait(CCPBConstants.fiveSecondWaitTime);
-    this.validate_transaction_page(case_number);
+    this.validateTransactionPage(case_number);
     I.see(case_title);
   },
 
-   check_bulk_case_success_payment(case_number, case_title,allocation_status)
-    {
-      I.wait(CCPBConstants.fiveSecondWaitTime);
-      this.validate_transaction_page_for_success_payment(case_number,allocation_status);
-      I.see(case_title);
-    },
+  checkBulkCaseSuccessPayment(case_number, case_title, allocation_status) {
+    I.wait(CCPBConstants.fiveSecondWaitTime);
+    this.validateTransactionPageForSuccessPayment(case_number, allocation_status);
+    I.see(case_title);
+  },
 
-  check_for_unallocated_payments(total_dcn, dcn_number,amount,method) {
+  checkUnallocatedPayments(total_dcn, dcn_number, amount, method) {
     I.see(total_dcn);
     I.see(dcn_number);
     I.see(amount);
@@ -42,74 +31,72 @@ module.exports =  {
 
   },
 
-  allocate_to_new_fee() {
-    I.checkOption(this.locators.unallocatedPaymentSelectOption)
+  allocateToNewFee() {
+    I.checkOption(this.locators.unallocated_payment_select_option)
     I.click('Allocate to a new fee');
     I.wait(CCPBConstants.fiveSecondWaitTime);
   },
 
-   allocate_to_transferred() {
-      I.checkOption(this.locators.unallocatedPaymentSelectOption)
-      I.click('Mark as transferred');
-      I.wait(CCPBConstants.fiveSecondWaitTime);
-    },
+  allocateToTransferred() {
+    I.checkOption(this.locators.unallocated_payment_select_option)
+    I.click('Mark as transferred');
+    I.wait(CCPBConstants.fiveSecondWaitTime);
+  },
 
-   allocate_to_unidentified() {
-          I.checkOption(this.locators.unallocatedPaymentSelectOption)
-          I.click('Mark as unidentified');
-          I.wait(CCPBConstants.fiveSecondWaitTime);
-        },
+  allocateToUnidentified() {
+    I.checkOption(this.locators.unallocated_payment_select_option)
+    I.click('Mark as unidentified');
+    I.wait(CCPBConstants.fiveSecondWaitTime);
+  },
 
-  validate_transaction_page(case_number)
-  {
-        I.see(case_number);
-        I.see('Total payments');
-        I.see('Total remissions');
-        I.see('Amount due');
-        I.see('Unallocated payments');
-        I.see('Unallocated payments');
-        I.see('Select');
-        I.see('Payment asset number (DCN)');
-        I.see('Banked date');
-        I.see('Amount');
-        I.see('Method');
-        I.see('Fees');
-        I.see('Code');
-        I.see('Description');
-        I.see('Volume');
-        I.see('Fee amount');
-        I.see('Calculated amount');
-        I.see('Amount due');
-        I.see('Action');
-        I.see('No fees recorded');
-   },
+  validateTransactionPage(case_number) {
+    I.see(case_number);
+    I.see('Total payments');
+    I.see('Total remissions');
+    I.see('Amount due');
+    I.see('Unallocated payments');
+    I.see('Unallocated payments');
+    I.see('Select');
+    I.see('Payment asset number (DCN)');
+    I.see('Banked date');
+    I.see('Amount');
+    I.see('Method');
+    I.see('Fees');
+    I.see('Code');
+    I.see('Description');
+    I.see('Volume');
+    I.see('Fee amount');
+    I.see('Calculated amount');
+    I.see('Amount due');
+    I.see('Action');
+    I.see('No fees recorded');
+  },
 
-    validate_transaction_page_for_success_payment(case_number, allocation_status)
-     {
-           I.see(case_number);
-           I.see('Total payments');
-           I.see('Total remissions');
-           I.see('Amount due');
-           I.see('Unallocated payments');
-           I.see('Unallocated payments');
-           I.see('Select');
-           I.see('Payment asset number (DCN)');
-           I.see('Banked date');
-           I.see('Amount');
-           I.see('Method');
-           I.see('Fees');
-           I.see('Code');
-           I.see('Description');
-           I.see('Volume');
-           I.see('Fee amount');
-           I.see('Calculated amount');
-           I.see('Amount due');
-           I.see('Action');
-           I.see(allocation_status);
-           I.see('Bulk scan');
-           I.see('Success');
+  validateTransactionPageForSuccessPayment(case_number, allocation_status) {
+    I.see(case_number);
+    I.see('Total payments');
+    I.see('Total remissions');
+    I.see('Amount due');
+    I.see('Unallocated payments');
+    I.see('Unallocated payments');
+    I.see('Select');
+    I.see('Payment asset number (DCN)');
+    I.see('Banked date');
+    I.see('Amount');
+    I.see('Method');
+    I.see('Fees');
+    I.see('Code');
+    I.see('Description');
+    I.see('Volume');
+    I.see('Fee amount');
+    I.see('Calculated amount');
+    I.see('Amount due');
+    I.see('Action');
+    I.see(allocation_status);
+    I.see('Bulk scan');
+    I.see('Success');
 
-      }
+  }
 
 
 }
--- acceptance-tests/test/end-to-end/pages/case_transferred.js
+++ acceptance-tests/test/end-to-end/pages/case_transferred.js
@@ -1,24 +1,15 @@
 'use strict';
 const CCPBConstants = require('../tests/CCPBAcceptanceTestConstants');
-// in this file you can append custom step methods to 'I' object
-// const faker = require('faker');
-const faker = require('faker');
-
 const {I} = inject();
 
-const RANDOM_NUMBER = 9999999999999999;
-
-const CCDNumber = faker.random.number(RANDOM_NUMBER);
-
-module.exports =  {
-    locators :{
-      transferredReason : {xpath : '//*[@id="reason"]'},
-      receivingSiteId : {xpath : '//*[@id="responsibleOffice"]'},
-    },
+module.exports = {
+  locators: {
+    transferred_reason: {xpath: '//*[@id="reason"]'},
+    receiving_site_id: {xpath: '//*[@id="responsibleOffice"]'},
+  },
 
 
-  validate_transferred_page(dcn_number, amount, method)
-  {
+  validateTransferredPage(dcn_number, amount, method) {
     I.see('Mark payment as transferred');
     I.see('Payment asset number (DCN)');
     I.see('Banked date');
@@ -31,73 +22,59 @@ module.exports =  {
     I.see(method);
   },
 
-  cancel_payment()
-  {
-    I.click('Cancel');
+
+  confirmPayment(transferred_reason, site_id) {
+    I.click('Confirm');
     I.wait(CCPBConstants.fiveSecondWaitTime);
   },
 
-   confirm_payment(transferred_reason,site_id)
-    {
-      I.click('Confirm');
-      I.wait(CCPBConstants.fiveSecondWaitTime);
-    },
+  inputTransferredReason(transferred_reason) {
+    I.fillField(this.locators.transferred_reason, transferred_reason);
+  },
 
-    input_transferred_reason(transferred_reason)
-    {
-     I.fillField(this.locators.transferredReason, transferred_reason);
-    },
+  selectSiteId(site_id) {
+    I.selectOption(this.locators.receiving_site_id, site_id);
+  },
 
-    select_site_id(site_id)
-    {
-      I.selectOption(this.locators.receivingSiteId,site_id);
-    },
+  validateTransferredConfirmationPage(transferred_reason, site_id) {
+    I.see('Reason');
+    I.see('Receiving site ID');
+    I.see('Are you sure you want to mark this payment as transferred?');
+    I.see(transferred_reason);
+    I.see(site_id);
+  },
 
-    validate_transferred_confirmation_page(transferred_reason, site_id)
-      {
-        I.see('Reason');
-        I.see('Receiving site ID');
-        I.see('Are you sure you want to mark this payment as transferred?');
-        I.see(transferred_reason);
-        I.see(site_id);
-      },
+  validateAndConfirmTransferred(transferred_reason, site_id) {
+    this.inputTransferredReason(transferred_reason);
+    this.selectSiteId(site_id);
+    this.confirmPayment();
+    I.wait(CCPBConstants.fiveSecondWaitTime);
+    this.validateTransferredConfirmationPage(transferred_reason, site_id)
 
-      validate_and_confirm_transferred(transferred_reason, site_id)
-      {
-          this.input_transferred_reason(transferred_reason);
-          this.select_site_id(site_id);
-          this.confirm_payment();
-          I.wait(CCPBConstants.fiveSecondWaitTime);
-          this.validate_transferred_confirmation_page(transferred_reason, site_id)
+  },
 
-      },
+  cancelTransferred() {
+    I.click('Cancel');
+    I.wait(CCPBConstants.fiveSecondWaitTime);
+    I.click('Cancel');
+    I.wait(CCPBConstants.fiveSecondWaitTime);
+    I.see('Are you sure you want to cancel?');
+    I.click('Yes');
+  },
 
-      cancel_transferred()
-      {
-        I.click('Cancel');
-        I.wait(CCPBConstants.fiveSecondWaitTime);
-        I.click('Cancel');
-        I.wait(CCPBConstants.fiveSecondWaitTime);
-        I.see('Are you sure you want to cancel?');
-        I.click('Yes');
-      },
+  cancelTransferredReason() {
+    I.click('Cancel');
+    I.wait(CCPBConstants.fiveSecondWaitTime);
+    I.see('Are you sure you want to cancel?');
+    I.click('Yes');
+  },
 
-      cancel_transferred_reason()
-      {
-              I.click('Cancel');
-              I.wait(CCPBConstants.fiveSecondWaitTime);
-              I.see('Are you sure you want to cancel?');
-              I.click('Yes');
-      },
+  whenNoReasonAndSiteid() {
+    I.see('Enter a reason for marking this payment as transferred.');
+    I.see('Please select Receiving Site ID');
+  },
 
-      error_message_when_no_reason_and_siteid()
-      {
-       I.see('Enter a reason for marking this payment as transferred.');
-       I.see('Please select Receiving Site ID');
-      },
-
-      error_message_when_reason_less_than_limit()
-            {
-             I.see('Reason should be at least 3 characters.');
-            },
-      }
+  whenReasonLessThanLimit() {
+    I.see('Reason should be at least 3 characters.');
+  },
+}
