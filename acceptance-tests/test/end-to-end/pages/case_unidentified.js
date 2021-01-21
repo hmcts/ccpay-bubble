fa977b958ebc34882fb8135aac363c80ee0a312d
Satish Ramachandrappa <satish.ramachandrappa@hmcts.net>
2021-01-21 10:03:11 +0000

    Remission functionality added and code refactored

--- acceptance-tests/test/end-to-end/pages/case_unidentified.js
+++ acceptance-tests/test/end-to-end/pages/case_unidentified.js
@@ -1,23 +1,14 @@
 'use strict';
 const CCPBConstants = require('../tests/CCPBAcceptanceTestConstants');
-// in this file you can append custom step methods to 'I' object
-// const faker = require('faker');
-const faker = require('faker');
-
 const {I} = inject();
 
-const RANDOM_NUMBER = 9999999999999999;
-
-const CCDNumber = faker.random.number(RANDOM_NUMBER);
-
-module.exports =  {
-    locators :{
-      unidentifiedInvestigation : {xpath : '//*[@id="investicationDetail"]'},
-    },
+module.exports = {
+  locators: {
+    unidentified_investigation: {xpath: '//*[@id="investicationDetail"]'},
+  },
 
 
-  validate_unidentified_page(dcn_number, amount, method)
-  {
+  validateUnidentifiedPage(dcn_number, amount, method) {
     I.see('Mark payment as unidentified');
     I.see('Payment asset number (DCN)');
     I.see('Banked date');
@@ -30,58 +21,50 @@ module.exports =  {
     I.see(method);
   },
 
-   continue_payment()
-    {
-      I.click('Continue');
-      I.wait(CCPBConstants.fiveSecondWaitTime);
-    },
+  continuePayment() {
+    I.click('Continue');
+    I.wait(CCPBConstants.fiveSecondWaitTime);
+  },
 
-    input_unidentified_comment(unidentified_investigation)
-    {
-     I.fillField(this.locators.unidentifiedInvestigation, unidentified_investigation);
-    },
+  inputUnidentifiedComment(unidentified_investigation) {
+    I.fillField(this.locators.unidentified_investigation, unidentified_investigation);
+  },
 
-   validate_unidentified_confirmation_page(unidentified_investigation)
-      {
-        I.see('Investigations');
-        I.see(unidentified_investigation);
-        I.see('Are you sure you want to mark this payment as unidentified?');
-      },
+  validateUnidentifiedConfirmationPage(unidentified_investigation) {
+    I.see('Investigations');
+    I.see(unidentified_investigation);
+    I.see('Are you sure you want to mark this payment as unidentified?');
+  },
 
-      validate_and_confirm_unidentified(unidentified_investigation)
-      {
-          this.input_unidentified_comment(unidentified_investigation);
-          this.continue_payment();
-          I.wait(CCPBConstants.fiveSecondWaitTime);
-          this.validate_unidentified_confirmation_page(unidentified_investigation)
+  validateAndConfirmUnidentified(unidentified_investigation) {
+    this.inputUnidentifiedComment(unidentified_investigation);
+    this.continuePayment();
+    I.wait(CCPBConstants.fiveSecondWaitTime);
+    this.validateUnidentifiedConfirmationPage(unidentified_investigation)
 
-      },
+  },
 
-      cancel_unidentified()
-      {
-        I.click('Cancel');
-        I.wait(CCPBConstants.fiveSecondWaitTime);
-        I.click('Cancel');
-        I.wait(CCPBConstants.fiveSecondWaitTime);
-        I.see('Are you sure you want to cancel?');
-        I.click('Yes');
-      },
+  cancelUnidentified() {
+    I.click('Cancel');
+    I.wait(CCPBConstants.fiveSecondWaitTime);
+    I.click('Cancel');
+    I.wait(CCPBConstants.fiveSecondWaitTime);
+    I.see('Are you sure you want to cancel?');
+    I.click('Yes');
+  },
 
-      cancel_unidentified_comment()
-      {
-              I.click('Cancel');
-              I.wait(CCPBConstants.fiveSecondWaitTime);
-              I.see('Are you sure you want to cancel?');
-              I.click('Yes');
-      },
+  cancelUnidentifiedComment() {
+    I.click('Cancel');
+    I.wait(CCPBConstants.fiveSecondWaitTime);
+    I.see('Are you sure you want to cancel?');
+    I.click('Yes');
+  },
 
-      error_message_when_no_investigation()
-      {
-       I.see('Enter a reason for marking this payment as unidentified.');
-       },
+  whenNoInvestigation() {
+    I.see('Enter a reason for marking this payment as unidentified.');
+  },
 
-      error_message_when_comment_less_than_limit()
-            {
-             I.see('Reason should be at least 3 characters.');
-            },
-      }
+  whenCommentLessThanLimit() {
+    I.see('Reason should be at least 3 characters.');
+  },
+}
