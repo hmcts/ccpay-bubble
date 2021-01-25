626e334d690daf76c37c3eec5e6380aa04c3d713
vigneshpushkaranHMCTS <vignesh.pushkaran@hmcts.net>
2021-01-25 15:56:07 +0000

    Update PayhubController.js

--- express/mvc/controller/PayhubController.js
+++ express/mvc/controller/PayhubController.js
@@ -67,7 +67,7 @@ class PayhubController {
         }
       })
       .catch(error => {
-        res.status(error.statusCode).json({ err: error.message, success: false });
+        res.status(500).json({ err: error.message, success: false });
       });
   }
 
