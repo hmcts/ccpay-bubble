4755d6256191ace22528e0fe13ee2917940407c3
Satish Ramachandrappa <satish.ramachandrappa@hmcts.net>
2021-01-25 20:33:13 +0000

    Reduced wait time to 15s

--- acceptance-tests/test/end-to-end/tests/CCPBAcceptanceTestConstants.js
+++ acceptance-tests/test/end-to-end/tests/CCPBAcceptanceTestConstants.js
@@ -2,7 +2,7 @@ module.exports = {
   windowsSizeX: 1600,
   windowsSizeY: 1200,
   twoSecondWaitTime: 10,
-  fiveSecondWaitTime: 30,
+  fiveSecondWaitTime: 15,
   tenSecondWaitTime: 20,
   thirtySecondWaitTime: 40,
   retryCountForStep: 4,
