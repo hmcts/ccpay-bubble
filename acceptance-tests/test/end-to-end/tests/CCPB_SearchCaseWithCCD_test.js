40eec38b4fe832e57b8c42b414b5e55ed0b1e109
Satish Ramachandrappa <satish.ramachandrappa@hmcts.net>
2021-01-27 18:43:31 +0000

    Added tags for nightly specific run for few scenarios

--- acceptance-tests/test/end-to-end/tests/CCPB_SearchCaseWithCCD_test.js
+++ acceptance-tests/test/end-to-end/tests/CCPB_SearchCaseWithCCD_test.js
@@ -1,5 +1,7 @@
 const CCPBATConstants = require('./CCPBAcceptanceTestConstants');
 
+const nightlyTest = process.env.NIGHTLY_TEST;
+
 Feature('CC Pay Bubble Acceptance Tests');
 
 BeforeSuite(I => {
@@ -20,19 +22,24 @@ Scenario('Search for a case with dummy case number', I => {
   I.Logout();
 });
 
-Scenario('Search for a case with actual case number from CCD', I => {
-  I.login('robreallywantsccdaccess@mailinator.com', 'Testing1234');
-  I.wait(CCPBATConstants.tenSecondWaitTime);
-  I.waitForText('Search for a case', CCPBATConstants.tenSecondWaitTime);
-  I.see('Search for a case');
-  I.see('What do you want to search for?');
-  I.see('CCD case reference or exception record');
-  I.see('Payment Asset Number (DCN)');
-  I.see('Case Transaction');
-  I.see('Payment history');
-  I.see('Reports');
-  I.searchForCorrectCCDNumber();
-  I.Logout();
+Scenario('Search for a case with actual case number from CCD @nightly', I => {
+  if (nightlyTest === 'true')
+  {
+    I.login('robreallywantsccdaccess@mailinator.com', 'Testing1234');
+    I.wait(CCPBATConstants.tenSecondWaitTime);
+    I.waitForText('Search for a case', CCPBATConstants.tenSecondWaitTime);
+    I.see('Search for a case');
+    I.see('What do you want to search for?');
+    I.see('CCD case reference or exception record');
+    I.see('Payment Asset Number (DCN)');
+    I.see('Case Transaction');
+    I.see('Payment history');
+    I.see('Reports');
+    I.searchForCorrectCCDNumber();
+    I.Logout();
+
+  }
+
 });
 
 Scenario('Search for a case with actual case for Telephony flow', I => {
