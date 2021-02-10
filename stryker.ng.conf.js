383d246a82dd6433c16b9be1aca82687b00a7f7f
Satish Ramachandrappa <satish.ramachandrappa@hmcts.net>
2021-02-10 12:58:21 +0000

    Updated package json to use new stryker config with 6 workers (2) Added required unit components to be used for mutation testing

--- stryker.ng.conf.js
+++ stryker.ng.conf.js
@@ -4,30 +4,37 @@
 module.exports = {
   fileLogLevel: 'trace',
   // logLevel: 'trace',
-  comment:
-        'This config was generated using a preset. Please see the handbook for more information: https://github.com/stryker-mutator/stryker-handbook/blob/master/stryker/guides/angular.md#angular',
-  mutate: [
-    'src/**/*.ts',
-    '!src/**/*.spec.ts',
-    '!src/test.ts',
-    '!src/environments/*.ts'
+ mutator: 'typescript',
+  files:[
+    './**',
+    '!.idea/**',
+    '!.nyc_output/**',
+    '!.stryker-tmp/**',
+    '!.vscode/**',
+    '!acceptance-tests/**',
+    '!functional-output/**',
+    '!node_modules/**',
+    '!output/**',
+    '!reports/**'
   ],
-
-  mutator: 'typescript',
-  // mutator1:{
-  //   name: "typescript",
-  //   excludedMutations: ["EqualityOperator", "BooleanLiteral", "ConditionalExpression", "LogicalOperator", "StringLiteral", "MethodExpression"]
-  // },
+  mutate: [
+    // 'src/app/components/add-fee-detail/add-fee-detail.component.ts'
+    'src/app/**/*.ts',
+    '!src/app/**/*.spec.ts'
+  ],
 
   testRunner: 'karma',
   karma: {
-    configFile: './karma.conf.js',
+    configFile: 'karma.conf.js',
     projectType: 'angular-cli',
     config: { browsers: ['ChromeHeadless'] }
   },
-  reporters: ['progress', 'clear-text', 'html'],
-  maxConcurrentTestRunners: 4,
-  maxConcurrentTestRunners_comment:
-        'Recommended to use about half of your available cores when running stryker with angular',
-  coverageAnalysis: 'off'
+  reporters: ['html','clear-text','progress'],
+
+  // logLevel: "all",
+  thresholds: {high:95, low:85, break:56},
+  timeoutMS:60*1000, //1 minute
+  timeoutFactor: 4,
+  maxConcurrentTestRunners: 6,
+  coverageAnalysis : "off",
 };
