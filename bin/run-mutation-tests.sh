96d322603ce2b40aec131d76c094b9ac2235aac4
Satish Ramachandrappa <satish.ramachandrappa@hmcts.net>
2021-02-15 18:04:35 +0000

    Added multiple tries if stryker fails due to afterall error

--- bin/run-mutation-tests.sh
+++ bin/run-mutation-tests.sh
@@ -4,7 +4,21 @@ echo "start: $(date)"
 
 for file in mutation-tests/mutation_*.conf.js ; do
   echo "file: $file";
-  yarn test:stryker $file ;
+  count=0;
+  while [ $count -lt 5 ]
+  do
+        yarn test:stryker $file ;
+        let retVal=$?;
+        echo $retVal;
+        if [ $retVal == 0 ];
+        then
+        count=5;
+        else
+        count=$(($count + 1));
+        fi
+
+
+  done
 done
 
 echo "end: $(date)"
