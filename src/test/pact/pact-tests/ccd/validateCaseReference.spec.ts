3223bf78a8892c983dea72bd857c45ad22d50cba
hsjhita <harpreet.jhita@justice.gov.uk>
2021-03-03 12:25:39 +0000

    TA-450 - Test written but not fully working
    TA-450 - Test written but not fully working

---
+++ src/test/pact/pact-tests/ccd/validateCaseReference.spec.ts
@@ -0,0 +1,67 @@
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+const { PactTestSetup } = require('../settings/provider.mock');
+const { Matchers } = require('@pact-foundation/pact');
+const { somethingLike, like, eachLike } = Matchers;
+const { validateCaseReference } = require('../../pactUtil');
+const pactSetUp = new PactTestSetup({provider: 'ccdDataStoreApi', port: 8000});
+
+describe('CaseData', () => {
+  const mockResponse = {
+    'status_message' : somethingLike('OK'),
+    'caseref' : somethingLike('12345678')
+  };
+  describe('/get cases', () => {
+    const jwt = 'some-access-token';
+    const caseId = '12345678';
+    before(async () => {
+      await pactSetUp.provider.setup();
+      const interaction = {
+        state: 'a case exists',
+        uponReceiving: 'a request for that case',
+        withRequest: {
+          method: 'GET',
+          path: `/cases/`,
+          query: '12345678',
+          headers: {
+            ContentType: 'application/json',
+            Authorization: 'Bearer some-access-token',
+            ServiceAuthorization: 'Bearer ServiceAuthToken'
+          },
+        },
+        willRespondWith: {
+          status: 200
+          // headers: {
+          //   'Content-Type': 'application/json'
+          // },
+          // body: mockResponse
+        }
+      };
+      pactSetUp.provider.addInteraction(interaction).then(() => {});
+    });
+    it('Returns Case', async () => {
+      const taskUrl = `${pactSetUp.provider.mockService.baseUrl}`;
+      const response = validateCaseReference(taskUrl, jwt, caseId);
+      response.then((Response) => {
+        console.log('Test');
+        const dto: CcdGetResponseDto = <CcdGetResponseDto>Response.body;
+        assertResponse(dto);
+      }).then(() => {
+        pactSetUp.provider.verify();
+        pactSetUp.provider.finalize();
+      });
+    });
+  });
+});
+
+function assertResponse(dto: CcdGetResponseDto) {
+  expect(dto.status_message).to.be.equal('OK');
+  expect(dto.caseref).to.be.equal('12345678');
+}
+
+export interface CcdGetResponseDto {
+  status_message: string;
+  caseref: string;
+}
