3e28c8f168fae1a8a70ffc737f05d40717d9b2a9
santu412 <490436@ambgb000021.home>
2021-03-08 12:11:12 +0000

    reports

--- projects/fee-register-search/src/lib/pipes/filter-fees.pipe.ts
+++ projects/fee-register-search/src/lib/pipes/filter-fees.pipe.ts
@@ -33,13 +33,13 @@ export class FilterFeesPipe implements PipeTransform {
 
   filterValidFee(fees: IFee[]) {
         const todayDate = new Date();
-        return fees.filter(fee => fee.current_version.status === 'approved' &&  
-                              <any>new Date(fee.current_version.valid_from) <= todayDate &&
-                              (fee.current_version.valid_to === ' ' || 
-                              fee.current_version.valid_to === '' || 
-                               fee.current_version.valid_to === null || 
-                               fee.current_version.valid_to === undefined || 
-                              <any>new Date(fee.current_version.valid_to) >= todayDate));
+        return fees.filter(fee => fee.current_version.status === 'approved' &&
+   <any>new Date(fee.current_version.valid_from) <= todayDate &&
+  (fee.current_version.valid_to === ' ' ||
+   fee.current_version.valid_to === '' ||
+   fee.current_version.valid_to === null ||
+   fee.current_version.valid_to === undefined ||
+   <any>new Date(fee.current_version.valid_to) >= todayDate));
       }
 
   filterByDescription(fees, filter): IFee[] {
