7f1cd347dc973db3edee39014f34c2f0ca570708
santu412 <490436@ambgb000021.home>
2021-03-08 11:58:03 +0000

    reports

--- security.sh
+++ security.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#echo "${SECURITYCONTEXT}" > /zap/security.context
+#echo "${SECURITYCONTEXT}" > /zap/security.context 
 zap-x.sh -d -host 0.0.0.0 -port 1001 -config api.disablekey=true -config scanner.attackOnStart=true -config view.mode=attack -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true /dev/null 2>&1 &
 i=0
 while !(curl -s http://0.0.0.0:1001) > /dev/null
