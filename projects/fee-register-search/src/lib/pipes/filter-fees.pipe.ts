540c5a04a4da51dcc5b9801c71d27697cabafb27
“Kumar <Kumarmcamadura@gmail.com>
2021-03-09 09:07:31 +0000

    Discontinued fees changes

--- projects/fee-register-search/src/lib/pipes/filter-fees.pipe.ts
+++ projects/fee-register-search/src/lib/pipes/filter-fees.pipe.ts
@@ -16,6 +16,7 @@ export class FilterFeesPipe implements PipeTransform {
 
     if (this.isNumeric(searchFilter)) {
       filteredList = this.filterByNumber(fees, searchFilter);
+      console.log(filteredList);
     } else {
       searchFilter = searchFilter.toLowerCase();
 
@@ -35,6 +36,10 @@ export class FilterFeesPipe implements PipeTransform {
     return fees.filter(fee => fee.current_version);
   }
 
+  filterValidDiscontinuedFee(fees: IFee[]) {
+    return fees.filter(fee => fee.fee_versions ? fee.fee_versions : []);
+  }
+
   filterByDescription(fees, filter): IFee[] {
     const filterArray = filter.split(' ');
     return fees.filter((fee: IFee) => {
@@ -51,22 +56,49 @@ export class FilterFeesPipe implements PipeTransform {
 
   filterByNumber(fees, filter): IFee[] {
     const regExactWord = new RegExp(`\\b${filter}\\b`);
-    return fees.filter((fee: IFee) => {
+
+    let filteredFees = this.filterBydisFee(fees, filter);
+    return filteredFees.filter((fee: IFee) => {
       if (fee.current_version.flat_amount !== undefined
-      && fee.current_version.flat_amount.amount !== undefined) {
+      && fee.current_version.flat_amount.amount !== undefined ) {
         if (fee.current_version.flat_amount.amount === Number(filter)) {
           fee.sort_value = 1;
           return true;
         }
-      }
+      } 
+
       if (fee.current_version.description !== undefined) {
         if (regExactWord.test(fee.current_version.description)) {
           fee.sort_value = 0;
           return true;
         }
       }
+
+      if ( fee.isdiscontinued_fee !== undefined 
+         && fee.isdiscontinued_fee === 1 ) {
+          fee.sort_value = 1;
+          return true;
+      }
+    
       return false;
     }).sort((a, b) => b.sort_value - a.sort_value);
+  }
+
+  filterBydisFee(fees, filter) {
+    return fees.filter((fee: IFee) => {
+     if (fee.fee_versions && fee.fee_versions.length > 0) {
+       return fee.fee_versions.filter(oldFee => {
+        if (oldFee.flat_amount !== undefined
+          && oldFee.flat_amount.amount !== undefined) {
+            if (oldFee.flat_amount.amount === Number(filter)) {
+              fee.isdiscontinued_fee = 1;
+              return true;
+            }
+          }
+       });
+    }
+  });
+
   }
 
   filterByAmount(fees, filter): IFee[] {
