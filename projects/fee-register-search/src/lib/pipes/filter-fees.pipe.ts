0cade013a5c9060312e1bf4a8d6e21e771240004
“Kumar <Kumarmcamadura@gmail.com>
2021-03-10 22:03:28 +0000

    Pay-4097 disconntinue fee change

--- projects/fee-register-search/src/lib/pipes/filter-fees.pipe.ts
+++ projects/fee-register-search/src/lib/pipes/filter-fees.pipe.ts
@@ -1,5 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { IFee } from '../interfaces';
+import { IVersion } from '../interfaces';
 import { Jurisdictions } from '../models/Jurisdictions';
 
 @Pipe({
@@ -11,8 +12,6 @@ export class FilterFeesPipe implements PipeTransform {
     if (!searchFilter) { return fees; }
 
     let filteredList: IFee[] = [];
-
-    fees = this.filterValidFee(fees);
 
     if (this.isNumeric(searchFilter)) {
       filteredList = this.filterByNumber(fees, searchFilter);
@@ -56,30 +55,42 @@ export class FilterFeesPipe implements PipeTransform {
 
   filterByNumber(fees, filter): IFee[] {
     const regExactWord = new RegExp(`\\b${filter}\\b`);
-
-    let filteredFees = this.filterBydisFee(fees, filter);
+    const filteredFees = this.filterBydisFee(fees, filter);
     return filteredFees.filter((fee: IFee) => {
-      if (fee.current_version.flat_amount !== undefined
-      && fee.current_version.flat_amount.amount !== undefined ) {
+      if (this.validOldFeesVersions(fee).length > 0) {
+        fee.discontinued_list = this.validOldFeesVersions(fee);
+      }
+
+      if (fee.current_version !== undefined &&
+        ((fee.current_version.flat_amount && fee.current_version.flat_amount.amount)
+        || (fee.current_version.volume_amount && fee.current_version.volume_amount.amount)
+        || (fee.current_version.percentage_amount && fee.current_version.percentage_amount.percentage))) {
+        fee.isCurrentAmount_available = 1;
+      }
+
+      if (fee.current_version !== undefined
+        && fee.current_version.flat_amount !== undefined
+        && fee.current_version.flat_amount.amount !== undefined) {
         if (fee.current_version.flat_amount.amount === Number(filter)) {
           fee.sort_value = 1;
           return true;
         }
-      } 
+      }
 
-      if (fee.current_version.description !== undefined) {
+      if (fee.current_version !== undefined
+        && fee.current_version.description !== undefined) {
         if (regExactWord.test(fee.current_version.description)) {
           fee.sort_value = 0;
           return true;
         }
       }
 
-      if ( fee.isdiscontinued_fee !== undefined 
-         && fee.isdiscontinued_fee === 1 ) {
+      if ( fee.isdiscontinued_fee !== undefined
+          && fee.isdiscontinued_fee === 1) {
           fee.sort_value = 1;
           return true;
       }
-    
+
       return false;
     }).sort((a, b) => b.sort_value - a.sort_value);
   }
@@ -99,6 +110,82 @@ export class FilterFeesPipe implements PipeTransform {
     }
   });
 
+  }
+
+  validOldFeesVersions(feesObject: any) {
+    const validOldFeeVersionArray = new Array();
+    let validOldVersionArray: IVersion[] = [];
+
+    if ((feesObject.current_version !== undefined && feesObject.fee_versions.length > 1)
+        || (feesObject.current_version === undefined && feesObject.fee_versions.length === 1)) {
+      /* sort based on valid from */
+      feesObject.fee_versions = feesObject.fee_versions.
+        filter(feesVersion => feesVersion.status === 'approved')
+        .sort((a, b) => {
+          return <any>new Date(b.valid_from) - <any>new Date(a.valid_from);
+        });
+
+      feesObject.fee_versions.forEach((value, i) => {
+        if (i !== 0) {
+          // if amount is diffrent then only consider it for push need to confirm that as well
+          if (this.getAmountFromFeeVersion(value) === this.getAmountFromFeeVersion(feesObject.fee_versions[i - 1])) {
+            return;
+          }
+
+          //  set valid to date if not present for fee version from previous version
+          if (value.valid_to === null) {
+            const oldFeeVersionPreviousIndex = validOldFeeVersionArray.length - 1;
+            const new_valid_to = new Date(validOldFeeVersionArray[oldFeeVersionPreviousIndex].valid_from);
+            new_valid_to.setDate(new_valid_to.getDate() - 1);
+            value.valid_to = new_valid_to.toDateString();
+          }
+        }
+        if (feesObject.current_version === undefined && feesObject.fee_versions.length === 1) {
+            //  set valid to date if not present for fee version from previous version
+            if (value.valid_to === null) {
+              const oldFeeVersionPreviousIndex = 0 ;
+              const new_valid_to = new Date(validOldFeeVersionArray[oldFeeVersionPreviousIndex].valid_from);
+              new_valid_to.setDate(new_valid_to.getDate() - 1);
+              value.valid_to = new_valid_to.toDateString();
+            }
+        }
+
+        validOldFeeVersionArray.push(value);
+      });
+    }
+
+  if ((feesObject.current_version !== undefined && validOldFeeVersionArray.length > 1)
+  || (feesObject.current_version === undefined && validOldFeeVersionArray.length === 1)) {
+      validOldVersionArray = validOldFeeVersionArray.filter(feesVersion => this.getValidFeeVersionsBasedOnDate(feesVersion));
+      return validOldVersionArray;
+    } else {
+      return validOldVersionArray = [];
+    }
+  }
+
+  getValidFeeVersionsBasedOnDate(feeVersion: IVersion) {
+    const feesLimitDate = new Date();
+    /* Check valid fees till 6 months  */
+    feesLimitDate.setMonth(feesLimitDate.getMonth() - 6);
+
+    const vaidFrom = feeVersion.valid_from;
+    const valid_to = feeVersion.valid_to;
+
+    if ((vaidFrom != null && <any>new Date(vaidFrom) > feesLimitDate) ||
+      (valid_to != null && <any>new Date(valid_to) > feesLimitDate)) {
+      return true;
+    }
+    return false;
+  }
+
+  getAmountFromFeeVersion(feeVersion: any) {
+    if (feeVersion['volume_amount'] != null) {
+      return feeVersion['volume_amount'].amount;
+    } else if (feeVersion['flat_amount'] != null) {
+      return feeVersion['flat_amount'].amount;
+    } else if (feeVersion['percentage_amount'] != null) {
+      return feeVersion['percentage_amount'].percentage;
+    }
   }
 
   filterByAmount(fees, filter): IFee[] {
