c32528418003f9f2f57129da084f4217a8c838db
marianadpereira <mariana.pereira@hmcts.net>
2021-03-18 16:34:09 +0000

    fixing helpers saucelabs.conf

--- acceptance-tests/saucelabs.conf.js
+++ acceptance-tests/saucelabs.conf.js
@@ -56,8 +56,8 @@ const setupConfig = {
     },
     SauceLabsReportingHelper: { require: './test/end-to-end/helpers/SauceLabsReportingHelper.js' },
     // MiscHelper: { require: './test/end-to-end/helpers/misc.js' },
-    NumberHelpers: { require: './test/end-to-end/helpers/number_utils.js' },
-    StringHelper: { require: './test/end-to-end/helpers/string_utils.js' },
+    // NumberHelpers: { require: './test/end-to-end/helpers/number_utils.js' },
+    // StringHelper: { require: './test/end-to-end/helpers/string_utils.js' },
     UtilsHelper: { require: './test/end-to-end/helpers/utils.js' }
   },
   plugins: {
