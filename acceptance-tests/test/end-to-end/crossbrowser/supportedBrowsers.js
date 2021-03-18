2f8d6d812929d903b187396fcdbc4f9e01d05164
marianadpereira <mariana.pereira@hmcts.net>
2021-03-18 12:23:20 +0000

    adding config files

---
+++ acceptance-tests/test/end-to-end/crossbrowser/supportedBrowsers.js
@@ -0,0 +1,35 @@
+const LATEST_MAC = 'macOS 10.15';
+const LATEST_WINDOWS = 'Windows 10';
+
+const supportedBrowsers = {
+  chrome: {
+    chrome_win_latest: {
+      browserName: 'chrome',
+      platformName: LATEST_WINDOWS,
+      browserVersion: 'latest',
+      'sauce:options': { name: 'FeeAndPay: WIN_CHROME_LATEST' }
+    },
+    chrome_mac_latest: {
+      browserName: 'chrome',
+      platformName: LATEST_MAC,
+      browserVersion: 'latest',
+      'sauce:options': { name: 'FeeAndPay: MAC_CHROME_LATEST' }
+    }
+  },
+  firefox: {
+    firefox_win_latest: {
+      browserName: 'firefox',
+      platformName: LATEST_WINDOWS,
+      browserVersion: 'latest',
+      'sauce:options': { name: 'FeeAndPay: WIN_FIREFOX_LATEST' }
+    },
+    firefox_mac_latest: {
+      browserName: 'firefox',
+      platformName: LATEST_MAC,
+      browserVersion: 'latest',
+      'sauce:options': { name: 'FeeAndPay: MAC_FIREFOX_LATEST' }
+    }
+  }
+};
+
+module.exports = supportedBrowsers;
