01dc6014d87530fa933977288889834aa5ab9a19
santu412 <490436@ambgb000021.home>
2021-03-19 16:06:38 +0000

    PAY-3408

--- security.sh
+++ security.sh
@@ -2,28 +2,21 @@
 #echo "${SECURITYCONTEXT}" > /zap/security.context 
 zap-x.sh -d -host 0.0.0.0 -port 1001 -config globalexcludeurl.url_list.url.regex='(http.?:\/\/idam.*state=.*)' -config api.disablekey=true -config scanner.attackOnStart=true -config view.mode=attack -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true /dev/null 2>&1 &
 i=0
-while !(curl -s http://0.0.0.0:1001) >/dev/null; do
-  i=$(((i + 1) % 4))
-  sleep .1
-done
-
-echo "ZAP has successfully started"
-zap-cli --zap-url http://0.0.0.0 -p 1001 status -t 120
-zap-cli --zap-url http://0.0.0.0 -p 1001 open-url "${TEST_URL}"
-zap-cli --zap-url http://0.0.0.0 -p 1001 spider ${TEST_URL}
-zap-cli --zap-url http://0.0.0.0 -p 1001 active-scan --scanners all --recursive "${TEST_URL}"
-zap-cli --zap-url http://0.0.0.0 -p 1001 report -o activescan.html -f html
-zap-cli --zap-url http://0.0.0.0 -p 1001 report -o activescanReport.xml -f xml
-echo 'Changing owner from $(id -u):$(id -g) to $(id -u):$(id -u)'
-chown -R $(id -u):$(id -u) activescan.html
-chown -R $(id -u):$(id -u) activescanReport.xml
-
-cp *.html functional-output/
-cp activescanReport.xml functional-output/
-
-zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Low --exit-code False
-curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
-cp *.* functional-output/
+while !(curl -s http://0.0.0.0:1001) > /dev/null
+  do
+    i=$(( (i+1) %4 ))
+    sleep .1
+  done
+  echo "ZAP has successfully started"
+  zap-cli --zap-url http://0.0.0.0 -p 1001 status -t 120
+  zap-cli --zap-url http://0.0.0.0 -p 1001 open-url "${TEST_URL}"
+  zap-cli --zap-url http://0.0.0.0 -p 1001 spider ${TEST_URL}
+  zap-cli --zap-url http://0.0.0.0 -p 1001 active-scan --scanners all --recursive "${TEST_URL}"
+  zap-cli --zap-url http://0.0.0.0 -p 1001 report -o activescan.html -f html
+  echo 'Changing owner from $(id -u):$(id -g) to $(id -u):$(id -u)'
+  chown -R $(id -u):$(id -u) activescan.html
+  cp *.html functional-output/
+  zap-cli -p 1001 alerts -l Medium
 
 echo
 echo ZAP Security vulnerabilities were found that were not ignored
