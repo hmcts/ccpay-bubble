3c340547da65162f858ac11819e73287fcc71594
Satish Ramachandrappa <satish.ramachandrappa@hmcts.net>
2021-03-22 14:24:12 +0000

    Case disabled validation now pointing to payment api pr, this need to be reverted before merge to master

--- acceptance-tests/test/end-to-end/helpers/utils.js
+++ acceptance-tests/test/end-to-end/helpers/utils.js
@@ -41,7 +41,8 @@ async function getServiceToken(service) {
 async function CaseValidation(flag) {
   logger.info(`${flag} case validation`);
 
-  const paymentBaseUrl = `http://payment-api-${env}.service.core-compute-${env}.internal`;
+  // const paymentBaseUrl = `http://payment-api-${env}.service.core-compute-${env}.internal`;
+  const paymentBaseUrl = `https://payment-api-pr-743.service.core-compute-preview.internal`;
   const disablePath = `/api/ff4j/store/features/caseref-validation/${flag}`;
   // eslint-disable-next-line global-require
 
