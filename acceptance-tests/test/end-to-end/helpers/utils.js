80d1c66abeba660be22c862d2ed1c1a73374202a
“Kumar <Kumarmcamadura@gmail.com>
2021-03-26 13:34:52 +0000

    pointing all backend pr to aat environment

--- acceptance-tests/test/end-to-end/helpers/utils.js
+++ acceptance-tests/test/end-to-end/helpers/utils.js
@@ -41,7 +41,7 @@ async function getServiceToken(service) {
 async function CaseValidation(flag) {
   logger.info(`${flag} case validation`);
 
-  // const paymentBaseUrl = `http://payment-api-${env}.service.core-compute-${env}.internal`;
+  const paymentBaseUrl = `http://payment-api-${env}.service.core-compute-${env}.internal`;
   const disablePath = `/api/ff4j/store/features/caseref-validation/${flag}`;
   // eslint-disable-next-line global-require
 
