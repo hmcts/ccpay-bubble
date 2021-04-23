ca8e86161c5c26ba2eae7e9fc6b524230b19eeb0
“Kumar <Kumarmcamadura@gmail.com>
2021-04-23 14:43:12 +0100

    Fee details issue fixes

--- src/app/components/fee-details/fee-details.component.ts
+++ src/app/components/fee-details/fee-details.component.ts
@@ -3,6 +3,7 @@ import { IVersion } from './../../../../projects/fee-register-search/src/lib/int
 import { Component, EventEmitter, Input, OnInit, Output, OnChanges } from '@angular/core';
 import { IFee } from '../../../../projects/fee-register-search/src/lib/interfaces';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { Console } from 'console';
 
 
 
