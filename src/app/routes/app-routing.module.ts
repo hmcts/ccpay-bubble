dc6233ecf9ad2969283dc304b0765a57c6d96fd6
kumarmadurai <kumar.dhanasamy@hmcts.net>
2021-08-12 09:29:14 +0100

    Changes

--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -8,6 +8,7 @@ import { ConfirmationGuard } from 'src/app/components/confirmation/route-guards/
 import { ServiceFailureComponent } from 'src/app/shared/components/service-failure/service-failure.component';
 import { ViewPaymentComponent } from '../components/view-payment/view-payment.component';
 import { FeeSearchComponent } from '../components/fee-search/fee-search.component';
+import { RefundListComponent } from '../components/refund-list/refund-list.component';
 import { PaymentHistoryComponent } from '../components/payment-history/payment-history.component';
 import { CcdSearchComponent } from '../components/ccd-search/ccd-search.component';
 import { MVPGuard } from '../route-guards/mvp-guard.service';
@@ -55,6 +56,10 @@ const routes: Routes = [
     path: '',
     pathMatch: 'full',
     redirectTo: 'ccd-search'
+  },
+  {
+    path: 'refund-list/:ccdCaseNumber',
+    component: RefundListComponent
   },
   {
     path: 'payment-history/:ccdCaseNumber',
