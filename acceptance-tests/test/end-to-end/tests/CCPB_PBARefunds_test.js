634f6432c8edb4ab016fe056d015b5314ebaf72e
vigneshpushkaranHMCTS <vignesh.pushkaran@hmcts.net>
2021-09-16 14:39:42 +0100

    Squashed commit of the following:
    commit 67beb52d9f733e9a27546f6143fa7e3dd5fc6360
    Merge: 02997efa 3950941d
    Author: vigneshpushkaranHMCTS <vignesh.pushkaran@hmcts.net>
    Date:   Thu Sep 16 14:38:43 2021 +0100
    
        Merge branch 'REFUNDS' into ORDERS-REMISSIONS-REFUNDS
    
    commit 02997efa463963e26f41b10e8f918586bc2379e4
    Merge: 3ba8b7b0 f8486e3f
    Author: Pettedson John <pettedson.john@hmcts.net>
    Date:   Tue Sep 14 22:27:22 2021 +0100
    
        Merge branch 'ORDERS-REMISSIONS-REFUNDS' of https://github.com/hmcts/ccpay-bubble into ORDERS-REMISSIONS-REFUNDS
    
    commit 3ba8b7b01545f3ded494ac6827f5775e52e132c0
    Author: Pettedson John <pettedson.john@hmcts.net>
    Date:   Tue Sep 14 22:27:03 2021 +0100
    
        Adding the rought Front End tests for the Approver Journey.....

---
+++ acceptance-tests/test/end-to-end/tests/CCPB_PBARefunds_test.js
@@ -0,0 +1,77 @@
+const CCPBATConstants = require('./CCPBAcceptanceTestConstants');
+
+const nightlyTest = process.env.NIGHTLY_TEST;
+
+const bulkScanApiCalls = require('../helpers/utils');
+
+const { Logger } = require('@hmcts/nodejs-logging');
+
+const logger = Logger.getLogger('CCPB_PBARefunds_test.js');
+
+const successResponse = 202;
+
+Feature('CC Pay Bubble Acceptance Tests For Refunds and Remissions').retry(CCPBATConstants.retryScenario);
+
+/*BeforeSuite(async I => {
+  const response = await bulkScanApiCalls.toggleOffCaseValidation();
+  I.wait(CCPBATConstants.fiveSecondWaitTime);
+  if (response === successResponse) {
+    logger.info('Disabled CCD validation');
+  }
+});
+
+AfterSuite(async I => {
+  const response = await bulkScanApiCalls.toggleOnCaseValidation();
+  I.wait(CCPBATConstants.fiveSecondWaitTime);
+  if (response === successResponse) {
+    logger.info('Enabled CCD validation');
+  }
+});*/
+
+Scenario.skip('Refund a Successful PBA Payment through the Payments', async I => {
+
+  console.log('Starting the PBA Payment');
+  const ccd_case_number = await bulkScanApiCalls.createAPBAPayment("90.00");
+  console.log('CCD Case Number : ' + ccd_case_number);`
+
+  I.login('robreallywantsccdaccess@mailinator.com', 'Testing1234');
+  await miscUtils.multipleSearch(CaseSearch, I, ccd_case_number);
+  InitiateRefunds.issueRefundJourney('Payments');
+});
+
+Scenario.skip('Refund a Successful PBA Payment through the Service Requests', async I => {
+
+  console.log('Starting the PBA Payment');
+  const ccd_case_number = await bulkScanApiCalls.createAPBAPayment("90.00");
+  console.log('CCD Case Number : ' + ccd_case_number);`
+
+  I.login('robreallywantsccdaccess@mailinator.com', 'Testing1234');
+  await miscUtils.multipleSearch(CaseSearch, I, ccd_case_number);
+  InitiateRefunds.issueRefundJourney('Payments');
+});
+
+Scenario.skip('Refund List Page and Review Refund Journey', async I => {
+
+  console.log('Starting the PBA Payment');
+  const ccd_case_number = await bulkScanApiCalls.createAPBAPayment("90.00");
+  console.log('CCD Case Number : ' + ccd_case_number);`
+
+  I.login('robreallywantsccdaccess@mailinator.com', 'Testing1234');
+  await miscUtils.multipleSearch(CaseSearch, I, ccd_case_number);
+  const refund_reference = InitiateRefunds.issueRefundJourney();
+  RefundsList.reviewRefundJourney(refund_reference);
+});
+
+Scenario.skip('Refund List Page and Approver Journey', async I => {
+
+  console.log('Starting the PBA Payment');
+  const ccd_case_number = await bulkScanApiCalls.createAPBAPayment("90.00");
+  console.log('CCD Case Number : ' + ccd_case_number);`
+
+  I.login('robreallywantsccdaccess@mailinator.com', 'Testing1234');
+  await miscUtils.multipleSearch(CaseSearch, I, ccd_case_number);
+  const refund_reference = InitiateRefunds.issueRefundJourney();
+  RefundsList.approveRefundJourney(refund_reference);
+});
+
+
---
+++ acceptance-tests/test/end-to-end/tests/config/CCPBConfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+  TestCaseWorkerUserName: process.env.CASE_WORKER_USER_NAME,
+  TestCaseWorkerPassword: process.env.CASE_WORKER_PASSWORD,
+  TestRefundsApproverUserName: process.env.REFUNDS_APPROVER_USER_NAME,
+  TestRefundsApproverPassword: process.env.REFUNDS_APPROVER_PASSWORD,
+  TestClientID: process.env.CLIENT_ID,
+  TestClientSecret: process.env.CLIENT_SECRET,
+  TestRedirectURI: process.env.CLIENT_REDIRECT_URI,
+  TestAccountNumberActive : process.env.PBA_ACCOUNT_NUMBER_ACTIVE,
+  TestCMCSecret : process.env.CMC_S2S_SERVICE_SECRET,
+  TestRunningEnvironment : process.env.RUNNING_ENV
+};
