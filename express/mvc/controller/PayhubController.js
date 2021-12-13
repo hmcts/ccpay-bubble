cf6cc5385dc0bb1bb5c86a3d42f6c9cc8ed562d8
santu412 <490436@ambgb000021.home>
2021-12-13 19:49:07 +0000

    refunds

--- express/mvc/controller/PayhubController.js
+++ express/mvc/controller/PayhubController.js
@@ -4,6 +4,7 @@ const config = require('config');
 const request = require('request-promise-native');
 const LaunchDarkly = require('launchdarkly-node-client-sdk');
 const HttpStatusCodes = require('http-status-codes');
+const { Logger } = require('@hmcts/nodejs-logging');
 
 const ccpayBubbleLDclientId = config.get('secrets.ccpay.launch-darkly-client-id');
 const LDprefix = config.get('environment.ldPrefix');
@@ -126,6 +127,7 @@ class PayhubController {
         })
         .catch(err => {
           res.status(500).json({ err, success: false });
+       
         });
     }
     return Promise.reject(new Error('Missing url parameter')).catch(err => {
@@ -301,6 +303,52 @@ class PayhubController {
       });
   }
 
+  getPbaAccountList(req, res) {
+    return this.payhubService.getPbaAccountList(req)
+      .then(result => {
+        Logger.getLogger('Get-User-Details').info({ result });
+        res.status(200).json(result);
+      })
+      .catch(error => {
+        if (error.statusCode) {
+          res.status(error.statusCode).json(error.message);
+        } else {
+          res.status(500).json(error);
+        }
+      });
+  }
+
+  postPBAAccountPayment(req, res, appInsights) {
+    return this.payhubService.postPBAAccountPayment(req, appInsights)
+      .then(result => {
+        res.status(200).json({ data: result, success: true });
+      })
+      .catch(error => {
+        if (error.statusCode) {
+          res.status(error.statusCode).json({ err: error.message, success: false });
+        } else {
+          res.status(500).json({ err: error.message, success: false });
+        }
+      });
+  }
+  postWays2PayCardPayment(req, res, appInsights) {
+    return this.payhubService.postWays2PayCardPayment(req, appInsights)
+      .then(result => {
+        Logger.getLogger('Get-User-Details').info({ result });
+        res.status(200).json(result);
+      })
+      .catch(error => {
+        if (error.statusCode) {
+          res.status(error.statusCode).json({
+            err: error.message,
+            success: false
+          });
+        } else {
+          res.status(500).json({ err: error, success: false });
+        }
+      });
+  }
+
   getPaymentGroup(req, res) {
     return this.payhubService.getPaymentGroup(req)
       .then(result => {
@@ -432,7 +480,7 @@ class PayhubController {
       })
       .catch(error => {
         if (error.statusCode) {
-          res.status(400).json(error);
+          res.status(400).send(error);
         }
       });
   }
