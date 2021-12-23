402b383f28861042054578c363b70787b4b10b6c
Pettedson John <pettedson.john@hmcts.net>
2021-12-23 14:45:08 +0000

    Code Fix changing the show flag to false....

--- acceptance-tests/codecept.conf.js
+++ acceptance-tests/codecept.conf.js
@@ -17,7 +17,7 @@ exports.config = {
       waitForAction,
       // waitForNavigation: 'networkidle0',
       waitForNavigation: 'domcontentloaded',
-      show: true,
+      show: false,
       restart: true,
       keepCookies: false,
       keepBrowserState: true,
