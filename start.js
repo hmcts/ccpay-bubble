229042a84ba6313eb3c019541a98dfee990ac19b
kumarmadurai <kumar.dhanasamy@hmcts.net>
2022-02-16 20:16:29 +0000

    changes

--- start.js
+++ start.js
@@ -20,12 +20,13 @@ const app = require('./server')(security(appInsights), appInsights),
   https = require('https'),
   http = require('http');
 // reverse proxy handles tls in non local environments
-  if (process.env.NODE_ENV === 'development') {
-    const crtLocation = config.get('certs.crt'),
-      keyLocation = config.get('certs.key'),
-      cert = fs.readFileSync(crtLocation),
-      key = fs.readFileSync(keyLocation);
-    https.createServer({ key, cert }, app).listen(port);
-  } else {
-    http.createServer(app).listen(port);
-  }
+
+if (process.env.NODE_ENV === 'development') {
+  const crtLocation = config.get('certs.crt'),
+    keyLocation = config.get('certs.key'),
+    cert = fs.readFileSync(crtLocation),
+    key = fs.readFileSync(keyLocation);
+  https.createServer({ key, cert }, app).listen(port);
+} else {
+  http.createServer(app).listen(port);
+}
