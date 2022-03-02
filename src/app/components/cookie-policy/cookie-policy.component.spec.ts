ee55fce24c6c54f87d292f4cd2cff9cd2941a1e4
santu412 <490436@ambgb000021.home>
2022-03-02 21:51:22 +0000

    cb

--- src/app/components/cookie-policy/cookie-policy.component.spec.ts
+++ src/app/components/cookie-policy/cookie-policy.component.spec.ts
@@ -1,65 +1,70 @@
-// import { Component } from '@angular/core';
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { CookiePolicyComponent } from './cookie-policy.component';
+import { Component } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CookieService } from 'src/app/services/cookie/cookie.service';
+import { CookiePolicyComponent } from './cookie-policy.component';
 
-// describe('CookiePolicyComponentTest', () => {
+describe('CookiePolicyComponentTest', () => {
+  let cookieService: any;
+  @Component({selector: 'app-header', template: ''})
+  class HeaderStubComponent {
+  }
 
-//   @Component({selector: 'app-app-header', template: ''})
-//   class HeaderStubComponent {
-//   }
+  @Component({selector: 'app-footer', template: ''})
+  class FooterStubComponent {
+  }
 
-//   @Component({selector: 'app-app-footer', template: ''})
-//   class FooterStubComponent {
-//   }
+  @Component({
+    selector: `exui-app-host-dummy-component`,
+    template: `<app-cookie-policy/>`
+  })
+  class TestDummyHostComponent {
+    public cookiePolicy: CookiePolicyComponent;
+  }
+  const testHostComponent = TestDummyHostComponent;
+  let component: CookiePolicyComponent;
+  let fixture: ComponentFixture<CookiePolicyComponent>;
 
-//   @Component({
-//     selector: `app-app-host-dummy-component`,
-//     template: `<app-cookie-policy/>`
-//   })
-//   class TestDummyHostComponent {
-//     public cookiePolicy: CookiePolicyComponent;
-//   }
-//   const testHostComponent = TestDummyHostComponent;
-//   let component: CookiePolicyComponent;
-//   let fixture: ComponentFixture<CookiePolicyComponent>;
+  beforeEach(async(() => {
+    cookieService = jasmine.createSpyObj('CookieService', ['setCookie', 'checkCookie', 'getCookie']);
+    TestBed.configureTestingModule({
+      declarations: [ HeaderStubComponent, FooterStubComponent, CookiePolicyComponent ],
+      providers: [
+        { provide: CookieService, useValue: cookieService }
+        ],
+      imports: [
+        RouterTestingModule
+      ]
+    })
+      .compileComponents();
+  }));
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ HeaderStubComponent, FooterStubComponent, CookiePolicyComponent ],
-//       imports: [
-//         RouterTestingModule
-//       ]
-//     })
-//       .compileComponents();
-//   }));
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CookiePolicyComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(CookiePolicyComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-//   it('should be created by angular', () => {
-//     expect(fixture).not.toBeNull();
-//   });
-//   it('should include 4 security cookies', () => {
-//     expect(component.countCookies(component.SECURITY)).toBe(4);
-//   });
-//   it ('should return the __userid__ cookie as an identity cookie', () => {
-//     const cookieName = component.cookiesByCat(component.IDENTIFY)[0].name;
-//     expect (cookieName).toBe('__userid__');
-//   });
-//   it ('cookiesByCat should be consistent with countCookies', () => {
-//     const cookies = component.cookiesByCat(component.SECURITY);
-//     let cc = 0;
-//     for (const ccc of cookies) {
-//       expect(ccc.cat).toBe(component.SECURITY);
-//       cc = cc + 1;
-//     }
-//     expect (cc).toEqual(component.countCookies(component.SECURITY));
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  it('should be created by angular', () => {
+    expect(fixture).not.toBeNull();
+  });
+  it('should include 4 security cookies', () => {
+    expect(component.countCookies(component.SECURITY)).toBe(4);
+  });
+  it ('should return the __userid__ cookie as an identity cookie', () => {
+    const cookieName = component.cookiesByCat(component.IDENTIFY)[0].name;
+    expect (cookieName).toBe('__userid__');
+  });
+  it ('cookiesByCat should be consistent with countCookies', () => {
+    const cookies = component.cookiesByCat(component.SECURITY);
+    let cc = 0;
+    for (const ccc of cookies) {
+      expect(ccc.cat).toBe(component.SECURITY);
+      cc = cc + 1;
+    }
+    expect (cc).toEqual(component.countCookies(component.SECURITY));
+  });
+});
