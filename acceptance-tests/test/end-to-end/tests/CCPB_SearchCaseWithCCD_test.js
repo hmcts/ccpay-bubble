a45bb3e9edafd040532d31a0ec46a3307346dcd1
Pettedson John <pettedson.john@hmcts.net>
2022-03-24 12:52:28 +0000

    Adding the relevant lint Fixes to the tests for the purpose of Testing....

--- acceptance-tests/test/end-to-end/tests/CCPB_SearchCaseWithCCD_test.js
+++ acceptance-tests/test/end-to-end/tests/CCPB_SearchCaseWithCCD_test.js
@@ -31,18 +31,16 @@ AfterSuite(async I => {
 });
 
 Scenario('Search for a case with actual case number from CCD', async I => {
-
-    I.login(testConfig.TestDivorceCaseWorkerUserName, testConfig.TestDivorceCaseWorkerPassword);
-    I.see('Search for a case');
-    I.see('What do you want to search for?');
-    I.see('CCD case reference or exception record');
-    I.see('Payment Asset Number (DCN)');
-    I.see('Case Transaction');
-    I.see('Payment history');
-    I.see('Reports');
-    await I.searchForCorrectCCDNumber();
-    I.Logout();
-
+  I.login(testConfig.TestDivorceCaseWorkerUserName, testConfig.TestDivorceCaseWorkerPassword);
+  I.see('Search for a case');
+  I.see('What do you want to search for?');
+  I.see('CCD case reference or exception record');
+  I.see('Payment Asset Number (DCN)');
+  I.see('Case Transaction');
+  I.see('Payment history');
+  I.see('Reports');
+  await I.searchForCorrectCCDNumber();
+  I.Logout();
 }).tag('@nightly');
 
 Scenario('Search for a case with actual case for Telephony flow', async I => {
@@ -54,7 +52,6 @@ Scenario('Search for a case with actual case for Telephony flow', async I => {
 }).tag(' @nightly');
 
 Scenario('Amount Due case for Telephony flow', async I => {
-
   I.login(testConfig.TestDivorceCaseWorkerUserName, testConfig.TestDivorceCaseWorkerPassword);
   const responseoff = await bulkScanApiCalls.toggleOffCaseValidation();
   I.wait(CCPBATConstants.fiveSecondWaitTime);
@@ -63,17 +60,15 @@ Scenario('Amount Due case for Telephony flow', async I => {
   }
   await I.AmountDueCaseForTelephonyFlow();
   I.Logout();
-
 }).tag('@nightly @crossbrowser');
 
 Scenario('Remove fee from case transaction page Telephony flow', async I => {
-    I.login(testConfig.TestDivorceCaseWorkerUserName, testConfig.TestDivorceCaseWorkerPassword);
-    await I.removeFeeFromCaseTransactionPageTelephonyFlow();
-    I.Logout();
+  I.login(testConfig.TestDivorceCaseWorkerUserName, testConfig.TestDivorceCaseWorkerPassword);
+  await I.removeFeeFromCaseTransactionPageTelephonyFlow();
+  I.Logout();
 }).tag(' @nightly');
 
 Scenario('Search for a case with dummy case number @nightly', async I => {
-
   I.login(testConfig.TestDivorceCaseWorkerUserName, testConfig.TestDivorceCaseWorkerPassword);
   const responseOn = await bulkScanApiCalls.toggleOnCaseValidation();
   I.wait(CCPBATConstants.fiveSecondWaitTime);
@@ -82,5 +77,4 @@ Scenario('Search for a case with dummy case number @nightly', async I => {
   }
   await I.searchForCCDdummydata();
   I.Logout();
-  
 }).tag('@pipeline');
