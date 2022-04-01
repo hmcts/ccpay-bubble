d4b32a7c33c9c3cf7f3ccd0a295845af90718588
santu412 <490436@ambgb000021.home>
2022-04-01 14:00:06 +0100

    cve

--- karma.conf.js
+++ karma.conf.js
@@ -45,7 +45,7 @@ module.exports = config => {
     colors: true,
     logLevel: config.LOG_DEBUG,
     autoWatch: false,
-    browsers: [ 'ChromeDebug' ],
+    browsers: [ 'Chrome' ],
     singleRun: true,
     watch: false
   });
