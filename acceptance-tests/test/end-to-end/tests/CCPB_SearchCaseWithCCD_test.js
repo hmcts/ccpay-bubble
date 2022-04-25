ea9c340cc60be9799da632ecf1e94d86a5b53634
Pettedson John <pettedson.john@hmcts.net>
2022-04-25 15:51:51 +0100

    Testing the pipeline for Nigtly Stability....

--- acceptance-tests/test/end-to-end/tests/CCPB_SearchCaseWithCCD_test.js
+++ acceptance-tests/test/end-to-end/tests/CCPB_SearchCaseWithCCD_test.js
@@ -39,13 +39,13 @@ Scenario('Search for a case with actual case number from CCD', async I => {
   I.see('Reports');
   await I.searchForCorrectCCDNumber();
   I.Logout();
-}).tag('@nightly');
+}).tag('@nightly @crossbrowser');
 
 Scenario('Search for a case with actual case for Telephony flow', async I => {
   I.login(testConfig.TestDivorceCaseWorkerUserName, testConfig.TestDivorceCaseWorkerPassword);
   await I.caseforTelephonyFlow();
   I.Logout();
-}).tag('@nightly');
+}).tag('@nightly @corssbrowser');
 
 Scenario('Amount Due case for Telephony flow', async I => {
   I.login(testConfig.TestDivorceCaseWorkerUserName, testConfig.TestDivorceCaseWorkerPassword);
