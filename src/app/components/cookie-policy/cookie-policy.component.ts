8db497a5308afe184e9fa6cb82c08544275b82b3
kumarmadurai <kumar.dhanasamy@hmcts.net>
2022-07-04 18:06:14 +0100

    4xx - Issue fixes

--- src/app/components/cookie-policy/cookie-policy.component.ts
+++ src/app/components/cookie-policy/cookie-policy.component.ts
@@ -26,8 +26,7 @@ export class CookiePolicyComponent implements OnInit {
       'Cookie name',
       'What this cookie is for',
       'Expires after'
-    ]
-
+    ];
 
     public cookieDetails =
       [
