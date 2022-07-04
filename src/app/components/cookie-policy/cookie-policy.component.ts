ead2bc0b29a00a18f094c9dacfe8ce0b987234c8
kumarmadurai <kumar.dhanasamy@hmcts.net>
2022-07-04 16:22:55 +0100

    cookie banner fixes

--- src/app/components/cookie-policy/cookie-policy.component.ts
+++ src/app/components/cookie-policy/cookie-policy.component.ts
@@ -9,7 +9,6 @@ import cookieManager from '@hmcts/cookie-manager';
 })
 
 export class CookiePolicyComponent implements OnInit {
-    public readonly googlePurpose = 'This helps us count how many people visit the service by tracking if you\'ve visited before';
     // Ideally this would be an enum but angular can't seem to cope with enums in templates
     public readonly USAGE = 'Usage';
     public readonly INTRO = 'Intro';
@@ -18,6 +17,7 @@ export class CookiePolicyComponent implements OnInit {
     public readonly SECURITY = 'Security';
     public readonly GOOGLE = 'Google';
     public readonly DYNATRACE = 'Dynatrace';
+    isCookiePageEnabled: boolean = false;
 
     public cookieDetails =
       [
@@ -25,12 +25,9 @@ export class CookiePolicyComponent implements OnInit {
         {name: 'rxVisitor', cat: this.USAGE, purpose: 'Generated user ID for usage tracking (Dynatrace)', expires: '2 years'},
         {name: 'ai_user', cat: this.USAGE, purpose: 'Generated user ID for usage tracking (Application Insights)', expires: '6 months'},
         {name: '_oauth2_proxy', cat: this.SECURITY, purpose: 'Used to protect your login session', expires: '4 hours'},
-        {name: '_gid', cat: this.GOOGLE, purpose: this.googlePurpose, expires: '1 day'},
-        {name: '_ga', cat: this.GOOGLE, purpose: 'This stores information about your session', expires: '2 years'},
-        {name: '_ga_XXXXXXXXXX', cat: this.GOOGLE, purpose: 'This stores information about your session', expires: '2 years'},
-        {name: '_gat_XXXXXXXXXX',
-        cat: this.GOOGLE,
-        purpose: 'This is used to control the rate at which requests to the analytics software are made', expires: '1 day'},
+        {name: '_ga', cat: this.GOOGLE, purpose: 'This helps us count how many people visit the service by tracking if you’ve visited before', expires: '2 years'},
+        {name: '_gat', cat: this.GOOGLE, purpose: 'Manages the rate at which page view requests are made', expires: '10 minutes'},
+        {name: '_gid', cat: this.GOOGLE, purpose: 'Identifies you to the service', expires: '24 hours'},
         {name: '__userid__', cat: this.IDENTIFY, purpose: 'Your user ID', expires: 'When you close your browser'},
         {name: '__auth__',
         cat: this.SECURITY,
@@ -64,5 +61,11 @@ export class CookiePolicyComponent implements OnInit {
 
   public cookiesByCat(category: string): {name: string, cat: string, purpose: string, expires: string}[] {
     return this.cookieDetails.filter(c => c.cat === category);
+  }
+  showCookiePage() {
+   this.isCookiePageEnabled = true;
+  }
+  hideCookiePage() {
+    this.isCookiePageEnabled = false;
   }
 }
