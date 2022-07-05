55857e7f499a0f368a053bb414a1da422d468f25
kumarmadurai <kumar.dhanasamy@hmcts.net>
2022-07-05 16:22:29 +0100

    pay-5617 payment status changes

--- src/app/components/cookie-details/cookie-details.component.ts
+++ src/app/components/cookie-details/cookie-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cookie-policy',
@@ -7,16 +7,8 @@ import { Component, OnInit} from '@angular/core';
 
 })
 
-export class CookieDetailsComponent implements OnInit {
+export class CookieDetailsComponent {
     // Ideally this would be an enum but angular can't seem to cope with enums in templates
-    public readonly USAGE = 'Usage';
-    public readonly INTRO = 'Intro';
-    public readonly SESSION = 'Session';
-    public readonly IDENTIFY = 'Identify';
-    public readonly SECURITY = 'Security';
-    public readonly GOOGLE = 'Google';
-    public readonly DYNATRACE = 'Dynatrace';
-
     public readonly MORESERVICE = 'Moreservice';
     public readonly FAMILY = 'Family';
     public readonly SECURE = 'Secure';
@@ -37,68 +29,27 @@ export class CookieDetailsComponent implements OnInit {
 
     public cookieDetails =
       [
-        {name: 'ccpay-bubble', cat: this.SECURITY, purpose: 'Used to secure communications with HMCTS data services', expires: '8 hours'},
-        {name: 'rxVisitor', cat: this.USAGE, purpose: 'Generated user ID for usage tracking (Dynatrace)', expires: '2 years'},
-        {name: 'ai_user', cat: this.USAGE, purpose: 'Generated user ID for usage tracking (Application Insights)', expires: '6 months'},
-        {name: '_oauth2_proxy', cat: this.SECURITY, purpose: 'Used to protect your login session', expires: '4 hours'},
-        {name: '_ga',
-        cat: this.GOOGLE,
-        purpose: 'This helps us count how many people visit the service by tracking if you’ve visited before',
-        expires: '2 years'},
-        {name: '_gat', cat: this.GOOGLE, purpose: 'Manages the rate at which page view requests are made', expires: '10 minutes'},
-        {name: '_gid', cat: this.GOOGLE, purpose: 'Identifies you to the service', expires: '24 hours'},
-        {name: '__userid__', cat: this.IDENTIFY, purpose: 'Your user ID', expires: 'When you close your browser'},
-        {name: '__auth__',
-        cat: this.SECURITY,
-        purpose: 'Information about your current system authorisations', expires: 'When you close your browser'},
-        {name: 'XSRF-TOKEN',
-        cat: this.SECURITY,
-        purpose: 'Used to protect your session against cross site scripting attacks', expires: 'When you close your browser'},
-        {name: 'dtCookie', cat: this.DYNATRACE, purpose: 'Tracks a visit across multiple request', expires: 'When session ends'},
-        {name: 'dtLatC', cat: this.DYNATRACE, purpose: 'Measures server latency for performance monitoring', expires: 'When session ends'},
-        {name: 'dtPC',
-        cat: this.DYNATRACE,
-        purpose: 'dtPC	Required to identify proper endpoints for beacon transmission; includes session ID for correlation',
-        expires: 'When session ends'},
-        {name: 'dtSa', cat: this.DYNATRACE, purpose: 'Intermediate store for page-spanning actions', expires: 'When session ends'},
-        {name: 'rxVisitor', cat: this.DYNATRACE, purpose: 'Visitor ID to correlate sessions', expires: '1 year'},
-        {name: 'rxvt', cat: this.DYNATRACE, purpose: 'Session timeout', expires: 'When session ends'},
-
         {name: 'TSxxxxxxxx', cat: this.MORESERVICE,
         purpose: 'Protects your session from tampering', expires: 'When you close your browser'},
         {name: '__state', cat: this.MORESERVICE,
         purpose: 'Identifies you to the service and secures your authentication', expires: 'When you close your browser'},
         {name: 'X_CMC', cat: this.MORESERVICE,
         purpose: 'Helps us keep track of your session', expires: 'When you close your browser'},
-
         {name: 'connect.sid', cat: this.FAMILY, purpose: 'Carries details of your current session', expires: 'When you close your browser'},
         {name: 'sessionKey', cat: this.FAMILY, purpose: 'rotects your session using encryption', expires: 'When you close your browser'},
-
         {name: 'state', cat: this.SECURE,
         purpose: 'Identifies you to the service and secures your authentication', expires: 'When session ends'},
         {name: 'ARRAfinnity', cat: this.SECURE, purpose: 'Protects your session from tampering', expires: 'When session ends'},
         {name: '_csrf', cat: this.SECURE, purpose: 'Helps protect against forgery', expires: 'When session ends'},
-
         {name: '__auth-token', cat: this.AUTHTOKEN, purpose: 'Identifies you to the service', expires: 'When you close your browser'},
-
-
         {name: 'TS01842b02', cat: this.SECURE1,
         purpose: 'Protects your session from tampering', expires: 'When you close your browser'},
         {name: '__state', cat: this.SECURE1,
         purpose: 'Identifies you to the service and secures your authentication', expires: 'When you close your browser'},
         {name: '_csrf', cat: this.SECURE1, purpose: 'Helps protect against forgery', expires: 'When you close your browser'},
-
         {name: 'session_ID', cat: this.MONEYCLAIM, purpose: 'Keeps track of your answers', expires: 'When session ends'},
         {name: 'eligibility-check', cat: this.MONEYCLAIM, purpose: 'Stores answers to eligibility questions', expires: 'Ten minutes'}
-
-
       ];
-
-
-  ngOnInit(): void {
-
-  }
-
   public countCookies(category: string): number {
     return this.cookiesByCat(category).length;
   }
