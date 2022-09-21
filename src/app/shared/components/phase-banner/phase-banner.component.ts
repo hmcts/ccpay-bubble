31e1f3551b3ed2f4402e454f5832f419c7ccd58d
abdulkhan5 <Abdul.Khan@HMCTS.NET>
2022-09-21 16:41:50 +0100

    testing iFrameDrivenImageValue

--- src/app/shared/components/phase-banner/phase-banner.component.ts
+++ src/app/shared/components/phase-banner/phase-banner.component.ts
@@ -11,9 +11,21 @@ export class PhaseBannerComponent {
   @Input() type = 'beta';
 
   myFunction () {
-    const myInput = (document.querySelector('iFrameDrivenImageValue') as HTMLInputElement).value;
-	if ((myInput != null) || (myInput.length !== 0)) {
-		window.open('https://www.smartsurvey.co.uk/s/PayBubble/?pageurl=' + myInput, '_blank');
-	}
+    const myInput = (document.getElementById('iFrameDrivenImageValue') as HTMLInputElement).value;
+    const myInput2 = (document.getElementById('iFrameDrivenImageValue2') as HTMLInputElement).value;
+    const myInput3 = (document.getElementById('iFrameDrivenImageValue3') as HTMLInputElement).value;
+    const myInput4 = (document.getElementById('iFrameDrivenImageValue4') as HTMLInputElement).value;
+    if ((myInput != null) || (myInput.length !== 0)) {
+      window.open('https://www.smartsurvey.co.uk/s/PayBubble/?pageurl=' + myInput, '_blank');
+    }
+    if ((myInput2 != null) || (myInput2.length !== 0)) {
+      window.open('https://www.smartsurvey.co.uk/s/PayBubble/?pageurl=' + myInput, '_blank');
+    }
+    if ((myInput3 != null) || (myInput3.length !== 0)) {
+      window.open('https://www.smartsurvey.co.uk/s/PayBubble/?pageurl=' + myInput, '_blank');
+    }
+    if ((myInput4 != null) || (myInput4.length !== 0)) {
+      window.open('https://www.smartsurvey.co.uk/s/PayBubble/?pageurl=' + myInput, '_blank');
+    }
   }
 }
