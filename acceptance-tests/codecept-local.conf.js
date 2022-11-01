6afcde822da729d450d65fe939cf888ca730fd7e
VamshiRudrabhatlaHMCTS <vamshi.rudrabhatla@HMCTS.net>
2022-11-01 16:43:29 +0000

    Turning OFF Chrome Browser

--- acceptance-tests/codecept-local.conf.js
+++ acceptance-tests/codecept-local.conf.js
@@ -20,7 +20,7 @@ exports.config = {
       show: false,
       restart: false,
       keepCookies: false,
-      keepBrowserState: false,
+      keepBrowserState: true,
       networkIdleTimeout: 5000,
       waitUntil: 'networkidle',
       timeout: 3000000,
