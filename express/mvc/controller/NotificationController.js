57dd2a6dae6bb8b78667a5126be7c7b77849c142
“akhil-nuthakki” <akhil.nuthakki@HMCTS.net>
2022-11-15 15:08:00 +0000

    doc-preview endpoint addition

--- express/mvc/controller/NotificationController.js
+++ express/mvc/controller/NotificationController.js
@@ -7,38 +7,6 @@ class NotificationController {
     this.notificationService = notificationService;
   }
   getRefundNotification(req, res) {
-    // return res.status(200).json(
-    //   {
-    //     notifications: [
-    //       {
-    //         contact_details: {
-    //           date_created: '2022-01-20T10:49:33.650Z',
-    //           date_updated: '2022-01-20T10:49:33.650Z',
-    //           email: 'marikumar01@gmail.com'
-    //         },
-    //         date_created: '2022-01-20T10:49:33.650Z',
-    //         date_updated: '2022-01-20T10:49:33.650Z',
-    //         notification_type: 'EMAIL',
-    //         reference: 'RF-1234-1234-1234-1111'
-    //       },
-    //       {
-    //         contact_details: {
-    //           date_created: '2022-01-20T10:49:33.650Z',
-    //           date_updated: '2022-01-20T10:49:33.650Z',
-    //           address_line: 'Flat 407, Schrier ropeworks',
-    //           city: 'Barking',
-    //           country: 'London',
-    //           county: 'London',
-    //           postal_code: 'IG117GU'
-    //         },
-    //         date_created: '2022-01-20T10:49:33.650Z',
-    //         date_updated: '2022-01-20T10:49:33.650Z',
-    //         notification_type: 'LETTER',
-    //         reference: 'RF-1234-1234-1234-1111'
-    //       }
-    //     ]
-    //   }
-    // );
     return this.notificationService.getRefundNotification(req)
       .then(result => {
         res.status(200).json(result);
@@ -64,6 +32,20 @@ class NotificationController {
         }
       });
   }
+  docPreview(req, res) {
+    return this.notificationService.docPreview(req)
+      .then(result => {
+        res.status(200).json({ data: result, success: true });
+      })
+      .catch(error => {
+        if (error.statusCode) {
+          res.status(error.statusCode).json({ err: error.message, success: false });
+        } else {
+          res.status(500).json({ err: error, success: false });
+        }
+      });
+  }
+  
 }
 
 module.exports = NotificationController;
--- express/services/NotificationService.js
+++ express/services/NotificationService.js
@@ -32,6 +32,19 @@ class NotificationService {
       json: true
     });
   }
+  docPreview(req){
+    console.log('request body: ' + JSON.stringify(req.body));
+    return this.createAuthToken().then(token => request.post({
+      uri: `${notificationUrl}/doc-preview`,
+      body: req.body,
+      headers: {
+        Authorization: `Bearer ${req.authToken}`,
+        ServiceAuthorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      },
+      json: true
+    }));
+  }
   createAuthToken() {
     const otpPassword = otp({ secret: ccpayBubbleSecret }).totp();
     const serviceAuthRequest = {
