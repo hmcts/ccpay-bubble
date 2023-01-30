10a805536bca3a18416d6998aa60801d2f9e493b
abdulkhan5 <Abdul.Khan@HMCTS.NET>
2023-01-30 11:35:39 +0000

    revert changes & add BrowserDynamicTestingModule to imports in ngModule

--- projects/view-payment/src/lib/view-payment.module.ts
+++ projects/view-payment/src/lib/view-payment.module.ts
@@ -4,6 +4,7 @@ import { ViewPaymentComponent } from './view-payment.component';
 import { SinglePaymentViewComponent } from './components/single-payment-view/single-payment-view.component';
 import { FeeComponent } from './components/shared/fee/fee.component';
 import { StatusHistoryComponent } from './components/shared/status-history/status-history.component';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { StatusHistoryComponent } from './components/shared/status-history/statu
     StatusHistoryComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserDynamicTestingModule
   ],
   exports: [ViewPaymentComponent]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -38,6 +38,7 @@ import { CookiePolicyComponent } from './components/cookie-policy/cookie-policy.
 import { CookieDetailsComponent } from './components/cookie-details/cookie-details.component';
 import { windowProvider, windowToken } from '../window';
 import { CookieTableComponent } from './components/cookie-table/cookie-table.component';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
 
 const nonProductionProviders = [{
   provide: HTTP_INTERCEPTORS,
@@ -77,7 +78,8 @@ const nonProductionProviders = [{
     FeeRegisterSearchModule,
     PaymentLibModule,
     NoopAnimationsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    BrowserDynamicTestingModule
   ],
   providers: [
     PaybubbleHttpClient,
