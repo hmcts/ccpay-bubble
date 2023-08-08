b4554e7ac4af2f1f6727dbfe34700c8d91af5cd9
shravan mechineni <shravanmechineni5@gmail.com>
2023-08-08 09:50:56 +0100

    change browser show to false

--- acceptance-tests/codecept-playwright.conf.js
+++ acceptance-tests/codecept-playwright.conf.js
@@ -9,7 +9,7 @@ exports.config = {
   helpers: {
     Playwright: {
       url: CONF.e2e.frontendUrl,
-      show: true,
+      show: false,
       browser: 'chromium',
       waitForTimeout: 60001,
       waitForAction: 500,
