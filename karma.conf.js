0782512079da8704152d302c3a255f186d91913f
it.consulting@tutanota.com <it.consulting@tutanota.com>
2023-10-06 16:03:03 +0100

    PAY-6448: Node 18 Upgrade CCPay-Bubble        [PAY-6448] (https://tools.hmcts.net/jira/browse/PAY-6448).

--- karma.conf.js
+++ karma.conf.js
@@ -1,56 +1,40 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-const karmaJasmine = require('karma-jasmine');
-const karmaJasmineHtmlReporter = require('karma-jasmine-html-reporter');
-const karmaPhantomJsLauncher = require('karma-phantomjs-launcher');
-const karmaIntlShim = require('karma-intl-shim');
-const karmaCoverage = require('karma-coverage');
-const karmaAngularPluginsKarma = require('@angular-devkit/build-angular/plugins/karma');
-const karmaChromeLauncher = require('karma-chrome-launcher');
-const karmaCustomLogger = require('karma-spec-reporter');
-
-
-module.exports = config => {
+module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular', 'intl-shim'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      karmaJasmine,
-      karmaJasmineHtmlReporter,
-      karmaPhantomJsLauncher,
-      karmaIntlShim,
-      // require('./en-us.js'),
-      karmaCoverage,
-      karmaAngularPluginsKarma,
-      karmaChromeLauncher,
-      karmaCustomLogger
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
-    // leave Jasmine Spec Runner output visible in browser
-    client: { clearContext: false },
-    customLaunchers: {
-      ChromeDebug: {
-        base: 'Chrome',
-        flags: [ '--remote-debugging-port=9333', '--headless'],
-        debug: true
+    client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
       },
-      HeadlessChrome: {
-        base: 'ChromeHeadless',
-        flags: [ '--no-sandbox' ]
-      }
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: 'coverage/',
-      reporters: [{ type: 'lcovonly', subdir: '.', file: 'lcov.info' }],
-      fixWebpackSourcePaths: true
+      dir: require('path').join(__dirname, './coverage/ccpay-bubble'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: 'lcov' }
+      ]
     },
-    reporters: ['spec', 'kjhtml', 'coverage'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_WARN,
-    autoWatch: false,
-    browsers: [ 'ChromeHeadless' ],
-    singleRun: true,
-    watch: false
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['Chrome'],
+    restartOnFileChange: true
   });
 };
