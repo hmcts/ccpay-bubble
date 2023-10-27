82a4b8317d2f3b65cfdd4eebf056d642831e9a97
it.consulting@tutanota.com <it.consulting@tutanota.com>
2023-10-27 12:53:53 +0100

    PAY-6454: Node 18 Upgrade CCPay-Bubble        [PAY-6454] (https://tools.hmcts.net/jira/browse/PAY-6454).

--- src/app/components/ccd-search/ccd-search.component.ts
+++ src/app/components/ccd-search/ccd-search.component.ts
@@ -152,8 +152,7 @@ export class CcdSearchComponent implements OnInit {
               // tslint:disable-next-line:max-line-length
               let url = this.takePayment ? `?selectedOption=${this.selectedValue}&dcn=${this.dcnNumber}&view=case-transactions&takePayment=${this.takePayment}&servicerequest=${this.servicerequest}` : `?selectedOption=${this.selectedValue}&dcn=${this.dcnNumber}&view=case-transactions&servicerequest=${this.servicerequest}`;
               url = url.replace(/[\r\n]+/g, ' ');
-              this.paymentGroupService.getBSPaymentsByCCD(this.ccdCaseNumber).then( result => {
-
+              this.paymentGroupService.getBSPaymentsByCCD(this.ccdCaseNumber).then( response => {const result = JSON.parse(JSON.parse(response.toString()))
               if (this.caseResponse.case) {
                 this.caseType = this.caseResponse.case;
               } else {
@@ -161,7 +160,8 @@ export class CcdSearchComponent implements OnInit {
               }
 
               this.errorMessage = this.getErrorMessage(false);
-              if (result['data'] && result['data'].exception_record_reference && result['data'].ccd_reference) {
+
+                if (result['data'] && result['data'].exception_record_reference && result['data'].ccd_reference) {
                 if (this.caseResponse.case) {
                   this.caseType = this.caseResponse.exception;
                 }
