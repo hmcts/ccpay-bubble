4e3febfaad777baa0c4d285acff04c7c1d0eee31
StoeBenHMCTS <Ben.Stone@HMCTS.NET>
2024-02-16 13:27:40 +0000

    adds a footer component

---
+++ src/app/components/hmcts-global-footer/hmcts-global-footer.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { Helper, Navigation } from '../../containers/footer/footer.model';
+
+@Component({
+  selector: 'ccpay-bubble-hmcts-global-footer',
+  templateUrl: './hmcts-global-footer.component.html'
+})
+export class HmctsGlobalFooterComponent {
+  @Input() public help: Helper;
+  @Input() public navigation: Navigation;
+}
