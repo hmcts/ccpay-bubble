336381e5dc3ce37dd50fa795b6292b21875577be
shravanmechineni <shravanmechineni5@gmail.com>
2024-03-26 10:20:43 +0000

    fix telephony test

--- acceptance-tests/test/end-to-end/pages/steps_file.js
+++ acceptance-tests/test/end-to-end/pages/steps_file.js
@@ -930,8 +930,7 @@ module.exports = () => actor({
     this.wait(CCPBConstants.fiveSecondWaitTime);
     this.waitInUrl('pcipal', 2);
     this.click('Cancel');
-    //this.click('Finish Session');
-    this.wait(CCPBConstants.fiveSecondWaitTime);
+    this.click('Finish Session');
   },
 
   async removeFeeFromCaseTransactionPageTelephonyFlow() {
