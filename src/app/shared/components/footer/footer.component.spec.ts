33f23ddeb07180e49615dc756396b114edce983f
StoeBenHMCTS <Ben.Stone@HMCTS.NET>
2024-04-05 17:00:40 +0100

    fixes broken test import

--- src/app/shared/components/footer/footer.component.spec.ts
+++ src/app/shared/components/footer/footer.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FooterComponent } from './footer.component';
+import { HmctsGlobalFooterComponent } from '../hmcts-global-footer/hmcts-global-footer.component';
+import { RouterTestingModule } from '@angular/router/testing'
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -7,7 +9,10 @@ describe('FooterComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [FooterComponent]
+      declarations: [FooterComponent, HmctsGlobalFooterComponent],
+      imports: [
+        RouterTestingModule
+      ],
     });
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
