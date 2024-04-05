ba36f7b64cf8b4ecfe6e3f75b6c44981041629c2
StoeBenHMCTS <Ben.Stone@HMCTS.NET>
2024-04-05 16:35:36 +0100

    updates unit test

--- src/app/shared/components/footer/footer.component.spec.ts
+++ src/app/shared/components/footer/footer.component.spec.ts
@@ -1,59 +1,20 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, DebugElement, ViewChild } from '@angular/core';
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-// import { Store } from '@ngrx/store';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FooterComponent } from './footer.component';
 
 describe('FooterComponent', () => {
-  @Component({
-    selector: 'exui-app-host-dummy-component',
-    template: '<exui-app-footer></exui-app-footer>'
-  })
-  class TestDummyHostComponent {
-    @ViewChild(FooterComponent, { static: false })
-    public footerComponent: FooterComponent;
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  let testHostComponent: TestDummyHostComponent;
-  let testHostFixture: ComponentFixture<TestDummyHostComponent>;
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  let element: DebugElement;
 
-  beforeEach(waitForAsync(() => {
-    // const storeMock = jasmine.createSpyObj('mockStore', ['unsubscribe', 'dispatch', 'pipe']);
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [FooterComponent, TestDummyHostComponent],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
-      // providers: [{
-      //   provide: Store,
-      //   useValue: storeMock
-      // }]
-    })
-      .compileComponents();
-  }));
-
-  beforeEach(() => {
-    testHostFixture = TestBed.createComponent(TestDummyHostComponent);
-    testHostComponent = testHostFixture.componentInstance;
-  });
-
-  beforeEach(() => {
+      declarations: [FooterComponent]
+    });
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
-    element = fixture.debugElement;
+    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should be created by angular', () => {
-    expect(fixture).not.toBeNull();
   });
 });
--- src/app/shared/components/hmcts-global-footer/hmcts-global-footer.component.spec.ts
+++ src/app/shared/components/hmcts-global-footer/hmcts-global-footer.component.spec.ts
@@ -1,60 +1,39 @@
-import { Component, DebugElement, Input, Pipe, PipeTransform, ViewChild } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppConstants } from '../../../app.constants';
-import { Navigation } from '../footer/footer.model';
+import { Navigation } from '../footer/footer.model'
 import { HmctsGlobalFooterComponent } from './hmcts-global-footer.component';
 
+
 describe('HmctsGlobalFooterComponent', () => {
-    @Component({
-      selector: 'exui-app-host-dummy-component',
-      template: `<exui-app-hmcts-global-footer
-                    [reference]="iconFallbackText"
-                    [title]="type"
-                    [items]="text"></exui-app-hmcts-global-footer>`
+  let component: HmctsGlobalFooterComponent;
+  let fixture: ComponentFixture<HmctsGlobalFooterComponent>;
+
+  const navigationData: Navigation = AppConstants.FOOTER_DATA_NAVIGATION;
+
+  beforeEach(waitForAsync(() => {
+
+    TestBed.configureTestingModule({
+      declarations: [HmctsGlobalFooterComponent, ],
+      imports: [
+        RouterTestingModule
+      ],
     })
-  class TestDummyHostComponent {
-        @Input() public navigation: Navigation;
-        @ViewChild(HmctsGlobalFooterComponent, { static: false })
-        public hmctsGlobalFooterComponent: HmctsGlobalFooterComponent;
-    }
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const testHostComponent = TestDummyHostComponent;
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    let testHostFixture: ComponentFixture<TestDummyHostComponent>;
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    let el: DebugElement;
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    let de: any;
-    let component: HmctsGlobalFooterComponent;
-    let fixture: ComponentFixture<HmctsGlobalFooterComponent>;
+      .compileComponents();
+  }));
 
-    const navigationData: Navigation = AppConstants.FOOTER_DATA_NAVIGATION;
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HmctsGlobalFooterComponent);
+    component = fixture.componentInstance;
+    component.navigation = navigationData;
+    fixture.detectChanges();
+  });
 
-    beforeEach(waitForAsync(() => {
-      // eslint-disable-next-line @typescript-eslint/no-empty-function
-      const rpxTranslationServiceStub = () => ({ language: 'en', translate: () => {} });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-      TestBed.configureTestingModule({
-        declarations: [HmctsGlobalFooterComponent],
-        imports: [
-          RouterTestingModule
-        ],
-      })
-        .compileComponents();
-    }));
-
-    beforeEach(() => {
-      fixture = TestBed.createComponent(HmctsGlobalFooterComponent);
-      component = fixture.componentInstance;
-      component.navigation = navigationData;
-      fixture.detectChanges();
-    });
-
-    it('should create', () => {
-      expect(component).toBeTruthy();
-    });
-    it('should be created by angular', () => {
-      expect(fixture).not.toBeNull();
-    });
+  it('should be created by angular', () => {
+    expect(fixture).not.toBeNull();
+  });
 });
