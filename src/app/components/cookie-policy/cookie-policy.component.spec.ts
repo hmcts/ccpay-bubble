51bbfb043c011972b81e722dfe88bc8c7664e121
StoeBenHMCTS <Ben.Stone@HMCTS.NET>
2024-04-19 14:05:22 +0100

    updates unit tests

--- src/app/components/cookie-policy/cookie-policy.component.spec.ts
+++ src/app/components/cookie-policy/cookie-policy.component.spec.ts
@@ -48,9 +48,9 @@ describe('CookiePolicyComponentTest', () => {
   it('should include 4 security cookies', () => {
     expect(component.countCookies(component.SECURITY)).toBe(4);
   });
-  it ('should return the __userid__ cookie as an identity cookie', () => {
+  it ('should return the __user-info cookie as an identity cookie', () => {
     const cookieName = component.cookiesByCat(component.IDENTIFY)[0].name;
-    expect (cookieName).toBe('__userid__');
+    expect (cookieName).toBe('__user-info');
   });
   it ('cookiesByCat should be consistent with countCookies', () => {
     const cookies = component.cookiesByCat(component.SECURITY);
