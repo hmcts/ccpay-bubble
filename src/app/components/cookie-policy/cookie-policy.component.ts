110b9ff1d85c42534b0282212cee596e3de770ad
StoeBenHMCTS <Ben.Stone@HMCTS.NET>
2024-04-19 13:51:35 +0100

    updates content for cookies policy

--- src/app/components/cookie-policy/cookie-policy.component.ts
+++ src/app/components/cookie-policy/cookie-policy.component.ts
@@ -22,23 +22,12 @@ export class CookiePolicyComponent implements OnInit {
 
     public cookieDetails =
       [
-        {name: 'ccpay-bubble', cat: this.SECURITY, purpose: 'Used to secure communications with HMCTS data services', expires: '8 hours'},
-        {name: 'rxVisitor', cat: this.USAGE, purpose: 'Generated user ID for usage tracking (Dynatrace)', expires: '2 years'},
-        {name: 'ai_user', cat: this.USAGE, purpose: 'Generated user ID for usage tracking (Application Insights)', expires: '6 months'},
-        {name: '_oauth2_proxy', cat: this.SECURITY, purpose: 'Used to protect your login session', expires: '4 hours'},
         {name: '_ga',
         cat: this.GOOGLE,
         purpose: 'This helps us count how many people visit the service by tracking if you’ve visited before',
         expires: '2 years'},
         {name: '_gat', cat: this.GOOGLE, purpose: 'Manages the rate at which page view requests are made', expires: '10 minutes'},
         {name: '_gid', cat: this.GOOGLE, purpose: 'Identifies you to the service', expires: '24 hours'},
-        {name: '__userid__', cat: this.IDENTIFY, purpose: 'Your user ID', expires: 'When you close your browser'},
-        {name: '__auth__',
-        cat: this.SECURITY,
-        purpose: 'Information about your current system authorisations', expires: 'When you close your browser'},
-        {name: 'XSRF-TOKEN',
-        cat: this.SECURITY,
-        purpose: 'Used to protect your session against cross site scripting attacks', expires: 'When you close your browser'},
         {name: 'dtCookie', cat: this.DYNATRACE, purpose: 'Tracks a visit across multiple request', expires: 'When session ends'},
         {name: 'dtLatC', cat: this.DYNATRACE, purpose: 'Measures server latency for performance monitoring', expires: 'When session ends'},
         {name: 'dtPC',
@@ -47,7 +36,16 @@ export class CookiePolicyComponent implements OnInit {
         expires: 'When session ends'},
         {name: 'dtSa', cat: this.DYNATRACE, purpose: 'Intermediate store for page-spanning actions', expires: 'When session ends'},
         {name: 'rxVisitor', cat: this.DYNATRACE, purpose: 'Visitor ID to correlate sessions', expires: '1 year'},
-        {name: 'rxvt', cat: this.DYNATRACE, purpose: 'Session timeout', expires: 'When session ends'}
+        {name: 'rxvt', cat: this.DYNATRACE, purpose: 'Session timeout', expires: 'When session ends'},
+        {name: '__user-info', cat: this.IDENTIFY, purpose: 'Your user Information', expires: 'When you close your browser'},
+        {name: '__auth-token',
+        cat: this.SECURITY,
+        purpose: 'Information about your current system authorisations', expires: 'When you close your browser'},
+        {name: 'XSRF-TOKEN',
+        cat: this.SECURITY,
+        purpose: 'Used to protect your session against cross site scripting attacks', expires: 'When you close your browser'},
+        {name: 'ccpay-bubble-cookie-preferences', cat: this.SECURITY, purpose: 'Saves application preference information', expires: '1 year'},
+        {name: 'connect.sid', cat: this.SECURITY, purpose: 'Essentail application data', expires: 'When you close your browser'}
       ];
 
 
