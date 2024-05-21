69a9c29a97e554e201b292dffa29e3fef71dc6e4
it.consulting@tutanota.com <it.consulting@tutanota.com>
2024-05-21 10:43:48 +0100

    PAY-6748: Paybubble footer details        [PAY-6748] (https://tools.hmcts.net/jira/browse/PAY-6748).

---
+++ src/app/shared/components/footer/footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Navigation } from './footer.model';
+
+@Component({
+  selector: 'ccpay-bubble-app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+
+export class FooterComponent implements OnInit {
+
+  constructor() {}
+
+  public ngOnInit() {}
+}
