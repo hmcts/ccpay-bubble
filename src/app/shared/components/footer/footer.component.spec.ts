f50cb542a263804546690b44e396b2a079fd8673
it.consulting@tutanota.com <it.consulting@tutanota.com>
2024-05-22 15:03:58 +0100

    PAY-6748: Paybubble footer details     [PAY-6748] (https://tools.hmcts.net/jira/browse/PAY-6748).

--- src/app/shared/components/footer/footer.component.spec.ts
+++ src/app/shared/components/footer/footer.component.spec.ts
@@ -1,6 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FooterComponent } from './footer.component';
-import { HmctsGlobalFooterComponent } from '../hmcts-global-footer/hmcts-global-footer.component';
 import { RouterTestingModule } from '@angular/router/testing'
 
 describe('FooterComponent', () => {
@@ -9,7 +8,7 @@ describe('FooterComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [FooterComponent, HmctsGlobalFooterComponent],
+      declarations: [FooterComponent],
       imports: [
         RouterTestingModule
       ],
