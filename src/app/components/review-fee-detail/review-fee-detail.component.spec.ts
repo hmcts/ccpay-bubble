855be29cf821025a1d26853e45048e26b4f685fd
Chris Worthington <Christopher.Worthington@HMCTS.NET>
2025-01-30 13:43:40 +0000

    PAY-5794 use spy for titleService

--- src/app/components/review-fee-detail/review-fee-detail.component.spec.ts
+++ src/app/components/review-fee-detail/review-fee-detail.component.spec.ts
@@ -9,11 +9,10 @@ import { HttpClient } from '@angular/common/http';
 import { Meta } from '@angular/platform-browser';
 import { of } from 'rxjs';
 import { FeeModel } from 'src/app/models/FeeModel';
-import { Title } from '@angular/platform-browser'
 
 describe('ReviewFeeDetailComponent', () => {
   let addFeeDetailService: AddFeeDetailService;
-  let titleService: Title;
+  let titleService: any;
   let router: any;
   let http: PaybubbleHttpClient;
   let component: ReviewFeeDetailComponent;
@@ -22,6 +21,7 @@ describe('ReviewFeeDetailComponent', () => {
     http = new PaybubbleHttpClient(instance(mock(HttpClient)), instance(mock(Meta)));
     addFeeDetailService = new AddFeeDetailService(http);
     router = { navigate: jasmine.createSpy('navigate')};
+    titleService = { setTitle: jasmine.createSpy('setTitle')};
     component = new ReviewFeeDetailComponent(router, addFeeDetailService, titleService);
   });
 
