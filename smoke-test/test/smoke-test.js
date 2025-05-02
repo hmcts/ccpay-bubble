1f088c7b136793a472f0a4055d37b4e2ef2e525c
it.consulting@tutanota.com <it.consulting@tutanota.com>
2025-05-02 15:07:36 +0100

    PAY-7782: Pipeline fixes - week beginning 29nd April 2025     [PAY-7782] (https://tools.hmcts.net/jira/browse/PAY-7782).

--- smoke-test/test/smoke-test.js
+++ smoke-test/test/smoke-test.js
@@ -1,1 +1,9 @@
+
+// This has been comment it will be fixed in another ticket.
+//Feature('CCPayBubble Smoke Test');
+//
+//Scenario('CCPayBubble Web Health Check Test', async ({ I }) => {
+//  I.amOnPage('/'); // Navigate to /health with the correct base URL
+//}).timeout(30);;
+
 console.log('needs to fixed')
