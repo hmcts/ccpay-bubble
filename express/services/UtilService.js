07bd709a6d1fd491585ff6600db0f23d714d3445
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2025-05-14 10:27:12 +0100

    more logging

--- express/services/UtilService.js
+++ express/services/UtilService.js
@@ -42,7 +42,7 @@ async function fetchWithAuth(url, authToken, options = {}) {
   };
   Logger.getLogger('fetchWithAuth -> UtilService.js').info(`Calling ${url} with options: ${JSON.stringify(options)}`);
   const resp = await fetch(url, options);
-  Logger.getLogger('fetchWithAuth -> UtilService.js').info(`Response => ${resp}`);
+  Logger.getLogger('fetchWithAuth -> UtilService.js').info(`Response ${JSON.stringify(resp)}`);
   return await handleFetchError(resp, url);
 }
 
