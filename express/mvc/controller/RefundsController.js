fd00d5d156956efc9d2a64853ab92374156026a7
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2025-05-16 15:16:06 +0100

    debug

--- express/mvc/controller/RefundsController.js
+++ express/mvc/controller/RefundsController.js
@@ -37,7 +37,7 @@ class RefundsController {
   patchRefundAction(req, res, appInsights) {
     return this.refundsService.patchRefundAction(req, appInsights)
       .then(result => {
-        res.status(200).text(result);
+        res.status(200).json(JSON.stringify(result));
       })
       .catch(error => {
         return errorHandler(res, error);
