27f8e7df01403e3ccf00f1f2478f72554ca4b9e2
Kiran.Yenigala <kiran.yenigala@hmcts.net>
2025-09-04 14:48:18 +0100

    CME-522 upgrade to angular 20 - standalone

---
+++ projects/view-payment/src/lib/view-payment.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ViewPaymentComponent } from './view-payment.component';
+import { SinglePaymentViewComponent } from './components/single-payment-view/single-payment-view.component';
+import { FeeComponent } from './components/shared/fee/fee.component';
+import { StatusHistoryComponent } from './components/shared/status-history/status-history.component';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        ViewPaymentComponent,
+        SinglePaymentViewComponent,
+        FeeComponent,
+        StatusHistoryComponent,
+    ],
+    exports: [ViewPaymentComponent]
+})
+export class ViewPaymentModule { }
