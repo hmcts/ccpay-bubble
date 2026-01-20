ad29b69ceee27fe405442bce70a84e1fa979219a
Chris Worthington <Christopher.Worthington@HMCTS.NET>
2026-01-20 14:11:17 +0000

    CME-844: Only sample /health

--- app-insights.js
+++ app-insights.js
@@ -1,13 +1,24 @@
 const config = require('config');
 const appInsights = require('applicationinsights');
 
+function fineGrainedSampling(envelope) {
+  if (
+    ['RequestData', 'RemoteDependencyData'].includes(envelope.data.baseType) &&
+    envelope.data.baseData.name.includes('/health')
+  ) {
+    envelope.sampleRate = 1;
+  }
+
+  return true;
+}
+
 module.exports = {
   enable() {
     appInsights.setup(config.get('secrets.ccpay.AppInsightsInstrumentationKey'))
       .setAutoDependencyCorrelation(true)
       .setAutoCollectConsole(true, true);
     appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = config.get('appInsights.roleName');
-    appInsights.defaultClient.config.samplingPercentage = 1;
+    appInsights.defaultClient.addTelemetryProcessor(fineGrainedSampling);
     appInsights.start();
     return appInsights;
   }
