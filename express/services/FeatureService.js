6baae37fc75b8d5109d87d431cfab5cce50b9c63
GraemeB <graeme.bullimore@hmcts.net>
2026-04-29 16:00:11 +0100

    PAY-8591: removed call to features endpoint

--- express/services/FeatureService.js
+++ express/services/FeatureService.js
@@ -1,13 +1,6 @@
-const config = require('config');
-const { fetchWithAuth } = require("./UtilService");
-
-const payhubUrl = config.get('payhub.url');
-
 class FeatureService {
-  async getFeatures(req) {
-    const url = `${payhubUrl}/api/ff4j/store/features`;
-    const resp = await fetchWithAuth(url, req.authToken);
-    return resp.json();
+  async getFeatures() {
+    return [];
   }
 }
 
