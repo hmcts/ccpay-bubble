266fc0bf78c1e60f92d7f1b5db27912672455be8
GraemeB <graeme.bullimore@hmcts.net>
2026-04-30 09:49:20 +0100

    PAY-8683: remove getFeatures(req)

--- express/services/FeatureService.js
+++ express/services/FeatureService.js
@@ -4,11 +4,11 @@ const { fetchWithAuth } = require("./UtilService");
 const payhubUrl = config.get('payhub.url');
 
 class FeatureService {
-  async getFeatures(req) {
-    const url = `${payhubUrl}/api/ff4j/store/features`;
-    const resp = await fetchWithAuth(url, req.authToken);
-    return resp.json();
-    }
+//  async getFeatures(req) {
+//    const url = `${payhubUrl}/api/ff4j/store/features`;
+//    const resp = await fetchWithAuth(url, req.authToken);
+//    return resp.json();
+//  }
 }
 
 module.exports = FeatureService;
