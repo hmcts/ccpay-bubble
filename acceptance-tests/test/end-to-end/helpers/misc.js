93afc6ffb1cfc0dcc25781d57a8aab35e64db64f
shravanmechineni <shravanmechineni5@gmail.com>
2026-05-29 17:29:08 +0100

    FTs coverage

--- acceptance-tests/test/end-to-end/helpers/misc.js
+++ acceptance-tests/test/end-to-end/helpers/misc.js
@@ -30,8 +30,6 @@ function multipleSearchForRefunds(CaseSearch, CaseTransaction, I, searchOption) 
   } else if (searchOptionLen === rcLen) {
     searchItem = 'RC Search';
   }
-
-  I.wait(CCPBATConstants.fiveSecondWaitTime);
   searchSpecificOption(searchItem, CaseSearch, searchOption);
 }
 
@@ -49,40 +47,7 @@ async function multipleSearch(CaseSearch, I, searchOption) {
   } else if (searchOptionLen === rcLen) {
     searchItem = 'RC Search';
   }
-
-  I.wait(CCPBATConstants.fiveSecondWaitTime);
   searchSpecificOption(searchItem, CaseSearch, searchOption);
-  const headerValue1 = await CaseSearch.getHeaderValue();
-  if (headerValue1 === 'Search for a case') {
-    const headerValue5 = await CaseSearch.getHeaderValue();
-    if (headerValue5 === 'Search for a case') {
-      searchSpecificOption(searchItem, CaseSearch, searchOption);
-    }
-  }
-
-  const headerValue2 = await CaseSearch.getHeaderValue();
-  if (headerValue2 === 'Search for a case') {
-    const headerValue6 = await CaseSearch.getHeaderValue();
-    if (headerValue6 === 'Search for a case') {
-      searchSpecificOption(searchItem, CaseSearch, searchOption);
-    }
-  }
-
-  const headerValue3 = await CaseSearch.getHeaderValue();
-  if (headerValue3 === 'Search for a case') {
-    const headerValue7 = await CaseSearch.getHeaderValue();
-    if (headerValue7 === 'Search for a case') {
-      searchSpecificOption(searchItem, CaseSearch, searchOption);
-    }
-  }
-
-  const headerValue4 = await CaseSearch.getHeaderValue();
-  if (headerValue4 === 'Search for a case') {
-    const headerValue8 = await CaseSearch.getHeaderValue();
-    if (headerValue8 === 'Search for a case') {
-      searchSpecificOption(searchItem, CaseSearch, searchOption);
-    }
-  }
 }
 
 module.exports = { multipleSearch, multipleSearchForRefunds };
