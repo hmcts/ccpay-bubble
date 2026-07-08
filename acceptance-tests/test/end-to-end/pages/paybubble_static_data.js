913b6ba4ac46fb6f9186a8a7603f146b07eb60b2
shravanmechineni <shravanmechineni5@gmail.com>
2026-07-08 13:45:44 +0100

    fix fee desc

--- acceptance-tests/test/end-to-end/pages/paybubble_static_data.js
+++ acceptance-tests/test/end-to-end/pages/paybubble_static_data.js
@@ -8,6 +8,6 @@ module.exports = {
     FEE0258: 'Application for a maintenance order to be registered 1950 Act or 1958 Act',
     FEE0209: 'Money Claims - Claim Amount - 10000.01 up to 200000 GBP. FEE AMOUNT = 5% of claim value',
     FEE0441: 'Hearing fee: Fast track case',
-    FEE0532: 'Request for certificate/certified document (no other fee specified)'
+    FEE0532: 'Request for certificate/certified document (no fee specified)'
   }
 };
