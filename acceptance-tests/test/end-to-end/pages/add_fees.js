e581172556202b895157ab567564929b7d785c18
shravanmechineni <shravanmechineni5@gmail.com>
2026-07-23 13:45:37 +0100

    add bulk scan allocation with inflation updated test fee

--- acceptance-tests/test/end-to-end/pages/add_fees.js
+++ acceptance-tests/test/end-to-end/pages/add_fees.js
@@ -6,7 +6,8 @@ const { I } = inject();
 module.exports = {
   locators: {
     fee_search: { xpath: '//*[@id="fee-search"]' },
-    locatoramountselect: { amount_select: { xpath: '//*[@id="fee-version0"]' } },
+    old_amount_select:  { xpath: '//input[@id="fee-version0"]' },
+    new_amount_select:  { xpath: '//input[@id="fee-versions"]' },
     locator_calculatedRangedFee: { xpath: '//*[@id="calculatedRangedFee"]' },
     search_for_fee_text: {xpath:'//*[@id="content"]//h1'},
     allocate_payment: {xpath:'//button[@class="button govuk-!-margin-right-1"]'},
@@ -32,9 +33,9 @@ module.exports = {
     I.wait(CCPBConstants.fiveSecondWaitTime);
 
     /* Comment this out when fee change options expire for inflation update. */
-    let numOfElements = await I.grabNumberOfVisibleElements('//input[@id=\'fee-version0\']');
+    let numOfElements = await I.grabNumberOfVisibleElements(this.locators.old_amount_select);
     if(numOfElements) {
-      I.click('//input[@id=\'fee-version0\']');
+      I.click(this.locators.old_amount_select);
       I.click('Continue');
       I.wait(CCPBConstants.fiveSecondWaitTime);
     }
@@ -60,9 +61,9 @@ module.exports = {
     I.click('Select');
     I.wait(CCPBConstants.fiveSecondWaitTime);
     /* Comment this out when fee change options expire for inflation update. */
-    let numOfElements = await I.grabNumberOfVisibleElements('//input[@id=\'fee-version0\']');
+    let numOfElements = await I.grabNumberOfVisibleElements(this.locators.old_amount_select);
     if(numOfElements) {
-      I.click('//input[@id=\'fee-version0\']');
+      I.click(this.locators.old_amount_select);
       I.click('Continue');
       I.wait(CCPBConstants.fiveSecondWaitTime);
     }
@@ -84,9 +85,9 @@ module.exports = {
       I.wait(CCPBConstants.fiveSecondWaitTime);
     }
     /* Comment this out when fee change options expire for inflation update. */
-    let numOfElements = await I.grabNumberOfVisibleElements('//input[@value=\'currentVersion\']');
+    let numOfElements = await I.grabNumberOfVisibleElements(this.locators.new_amount_select);
     if(numOfElements) {
-      I.click('//input[@value=\'currentVersion\']');
+      I.click(this.locators.new_amount_select);
       I.click('Continue');
       I.wait(CCPBConstants.fiveSecondWaitTime);
     }
@@ -105,9 +106,9 @@ module.exports = {
     I.wait(CCPBConstants.fiveSecondWaitTime);
 
     /* Comment this out when fee change options expire for inflation update. */
-    let numOfElements = await I.grabNumberOfVisibleElements('//input[@id=\'fee-version0\']');
+    let numOfElements = await I.grabNumberOfVisibleElements(this.locators.old_amount_select);
     if(numOfElements) {
-      I.click('//input[@id=\'fee-version0\']');
+      I.click(this.locators.old_amount_select);
       I.click('Continue');
       I.wait(CCPBConstants.fiveSecondWaitTime);
     }
@@ -124,4 +125,19 @@ module.exports = {
     I.click(this.locators.confirm_button);
 
   },
+
+  async addInflationUpdatedFee(feeCode) {
+    I.see('Search for a fee');
+    I.fillField(this.locators.fee_search, feeCode);
+    I.click('Search');
+    I.wait(CCPBConstants.fiveSecondWaitTime);
+    I.click('Select');
+    I.wait(CCPBConstants.fiveSecondWaitTime);
+    I.seeElement(this.locators.old_amount_select);
+    I.seeElement(this.locators.new_amount_select);
+    I.click(this.locators.new_amount_select);
+    I.click('Continue');
+    I.wait(CCPBConstants.fiveSecondWaitTime);
+  },
+
 };
